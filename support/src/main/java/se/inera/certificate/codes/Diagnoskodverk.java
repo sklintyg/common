f034e0e09f1aaec57d4a55fcd891d6e8e0244fa1
Erik Forsgren <erik.forsgren@r2m.se>
2015-01-30 17:16:19 +0100

    Flyttade enum med diagnoskodverk till common

---
+++ support/src/main/java/se/inera/certificate/codes/Diagnoskodverk.java
@@ -0,0 +1,34 @@
+package se.inera.certificate.codes;
+
+public enum Diagnoskodverk {
+    //TODO Move snomed_CT to its own enum 
+    SNOMED_CT("1.2.752.116.2.1.1.1", "SNOMED-CT", null),
+
+    ICD_10_SE("1.2.752.116.1.1.1.1.3", "ICD-10-SE", null),
+
+    KSH_97_P("1.2.752.116.1.3.1.4.1", "KSH97P", null); 
+
+    private Diagnoskodverk(String codeSystem, String codeSystemName, String codeSystemVersion) {
+        this.codeSystem = codeSystem;
+        this.codeSystemName = codeSystemName;
+        this.codeSystemVersion = codeSystemVersion;
+    }
+
+    private final String codeSystem;
+
+    private final String codeSystemName;
+
+    private final String codeSystemVersion;
+
+    public String getCodeSystem() {
+        return codeSystem;
+    }
+
+    public String getCodeSystemName() {
+        return codeSystemName;
+    }
+
+    public String getCodeSystemVersion() {
+        return codeSystemVersion;
+    }
+}
