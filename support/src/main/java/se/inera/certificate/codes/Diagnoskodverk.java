1543096c563bf679f7e222dcc98fd5cdc6aafd6e
Bjorn Beskow <bb@callistaenterprise.se>
2015-02-03 17:35:41 +0100

    INTYG-1389.

--- support/src/main/java/se/inera/certificate/codes/Diagnoskodverk.java
+++ support/src/main/java/se/inera/certificate/codes/Diagnoskodverk.java
@@ -4,7 +4,7 @@ public enum Diagnoskodverk {
     //TODO Move snomed_CT to its own enum 
     SNOMED_CT("1.2.752.116.2.1.1.1", "SNOMED-CT", null),
 
-    ICD_10_SE("1.2.752.116.1.1.1.1.3", "ICD-10-SE", null),
+    ICD_10_SE("1.2.752.116.1.1.1.1.3", "ICD-10", null),
 
     KSH_97_P("1.2.752.116.1.3.1.4.1", "KSH97P", null); 
 
