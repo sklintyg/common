13396a27dbfe8b6588d837b95a65e13c202dfe60
stephenwhite <stephen.white@mac.com>
2015-02-09 09:58:04 +0100

    [webcert-1402] - enter button disabler directive ...

---
+++ web/src/main/resources/META-INF/resources/webjars/common/webcert/js/directives/wcDisableKeyDown.js
@@ -0,0 +1,39 @@
+angular.module('common').directive('wcDisableKeyDown',
+    function($rootScope, $timeout) {
+        'use strict';
+        var removeEnter = function(element, scope, attrs){
+            element.bind('keydown', function(event) {
+                var code = event.keyCode || event.which;
+                if (code === 13) {
+                    if (!event.shiftKey) {
+                        event.preventDefault();
+                        scope.$apply(attrs.enterSubmit);
+                    }
+                }
+            });
+        }
+        return {
+            restrict: 'A',
+            scope: {
+                elements: '@wcDisableKeyDownElements',
+                doneLoading: '=wcDisableKeyDownDoneLoading'
+            },
+            link: function (scope, elem, attrs, ctrl, transclude) {
+                if (!scope.elements) {
+                    removeEnter(elem, scope, attrs);
+                } else {
+                    // postpone until the dom is rendered
+                    scope.$watch('doneLoading', function(doneLoading) {
+                        if (doneLoading) {
+                            var elements = scope.elements.split(",");
+                            angular.forEach(elements, function(value) {
+                                removeEnter(elem.parents('form').find(value), scope, attrs);
+                            })
+                        }
+                    });
+
+                }
+
+            }
+        }
+    });
