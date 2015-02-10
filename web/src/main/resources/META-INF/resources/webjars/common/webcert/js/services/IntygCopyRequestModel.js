958dc71aec5dad6ecc957333e463ff187829bec2
Benny Samuelsson <benny.samuelsson@usify.se>
2015-02-10 12:52:01 +0100

    Implementerade IntygCopyRequestModel.

--- web/src/main/resources/META-INF/resources/webjars/common/webcert/js/services/IntygCopyRequestModel.js
+++ web/src/main/resources/META-INF/resources/webjars/common/webcert/js/services/IntygCopyRequestModel.js
@@ -9,12 +9,15 @@ angular.module('common').factory('common.IntygCopyRequestModel',
         /**
          * Constructor
          */
-        function IntygCopyRequestModel() {
-
+        function IntygCopyRequestModel(intygId, intygType, patientPersonnummer, nyttPatientPersonnummer) {
+            this.intygId = intygId;
+            this.intygType = intygType;
+            this.patientPersonnummer = patientPersonnummer;
+            this.nyttPatientPersonnummer = nyttPatientPersonnummer;
         }
 
-        IntygCopyRequestModel.build = function() {
-            return new IntygCopyRequestModel();
+        IntygCopyRequestModel.build = function(data) {
+            return new IntygCopyRequestModel(data.intygId, data.intygType, data.patientPersonnummer, data.nyttPatientPersonnummer);
         };
 
         return IntygCopyRequestModel;
