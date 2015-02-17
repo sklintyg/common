38f59da09e7f2084c8429c2f5551f2f05caf92a5
Niklas Pettersson <niklas.pettersson@r2m.se>
2015-02-17 18:14:47 +0100

    WEBCERT-1479: Lagt till ny metod för att validera en diagnoskod

--- support/src/main/java/se/inera/certificate/modules/service/WebcertModuleService.java
+++ support/src/main/java/se/inera/certificate/modules/service/WebcertModuleService.java
@@ -1,5 +1,7 @@
 package se.inera.certificate.modules.service;
 
+import se.inera.certificate.codes.Diagnoskodverk;
+
 /**
  * Interface for exposing Webcert services that can be used by modules.
  *
@@ -12,8 +14,20 @@ public interface WebcertModuleService {
      * Validates a diagnosis code using the DiagnosService.
      *
      * @param codeFragment
-     * @param codeSystem
+     *              The code to validate
+     * @param codeSystemStr
+     *              A string representing the code system the diagnosis belongs to
      * @return true if the code matches a diagnosis, false otherwise.
      */
-    boolean validateDiagnosisCode(String codeFragment, String codeSystem);
+    boolean validateDiagnosisCode(String codeFragment, String codeSystemStr);
+    
+    /**
+     * Validates a diagnosis code using the DiagnosService.
+     *
+     * @param codeFragment
+     * @param codeSystem
+     *              Enum representing the code system the diagnosis belongs to
+     * @return true if the code matches a diagnosis, false otherwise.
+     */
+    boolean validateDiagnosisCode(String codeFragment, Diagnoskodverk codeSystem);
 }
