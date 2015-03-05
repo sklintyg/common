ba84925f405e5b3185993e0a7d5a96e0b9011958
Per Beijer <per.beijer@usify.se>
2015-03-05 15:38:03 +0100

    WEBCERT-1014 Skapa ett header-direktiv för "signerat-sidor" som alla intyg kan använda

---
+++ web/src/main/resources/META-INF/resources/webjars/common/webcert/js/directives/wcIntygHeader.js
@@ -0,0 +1,65 @@
+/* global document */
+angular.module('common').directive('wcIntygHeader',
+    ['$log', '$routeParams', 'common.messageService', 'webcert.ManageCertificate', 'common.ManageCertView',
+     'common.IntygCopyRequestModel', 'common.User',
+        function($log, $routeParams, messageService, ManageCertificate, ManageCertView, IntygCopyRequestModel, User) {
+            'use strict';
+            return {
+                restrict: 'A',
+                replace: true,
+                scope: true,
+                controller: function($scope) {
+                    $scope.intygstyp = $routeParams.certificateType;
+
+                    ManageCertificate.initSend($scope);
+                    $scope.send = function(cert) {
+                        cert.intygType = $routeParams.certificateType;
+                        ManageCertificate.send($scope, cert, 'FK', $scope.intygstyp+'.label.send', function() {
+                            $scope.$emit('loadCertificate');
+                        });
+                    };
+
+                    ManageCertificate.initMakulera($scope);
+                    $scope.makulera = function(cert) {
+                        var confirmationMessage = messageService.getProperty($scope.intygstyp+'.label.makulera.confirmation', {
+                            namn: cert.grundData.patient.fullstandigtNamn,
+                            personnummer: cert.grundData.patient.personId
+                        });
+                        cert.intygType = $routeParams.certificateType;
+                        ManageCertificate.makulera($scope, cert, confirmationMessage, function() {
+                            $scope.$emit('loadCertificate');
+                        });
+                    };
+
+                    ManageCertificate.initCopyDialog($scope);
+                    $scope.copy = function(cert) {
+                        if (cert === undefined || cert.grundData === undefined) {
+                            $log.debug('cert or cert.grundData is undefined. Aborting copy.');
+                            return;
+                        }
+
+                        var isOtherCareUnit = User.getValdVardenhet() !== cert.grundData.skapadAv.vardenhet.enhetsid;
+
+                        ManageCertificate.copy($scope,
+                            IntygCopyRequestModel.build({
+                                intygId: cert.id,
+                                intygType: $routeParams.certificateType,
+                                patientPersonnummer: cert.grundData.patient.personId,
+                                nyttPatientPersonnummer: $routeParams.patientId
+                            }),
+                            isOtherCareUnit);
+                    };
+
+                    $scope.print = function(cert) {
+                        if ($scope.certProperties.isRevoked) {
+                            ManageCertView.printDraft(cert.id, $routeParams.certificateType);
+                        } else {
+                            document.pdfForm.submit();
+                        }
+                    };
+                },
+                templateUrl: '/web/webjars/common/webcert/js/directives/wcIntygHeader.html'
+            };
+        }
+    ]
+);
