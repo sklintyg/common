ad1cf49e117ef2cc66d1a09dce7d4e0fad489c9e
Magnus Ekstrand <magnus@ekstrand.net>
2015-04-09 11:15:25 +0200

    WEBCERT-1651: Created new schemas module/project. Moved Java-code fron schemas repo to common repo.

---
+++ schemas/src/main/java/se/inera/intyg/common/schemas/Constants.java
@@ -0,0 +1,12 @@
+package se.inera.intyg.common.schemas;
+
+
+public final class Constants {
+
+    public static final String HSA_ID_OID = "1.2.752.129.2.1.4.1";
+    public static final String PERSON_ID_OID = "1.2.752.129.2.1.3.1";
+    public static final String SAMORDNING_ID_OID = "1.2.752.129.2.1.3.3";
+    public static final String ARBETSPLATS_KOD_OID = "1.2.752.29.4.71";
+
+    private Constants() { }
+}
