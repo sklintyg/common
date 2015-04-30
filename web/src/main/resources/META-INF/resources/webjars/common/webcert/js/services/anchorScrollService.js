1cedf01169f1220e41e5f51d92a88e198c5a513c
Erik Lupander <erik.lupander@callistaenterprise.se>
2015-04-30 10:30:59 +0200

    WEBCERT-1752 : Anchor scroll service now uses external scrolling library

--- web/src/main/resources/META-INF/resources/webjars/common/webcert/js/services/anchorScrollService.js
+++ web/src/main/resources/META-INF/resources/webjars/common/webcert/js/services/anchorScrollService.js
@@ -1,22 +1,65 @@
 /**
  * Created by eriklupander on 15-04-29.
  */
-angular.module('common').factory('common.anchorScrollService', function($window, $location, $anchorScroll) {
+angular.module('common').factory('common.anchorScrollService', ['$location', 'smoothScroll', function($location, smoothScrollService) {
     'use strict';
 
-    function _scrollTo(anchorName) {
+    /**
+     *
+     * @param anchorName ID on the anchor element.
+     * @param offset If undefined, an offset based on the top menu height will be applied.
+     * @private
+     */
+    function _scrollTo(anchorName, offset) {
+
         var old = $location.hash();
         $location.hash(anchorName);
-        $anchorScroll();
-        // TODO add soft scrolling.
+
+        var elementToScrollTo = angular.element.find('#' + _escape(anchorName))[0];
+
+        var options = {
+            duration: 500,
+            easing: 'easeInOutQuart',
+            offset: _calculateOffset(offset)
+        }
+
+        smoothScrollService(elementToScrollTo, options);
+
         // reset to old to keep any additional routing logic from kicking in
         $location.hash(old);
     }
 
     function _scrollToWithOffset(anchorName, offset) {
-        _scrollTo(anchorName);
-        // TODO Apply offset
+        _scrollTo(anchorName, offset);
+    }
 
+    /**
+     * Calculate offset based on top menu height.
+     *
+     * @param offset
+     * @returns if offset was supplied, the unchanged value is returned. If an undefined value was supplied,
+     *          the return value is calculated from the first element found in the DOM having the 'header-fix-top'
+     *          class.
+     * @private
+     */
+    function _calculateOffset(offset) {
+        if (offset === null || typeof offset === 'undefined') {
+
+            var topMenuElements = angular.element.find('.header-fix-top');
+            if (topMenuElements.length > 0) {
+                offset = angular.element(topMenuElements[0]).prop('offsetHeight');
+            } else {
+                offset = 0;
+            }
+        }
+        return offset;
+    }
+
+    /**
+     * Since the anchors uses dots in their id's, we need to esacpe them before passing them to the selector function.
+     */
+    function _escape(str) {
+        return str.replace(/\./g, '\\.');
     }
 
 
@@ -25,4 +68,4 @@ angular.module('common').factory('common.anchorScrollService', function($window,
         scrollToWithOffset : _scrollToWithOffset
     };
 
-});
+}]);
