436b2a2833189aac46755cb23ea2956579609604
Jesper Holmberg <jesper.holmberg@callistaenterprise.se>
2015-05-21 08:44:33 +0200

    WEBCERT-1314 Uppdatera exception med info om huruvida problemet är permanent eller inte

--- support/src/main/java/se/inera/certificate/modules/support/api/exception/ExternalServiceCallException.java
+++ support/src/main/java/se/inera/certificate/modules/support/api/exception/ExternalServiceCallException.java
@@ -4,19 +4,42 @@ public class ExternalServiceCallException extends ModuleException {
 
     private static final long serialVersionUID = 4316452926850518463L;
 
+    private final ErrorIdEnum erroIdEnum;
+
     public ExternalServiceCallException() {
         super();
+        this.erroIdEnum = ErrorIdEnum.APPLICATION_ERROR;
     }
 
     public ExternalServiceCallException(String message, Throwable cause) {
         super(message, cause);
+        this.erroIdEnum = ErrorIdEnum.APPLICATION_ERROR;
     }
 
     public ExternalServiceCallException(String message) {
         super(message);
+        this.erroIdEnum = ErrorIdEnum.APPLICATION_ERROR;
     }
 
     public ExternalServiceCallException(Throwable cause) {
         super(cause);
+        this.erroIdEnum = ErrorIdEnum.APPLICATION_ERROR;
     }
+
+    public ExternalServiceCallException(String message, ErrorIdEnum errorIdEnum) {
+        super(message);
+        this.erroIdEnum = errorIdEnum;
+    }
+
+    public ErrorIdEnum getErroIdEnum() {
+        return this.erroIdEnum;
+    }
+
+    public enum ErrorIdEnum {
+        VALIDATION_ERROR,
+        TRANSFORMATION_ERROR,
+        APPLICATION_ERROR,
+        TECHNICAL_ERROR;
+    }
+
 }
