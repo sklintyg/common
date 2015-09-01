4d53d474a8ddbd973d6c1a26a1fa66e6712753ba
stephenwhite <stephen.white@callistaenterprise.se>
2015-09-01 09:55:11 +0200

    fitness fixes relaxed the animation detection so we just look at the end count ...

--- web/src/main/resources/META-INF/resources/webjars/common/webcert/gui/wcDetectAnimation.directive.js
+++ web/src/main/resources/META-INF/resources/webjars/common/webcert/gui/wcDetectAnimation.directive.js
@@ -41,29 +41,36 @@ angular.module('common').directive('wcDetectAnimation',
                     $window.animations[group].ids[id] = {rendered:false};
                 }
 
-                ////$log.info('--- add animation group : ' + group + ', id: ' + id + ', count :' + $window.animations[group].count);
+                //$log.info('--- add animation group : ' + group + ', id: ' + id + ', count :' + $window.animations[group].count);
 
                 var init = function(){
                     element.on('$animate:before', function() {
-                        ////$log.info('------------ id:'+id+', animate:before saving:' + $window.saving );
+                        $log.info('------------ id:'+id+', animate:before saving:' + $window.saving );
+                        if($window.animations[group].startCount > $window.animations[group].count){
+                            $window.animations[group].startCount = 0;
+                        }
+
                         $window.animations[group].startCount ++;
                         $window.animations[group].endCount ++;
+
                         $window.rendered = false;
                         $window.animations[group].rendered = false;
                         $window.animations[group].ids[id].rendered = false;
                         $rootScope.$broadcast('wcAnimationStart');
-                        //$log.info('++++ show g:'+group+' id:'+id + ', c:' + $window.animations[group].count );
-                        //$log.info('     startCount:' +$window.animations[group].startCount );
-                        //$log.info('     endCount:'+ $window.animations[group].endCount);
-                        //$log.info('     wr:' + $window.rendered + ', gr:' + $window.animations[group].rendered);
-                        //$log.info('     element ren:' + $window.animations[group].ids[id].rendered);
+                        $log.info('++++ show g:'+group+' id:'+id + ', c:' + $window.animations[group].count );
+                        $log.info('     startCount:' +$window.animations[group].startCount );
+                        $log.info('     endCount:'+ $window.animations[group].endCount);
+                        $log.info('     wr:' + $window.rendered + ', gr:' + $window.animations[group].rendered);
+                        $log.info('     element ren:' + $window.animations[group].ids[id].rendered);
                     });
                     element.on('$animate:close', function() {
-                        ////$log.info('------------- id:'+id+', animate:close saving:' + $window.saving );
+                        //$log.info('------------- id:'+id+', animate:close saving:' + $window.saving );
                         $window.animations[group].endCount --;
 
-                        if($window.animations[group].startCount === $window.animations[group].count
-                            && $window.animations[group].endCount === 0) {
+                        if($window.animations[group].endCount < 0 ){
+                            $window.animations[group].endCount = 0;
+                        }
+                        if($window.animations[group].endCount === 0) {
                             $window.animations[group].resetGroup();
                             $window.animations[group].rendered = true;
                             $window.rendered = true;
@@ -72,11 +79,11 @@ angular.module('common').directive('wcDetectAnimation',
 
                         $window.animations[group].ids[id].rendered = true;
 
-                        //$log.info('---- close g:'+group+' id:'+id + ', c:' + $window.animations[group].count);
-                        //$log.info('     startCount: '+ $window.animations[group].startCount);
-                        //$log.info('     endCount:'+ $window.animations[group].endCount);
-                        //$log.info('     wr:' + $window.rendered + ', gr:' + $window.animations[group].rendered);
-                        //$log.info('     element ren:' + $window.animations[group].ids[id].rendered);
+                        $log.info('---- close g:'+group+' id:'+id + ', c:' + $window.animations[group].count);
+                        $log.info('     startCount: '+ $window.animations[group].startCount);
+                        $log.info('     endCount:'+ $window.animations[group].endCount);
+                        $log.info('     wr:' + $window.rendered + ', gr:' + $window.animations[group].rendered);
+                        $log.info('     element ren:' + $window.animations[group].ids[id].rendered);
 
                     });
                 };
