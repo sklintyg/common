a131b701521424b81923bcf68ec2b19e4fe3e087
Magnus Ekstrand <magnus@ekstrand.net>
2015-11-12 21:24:51 +0100

    WEBCERT-2165: Fixade jasmin-test för uppbuggnad av mejllänk

--- web/src/test/js/webcert/intyg/fragaSvarCommonServiceSpec.js
+++ web/src/test/js/webcert/intyg/fragaSvarCommonServiceSpec.js
@@ -65,6 +65,7 @@ describe('fragaSvarCommonService', function() {
         it('should be correct for uthopp role', inject(['common.fragaSvarCommonService', function(fragaSvarCommonService) {
             userModelMock.isVardAdministratorUthopp = function(){ return true; };
             userModelMock.isLakareUthopp = function(){ return true; };
+            userModelMock.isTandlakareUthopp = function(){ return true; };
             var link = fragaSvarCommonService.buildMailToLink(qa);
             expect(link).toEqual("mailto:?subject=En%20fraga-svar%20ska%20besvaras%20i%20Webcert&body=Klicka%20pa%20lanktexten%20for%20att%20besvara%20fraga-svar%3A%0Ahttp%3A%2F%2Flocalhost%3A9876%2Fwebcert%2Fweb%2Fuser%2Fcertificate%2Ftestid%2Fquestions");
         }]));
@@ -72,6 +73,7 @@ describe('fragaSvarCommonService', function() {
         it('should be correct for lakare uthopp role', inject(['common.fragaSvarCommonService', function(fragaSvarCommonService) {
             userModelMock.isVardAdministratorUthopp = function(){ return false; };
             userModelMock.isLakareUthopp = function(){ return true; };
+            userModelMock.isTandlakareUthopp = function(){ return true; };
             var link = fragaSvarCommonService.buildMailToLink(qa);
             expect(link).toEqual("mailto:?subject=En%20fraga-svar%20ska%20besvaras%20i%20Webcert&body=Klicka%20pa%20lanktexten%20for%20att%20besvara%20fraga-svar%3A%0Ahttp%3A%2F%2Flocalhost%3A9876%2Fwebcert%2Fweb%2Fuser%2Fcertificate%2Ftestid%2Fquestions");
         }]));
@@ -79,6 +81,7 @@ describe('fragaSvarCommonService', function() {
         it('should be correct for admin uthopp role', inject(['common.fragaSvarCommonService', function(fragaSvarCommonService) {
             userModelMock.isVardAdministratorUthopp = function(){ return true; };
             userModelMock.isLakareUthopp = function(){ return false; };
+            userModelMock.isTandlakareUthopp = function(){ return false; };
             var link = fragaSvarCommonService.buildMailToLink(qa);
             expect(link).toEqual("mailto:?subject=En%20fraga-svar%20ska%20besvaras%20i%20Webcert&body=Klicka%20pa%20lanktexten%20for%20att%20besvara%20fraga-svar%3A%0Ahttp%3A%2F%2Flocalhost%3A9876%2Fwebcert%2Fweb%2Fuser%2Fcertificate%2Ftestid%2Fquestions");
         }]));
@@ -86,11 +89,11 @@ describe('fragaSvarCommonService', function() {
         it('should be correct for landsting role', inject(['common.fragaSvarCommonService', function(fragaSvarCommonService) {
             userModelMock.isVardAdministratorUthopp = function(){ return false; };
             userModelMock.isLakareUthopp = function(){ return false; };
+            userModelMock.isTandlakareUthopp = function(){ return false; };
             var link = fragaSvarCommonService.buildMailToLink(qa);
             expect(link).toEqual("mailto:?subject=En%20fraga-svar%20ska%20besvaras%20i%20Webcert&body=Klicka%20pa%20lanktexten%20for%20att%20besvara%20fraga-svar%3A%0Ahttp%3A%2F%2Flocalhost%3A9876%2Fwebcert%2Fweb%2Fuser%2Fbasic-certificate%2Ftestid%2Fquestions");
         }]));
 
     });
-
 
 });
