fd958c0ea7bf1f23ffa4bbab6b70ec167122865f
Rasmus Lundberg-Åblad <rasmus.ablad@usify.se>
2016-02-12 08:58:59 +0100

    INTYG-1918: prototypväxlare för intygsvyerna

--- web/src/main/resources/META-INF/resources/webjars/common/webcert/gui/wcPrototype.directive.js
+++ web/src/main/resources/META-INF/resources/webjars/common/webcert/gui/wcPrototype.directive.js
@@ -17,77 +17,15 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/**
- * wc-prototype directive makes it possible to include a dropdown switch and several tempaltes in a specified area
- *
- * usage:
- *  directive demands the following attributes on the element:
- *  dropdown-type : button or nothing, will render as a link if not button
- *  selected-value: property for holding the currently selected value
- *  preselected-value: the default template to load as default, and also triggers on the 'default' selected value
- *  data-dropdown-data: data array for populating the dropdown with template ids and names, used for the nested directive wc-prototype-template
- */
 angular.module('common').directive('wcPrototype',
     function($compile) {
         'use strict';
         return {
             restrict: 'E',
-            scope: {
-                items: '=dropdownData',
-                doSelect: '&selectVal',
-                selectedItem: '=preselectedItem',
-                hasSelected: '=prototypeSelected'
-            },
-            template: '<div class="webcert-proto-area" ng-class="{\'webcert-proto-selected\': hasSelected, \'webcert-proto-unselected\': !hasSelected  }"></div>',
-            link: function(scope, element, attrs) {
-                var html = '<div class="form-group proto-selector">';
+            replace: true,
+            transclude: true,
+            template: '<div class="webcert-proto-area wc-prototype-chooser-default" ng-transclude></div>'
 
-                switch (attrs.dropdownType) {
-                case 'button':
-                    html +=
-                        '<div class="btn-group"><button class="btn button-label btn-info">Action</button>' +
-                        '<button class="btn btn-info dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">' +
-                        '<span class="glyphicon glyphicon-random" aria-hidden="true"></span><span class="caret"></span><span class="sr-only">Default</span>' +
-                        '</button><span class="caret"></span></button>';
-                    break;
-                default:
-                    html +=
-                        '<div class="dropdown"><a class="dropdown-toggle" role="button" data-toggle="dropdown"  href="javascript:;">Dropdown<b class="caret"></b></a>';
-                    break;
-                }
-                html +=
-                    '<ul class="dropdown-menu"><li ng-repeat="item in items"><a tabindex="-1" data-ng-click="selectVal(item)">{{item.name}}</a></li></ul></div>';
-                html += '</div>';
-                element.append($compile(html)(scope));
-
-                for (var i = 0; i < scope.items.length; i++) {
-                    if (scope.items[i].id === scope.selectedItem) {
-                        scope.bSelectedItem = scope.items[i];
-                        break;
-                    }
-                }
-
-                scope.selectVal = function(item) {
-                    switch (attrs.dropdownType) {
-                    case 'button':
-                        $('button.button-label', element).html(item.name);
-                        break;
-                    default:
-                        $('a.dropdown-toggle', element).html('<b class="caret"></b> ' + item.name);
-                        break;
-                    }
-                    scope.doSelect({
-                        selectedVal: item.id
-                    });
-
-                    if (item.name === 'Default') {
-                        scope.hasSelected = false;
-                    } else {
-                        scope.hasSelected = true;
-                    }
-                };
-               // scope.selectVal(scope.bSelectedItem);
-            }
         };
 
     });
