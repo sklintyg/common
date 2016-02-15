31afcc53b22dc07848f00cad03cd3cfa33021d7d
Benny Samuelsson <benny.samuelsson@usify.se>
2016-02-15 09:05:09 +0100

    INTYG-1935: Tog bort oanvänd kod för radio group.

--- web/src/main/resources/META-INF/resources/webjars/common/webcert/gui/formly/radioGroup.formly.js
+++ web/src/main/resources/META-INF/resources/webjars/common/webcert/gui/formly/radioGroup.formly.js
@@ -4,23 +4,7 @@ angular.module('common').run(function(formlyConfig) {
     formlyConfig.setType({
         name: 'radio-group',
         templateUrl: '/web/webjars/common/webcert/gui/formly/radioGroup.formly.html',
-        controller: ['$scope', 'common.dynamicLabelService', function($scope, dynamicLabelService) {
-
-            $scope.$watch('model.' + $scope.options.key, function(newVal, oldVal) {
-                var formState = $scope.formState;
-                if (!formState[$scope.options.key]) {
-                    formState[$scope.options.key] = {};
-                }
-                if (newVal) {
-                    formState[$scope.options.key].checked = true;
-                } else {
-                    formState[$scope.options.key].checked = false;
-                }
-            });
-
-            $scope.getDynamicText = function(key) {
-                return dynamicLabelService.getProperty(key);
-            };
+        controller: ['$scope', function($scope) {
         }]
     });
 
