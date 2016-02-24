90724946c216df33c28f8948994f9acfd331bd6e
Katarina Olsson <katarina.olsson@r2m.se>
2016-02-24 17:43:56 +0100

    INTYG-1949: Removed warning.

--- support/src/main/java/se/inera/intyg/common/support/validate/ValidationException.java
+++ support/src/main/java/se/inera/intyg/common/support/validate/ValidationException.java
@@ -24,9 +24,6 @@ package se.inera.intyg.common.support.validate;
  */
 public class ValidationException extends RuntimeException {
 
-    /**
-     * 
-     */
     private static final long serialVersionUID = 1L;
 
     public ValidationException(String message) {
