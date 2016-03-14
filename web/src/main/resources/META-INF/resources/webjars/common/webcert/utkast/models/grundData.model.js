e9d6f5e0d077d683c4f76f256b9ce6ec7a19a448
Per Beijer <per.beijer@usify.se>
2016-03-14 13:37:54 +0100

    SVARA: work in progress

--- web/src/main/resources/META-INF/resources/webjars/common/webcert/utkast/models/grundData.model.js
+++ web/src/main/resources/META-INF/resources/webjars/common/webcert/utkast/models/grundData.model.js
@@ -18,34 +18,37 @@
  */
 
 angular.module('common').factory('common.Domain.GrundDataModel',
-    [ 'common.Domain.SkapadAvModel',
-        'common.Domain.PatientModel',  function(SkapadAvModel, PatientModel) {
-        'use strict';
+    [ 'common.Domain.SkapadAvModel', 'common.Domain.PatientModel', 'common.Domain.RelationModel',
+        function(SkapadAvModel, PatientModel, RelationModel) {
+            'use strict';
 
-        /**
-         * Constructor, with class name
-         */
-        function GrundDataModel() {
-            this.skapadAv = SkapadAvModel.build();
-            this.patient = PatientModel.build();
-        }
-
-        GrundDataModel.prototype.update = function (grundData) {
-            // refresh the model data
-            if(grundData === undefined) {
-                return;
+            /**
+             * Constructor, with class name
+             */
+            function GrundDataModel() {
+                this.skapadAv = SkapadAvModel.build();
+                this.patient = PatientModel.build();
+                this.relation = RelationModel.build();
             }
-            this.skapadAv.update(grundData.skapadAv);
-            this.patient.update(grundData.patient);
-        };
 
-        GrundDataModel.build = function() {
-            return new GrundDataModel();
-        };
+            GrundDataModel.prototype.update = function (grundData) {
+                // refresh the model data
+                if(grundData === undefined) {
+                    return;
+                }
+                this.skapadAv.update(grundData.skapadAv);
+                this.patient.update(grundData.patient);
+                this.relation.update(grundData.relation);
+            };
 
-        /**
-         * Return the constructor function GrundDataModel
-         */
-        return GrundDataModel;
+            GrundDataModel.build = function() {
+                return new GrundDataModel();
+            };
 
-    }]);
+            /**
+             * Return the constructor function GrundDataModel
+             */
+            return GrundDataModel;
+
+        }
+    ]);
