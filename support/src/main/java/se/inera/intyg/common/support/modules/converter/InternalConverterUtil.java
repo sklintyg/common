f834bd83dbd502e086021f637059dced493fa980
Mikaela Nöteberg <mnoteberg@gmail.com>
2016-04-21 13:04:25 +0200

    INTYG-2295: Util för UtlatandeToIntyg

---
+++ support/src/main/java/se/inera/intyg/common/support/modules/converter/InternalConverterUtil.java
@@ -0,0 +1,221 @@
+/*
+ * Copyright (C) 2016 Inera AB (http://www.inera.se)
+ *
+ * This file is part of sklintyg (https://github.com/sklintyg).
+ *
+ * sklintyg is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * sklintyg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package se.inera.intyg.common.support.modules.converter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.JAXBElement;
+import javax.xml.namespace.QName;
+
+import org.apache.commons.lang3.StringUtils;
+import org.joda.time.LocalDate;
+
+import se.inera.intyg.common.support.common.enumerations.BefattningKod;
+import se.inera.intyg.common.support.model.common.internal.*;
+import se.riv.clinicalprocess.healthcond.certificate.types.v2.*;
+import se.riv.clinicalprocess.healthcond.certificate.v2.*;
+import se.riv.clinicalprocess.healthcond.certificate.v2.Patient;
+import se.riv.clinicalprocess.healthcond.certificate.v2.Relation;
+import se.riv.clinicalprocess.healthcond.certificate.v2.Svar.Delsvar;
+import se.riv.clinicalprocess.healthcond.certificate.v2.Vardgivare;
+
+public final class InternalConverterUtil {
+
+    public static final String PERSON_ID_ROOT = "1.2.752.129.2.1.3.3";
+    public static final String HSA_ID_ROOT = "1.2.752.129.2.1.4.1";
+    public static final String CERTIFICATE_CODE_SYSTEM = "f6fb361a-e31d-48b8-8657-99b63912dd9b";
+    public static final String BEFATTNING_CODE_SYSTEM = "1.2.752.129.2.2.1.4";
+    public static final String ARBETSPLATSKOD_ROOT = "1.2.752.29.4.71";
+    public static final String RELATION_CODE_SYSTEM = "c2362fcd-eda0-4f9a-bd13-b3bbaf7f2146";
+
+    private InternalConverterUtil() {
+    }
+
+    public static Intyg getIntyg(Utlatande source) {
+        Intyg intyg = new Intyg();
+        intyg.setIntygsId(getIntygsId(source));
+        intyg.setVersion(getTextVersion(source));
+        intyg.setSigneringstidpunkt(source.getGrundData().getSigneringsdatum());
+        intyg.setSkickatTidpunkt(source.getGrundData().getSigneringsdatum());
+        intyg.setSkapadAv(getSkapadAv(source));
+        intyg.setPatient(getPatient(source.getGrundData().getPatient()));
+        decorateWithRelation(intyg, source);
+        return intyg;
+    }
+
+    private static HosPersonal getSkapadAv(Utlatande source) {
+        HoSPersonal sourceSkapadAv = source.getGrundData().getSkapadAv();
+        HosPersonal skapadAv = new HosPersonal();
+        skapadAv.setPersonalId(anHsaId(sourceSkapadAv.getPersonId()));
+        skapadAv.setFullstandigtNamn(sourceSkapadAv.getFullstandigtNamn());
+        skapadAv.setForskrivarkod(sourceSkapadAv.getForskrivarKod());
+        skapadAv.setEnhet(getEnhet(sourceSkapadAv.getVardenhet()));
+        for (String sourceBefattning : sourceSkapadAv.getBefattningar()) {
+            Befattning befattning = new Befattning();
+            befattning.setCodeSystem(BEFATTNING_CODE_SYSTEM);
+            befattning.setCode(sourceBefattning);
+            befattning.setDisplayName(BefattningKod.getDisplayNameFromCode(sourceBefattning));
+            skapadAv.getBefattning().add(befattning);
+        }
+        for (String sourceKompetens : sourceSkapadAv.getSpecialiteter()) {
+            Specialistkompetens kompetens = new Specialistkompetens();
+            kompetens.setCode(sourceKompetens);
+            skapadAv.getSpecialistkompetens().add(kompetens);
+        }
+        return skapadAv;
+    }
+
+    private static Enhet getEnhet(Vardenhet sourceVardenhet) {
+        Enhet vardenhet = new Enhet();
+        vardenhet.setEnhetsId(anHsaId(sourceVardenhet.getEnhetsid()));
+        vardenhet.setEnhetsnamn(sourceVardenhet.getEnhetsnamn());
+        vardenhet.setPostnummer(sourceVardenhet.getPostnummer());
+        vardenhet.setPostadress(sourceVardenhet.getPostadress());
+        vardenhet.setPostort(sourceVardenhet.getPostort());
+        vardenhet.setTelefonnummer(sourceVardenhet.getTelefonnummer());
+        vardenhet.setEpost(sourceVardenhet.getEpost());
+        vardenhet.setVardgivare(getVardgivare(sourceVardenhet.getVardgivare()));
+        vardenhet.setArbetsplatskod(getArbetsplatsKod(sourceVardenhet.getArbetsplatsKod()));
+        return vardenhet;
+    }
+
+    private static ArbetsplatsKod getArbetsplatsKod(String sourceArbetsplatsKod) {
+        ArbetsplatsKod arbetsplatsKod = new ArbetsplatsKod();
+        arbetsplatsKod.setRoot(ARBETSPLATSKOD_ROOT);
+        arbetsplatsKod.setExtension(sourceArbetsplatsKod);
+        return arbetsplatsKod;
+    }
+
+    private static Vardgivare getVardgivare(se.inera.intyg.common.support.model.common.internal.Vardgivare sourceVardgivare) {
+        Vardgivare vardgivare = new Vardgivare();
+        vardgivare.setVardgivareId(anHsaId(sourceVardgivare.getVardgivarid()));
+        vardgivare.setVardgivarnamn(sourceVardgivare.getVardgivarnamn());
+        return vardgivare;
+    }
+
+    private static Patient getPatient(se.inera.intyg.common.support.model.common.internal.Patient sourcePatient) {
+        Patient patient = new se.riv.clinicalprocess.healthcond.certificate.v2.Patient();
+        patient.setEfternamn(sourcePatient.getEfternamn());
+        patient.setFornamn(emptyStringIfNull(sourcePatient.getFornamn()));
+        patient.setMellannamn(sourcePatient.getMellannamn());
+        PersonId personId = new PersonId();
+        personId.setRoot(PERSON_ID_ROOT);
+        personId.setExtension(sourcePatient.getPersonId().getPersonnummer().replaceAll("-", ""));
+        patient.setPersonId(personId);
+        patient.setPostadress(emptyStringIfNull(sourcePatient.getPostadress()));
+        patient.setPostnummer(emptyStringIfNull(sourcePatient.getPostnummer()));
+        patient.setPostort(emptyStringIfNull(sourcePatient.getPostort()));
+        return patient;
+    }
+
+    private static IntygId getIntygsId(Utlatande source) {
+        IntygId intygId = new IntygId();
+        intygId.setRoot(source.getGrundData().getSkapadAv().getVardenhet().getEnhetsid());
+        intygId.setExtension(source.getId());
+        return intygId;
+    }
+
+    private static String getTextVersion(Utlatande source) {
+        return emptyStringIfNull(source.getTextVersion());
+    }
+
+    private static void decorateWithRelation(Intyg intyg, Utlatande source) {
+        if (source.getGrundData().getRelation() == null || source.getGrundData().getRelation().getRelationKod() == null) {
+            return;
+        }
+        Relation relation = new Relation();
+
+        IntygId intygId = new IntygId();
+        intygId.setRoot(source.getGrundData().getSkapadAv().getVardenhet().getEnhetsid());
+        intygId.setExtension(source.getGrundData().getRelation().getRelationIntygsId());
+
+        TypAvRelation typAvRelation = new TypAvRelation();
+        typAvRelation.setCode(source.getGrundData().getRelation().getRelationKod().value());
+        typAvRelation.setCodeSystem(RELATION_CODE_SYSTEM);
+        typAvRelation.setDisplayName(source.getGrundData().getRelation().getRelationKod().getKlartext());
+
+        relation.setIntygsId(intygId);
+        relation.setTyp(typAvRelation);
+
+        intyg.getRelation().add(relation);
+    }
+
+    private static HsaId anHsaId(String id) {
+        HsaId hsaId = new HsaId();
+        hsaId.setRoot(HSA_ID_ROOT);
+        hsaId.setExtension(id);
+        return hsaId;
+    }
+
+    private static String emptyStringIfNull(String s) {
+        return s != null ? s : "";
+    }
+
+    public static void addIfNotBlank(List<Svar> svars, String svarsId, String delsvarsId, String content) {
+        if (!StringUtils.isBlank(content)) {
+            svars.add(aSvar(svarsId).withDelsvar(delsvarsId, content).build());
+        }
+    }
+
+    public static JAXBElement<DatePeriodType> aDatePeriod(LocalDate from, LocalDate tom) {
+        DatePeriodType period = new DatePeriodType();
+        period.setStart(from);
+        period.setEnd(tom);
+        return new JAXBElement<>(new QName("urn:riv:clinicalprocess:healthcond:certificate:types:2", "datePeriod"), DatePeriodType.class, null,
+                period);
+    }
+
+    public static JAXBElement<CVType> aCV(String codeSystem, String code, String displayName) {
+        CVType cv = new CVType();
+        cv.setCodeSystem(codeSystem);
+        cv.setCode(code);
+        cv.setDisplayName(displayName);
+        return new JAXBElement<>(new QName("urn:riv:clinicalprocess:healthcond:certificate:types:2", "cv"), CVType.class, null, cv);
+    }
+
+    public static SvarBuilder aSvar(String id) {
+        return new SvarBuilder(id);
+    }
+
+    public static class SvarBuilder {
+        private String id;
+        public List<Delsvar> delSvars = new ArrayList<>();
+
+        SvarBuilder(String id) {
+            this.id = id;
+        }
+
+        public Svar build() {
+            Svar svar = new Svar();
+            svar.setId(id);
+            svar.getDelsvar().addAll(delSvars);
+            return svar;
+        }
+
+        public SvarBuilder withDelsvar(String delsvarsId, Object content) {
+            Delsvar delsvar = new Delsvar();
+            delsvar.setId(delsvarsId);
+            delsvar.getContent().add(content);
+            delSvars.add(delsvar);
+            return this;
+        }
+    }
+}
