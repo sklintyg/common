9a73ad432fdc14ecc8ee8a7d10acb675dacf62cf
Per Beijer <per.beijer@usify.se>
2016-05-09 15:34:40 +0200

    INTYG-2032: Värden behöver inte sparas till attic för varje uppdatering.
    Det räcker att spara undan när fältet döljs.

--- web/src/main/resources/META-INF/resources/webjars/common/webcert/gui/formly/helper/attic.helper.js
+++ web/src/main/resources/META-INF/resources/webjars/common/webcert/gui/formly/helper/attic.helper.js
@@ -10,24 +10,9 @@ angular.module('common').factory('common.AtticHelper',
             },
 
             updateToAttic: function($scope, model, optionsKey) {
-                var atticWatch = $scope.$watch(
-                        function () {
-                            return ObjectHelper.deepGet(model, optionsKey);
-                        },
-                        function (newVal, oldVal) {
-                            if (newVal === oldVal) { return; }
-                            if (!ObjectHelper.isEmpty(newVal)) {
-                                model.updateToAttic(ObjectHelper.deepGet(model.properties, optionsKey));
-                            } else {
-                                model.clear(model.properties[optionsKey.split('.')[0]]);
-                            }
-                        }
-                );
-
                 $scope.$on('$destroy', function () {
                     model.updateToAttic(ObjectHelper.deepGet(model.properties, optionsKey));
                     model.clear(model.properties[optionsKey.split('.')[0]]);
-                    atticWatch();
                 });
             }
         };
