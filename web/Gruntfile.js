3214150681e6fcb1ca17c1d3ac4f03a8e7e73c2f
Jesper Holmberg <jesper.holmberg@callistaenterprise.se>
2016-05-24 09:44:15 +0200

    DEVENV: första version som bygger alla subprojekt

--- web/Gruntfile.js
+++ web/Gruntfile.js
@@ -33,7 +33,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-sass-lint');
 
     var SRC_DIR = 'src/main/resources/META-INF/resources/';
-    var DEST_DIR = 'target/classes/META-INF/resources/';
+    var DEST_DIR = 'build/resources/main/META-INF/resources/';
 
     var minaintyg = grunt.file.expand({cwd:SRC_DIR}, ['webjars/common/minaintyg/**/*.js', '!**/*.spec.js', '!**/module.js']).sort();
     grunt.file.write(DEST_DIR + 'webjars/common/minaintyg/module-deps.json', JSON.stringify(minaintyg.
