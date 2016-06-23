75a5c22d228337ba2b37c157ffa3791286bc6af7
Per Beijer <per.beijer@usify.se>
2016-06-23 11:27:04 +0200

    INTYG-2609: Fix efter review

--- web/src/main/resources/META-INF/resources/webjars/common/webcert/gui/formly/singleDate.formly.js
+++ web/src/main/resources/META-INF/resources/webjars/common/webcert/gui/formly/singleDate.formly.js
@@ -3,36 +3,7 @@ angular.module('common').run(function(formlyConfig) {
 
     formlyConfig.setType({
         name: 'singleDate',
-        templateUrl: '/web/webjars/common/webcert/gui/formly/singleDate.formly.html',
-        controller: ['$scope', 'common.DateUtilsService', 'common.dynamicLabelService', function($scope, dateUtils, dynamicLabelService) {
-
-            //$scope.$watch('model.' + $scope.options.key, function(newVal, oldVal) {
-            //    var formState = $scope.formState;
-            //    if (!formState[$scope.options.key]) {
-            //        formState[$scope.options.key] = {};
-            //    }
-            //    //if (newVal) {
-            //    //    formState[$scope.options.key].checked = true;
-            //    //} else {
-            //    //    formState[$scope.options.key].checked = false;
-            //    //}
-            //});
-
-            //$scope.$watch('formState.' + $scope.options.key + '.checked', function(newVal, oldVal) {
-            //    if (newVal) {
-            //        if (!$scope.model[$scope.options.key]) {
-            //            $scope.model[$scope.options.key] = dateUtils.todayAsYYYYMMDD();
-            //        }
-            //    } else if (oldVal !== undefined) {
-            //        // Clear date if check is unchecked
-            //        $scope.model[$scope.options.key] = undefined;
-            //    }
-            //});
-
-            $scope.getDynamicText = function(key) {
-                return dynamicLabelService.getProperty(key);
-            };
-        }]
+        templateUrl: '/web/webjars/common/webcert/gui/formly/singleDate.formly.html'
     });
 
 });
