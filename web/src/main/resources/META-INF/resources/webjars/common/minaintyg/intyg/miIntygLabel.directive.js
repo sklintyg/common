f1d3b1374fcc3836e0683332645f85c50126a4cd
Per Beijer <per.beijer@usify.se>
2016-08-09 17:19:15 +0200

    DEVENV: Bytt namn från wc till mi på Mina Intyg specifikt directive.

--- web/src/main/resources/META-INF/resources/webjars/common/minaintyg/intyg/miIntygLabel.directive.js
+++ web/src/main/resources/META-INF/resources/webjars/common/minaintyg/intyg/miIntygLabel.directive.js
@@ -17,7 +17,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-angular.module('common').directive('wcIntygLabel',
+angular.module('common').directive('miIntygLabel',
     ['$log', 'common.dynamicLabelService',
         function($log, dynamicLabelService) {
             'use strict';
@@ -26,24 +26,24 @@ angular.module('common').directive('wcIntygLabel',
                 restrict: 'A',
                 replace: true,
                 scope: {
-                    wcIntygLabel: '='
+                    miIntygLabel: '='
                 },
-                templateUrl: '/web/webjars/common/minaintyg/intyg/wcIntygLabel.directive.html',
+                templateUrl: '/web/webjars/common/minaintyg/intyg/miIntygLabel.directive.html',
                 link: function(scope, element, attrs) {
-                    if (typeof scope.wcIntygLabel !== 'string') {
-                        $log.debug('wcIntygLabel argument is not a string');
+                    if (typeof scope.miIntygLabel !== 'string') {
+                        $log.debug('miIntygLabel argument is not a string');
                         return;
                     }
-                    scope.h5Label = scope.wcIntygLabel + '.RBK';
+                    scope.h5Label = scope.miIntygLabel + '.RBK';
 
-                    if (scope.wcIntygLabel.substring(0, 4) === 'DFR_') {
-                        var questionIds = scope.wcIntygLabel.substring(4).split('.');
+                    if (scope.miIntygLabel.substring(0, 4) === 'DFR_') {
+                        var questionIds = scope.miIntygLabel.substring(4).split('.');
                         if (questionIds.length === 2 && questionIds[1] === '1') {
                             var frageId = 'FRG_' + questionIds[0];
                             scope.$on('dynamicLabels.updated', function() {
                                 if (dynamicLabelService.hasProperty(frageId + '.RBK')) {
                                     scope.h4Label = frageId + '.RBK';
-                                    if (!dynamicLabelService.hasProperty(scope.wcIntygLabel + '.RBK')) {
+                                    if (!dynamicLabelService.hasProperty(scope.miIntygLabel + '.RBK')) {
                                         scope.h5Label = null;
                                     }
                                 }
@@ -54,7 +54,7 @@ angular.module('common').directive('wcIntygLabel',
                             });
                         }
                     }
-                    else if (scope.wcIntygLabel.substring(0, 4) === 'FRG_') {
+                    else if (scope.miIntygLabel.substring(0, 4) === 'FRG_') {
                         scope.h4Label = scope.h5Label;
                         scope.h5Label = null;
                     }
