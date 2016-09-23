d322bb05115b6641d35a2dadd4ec5c0c18b9c5bd
Per Beijer <per.beijer@usify.se>
2016-09-23 10:53:49 +0200

    INTYG-2835: Minskat kodduplicering av hsaInfoMissing

--- web/src/main/resources/META-INF/resources/webjars/common/webcert/gui/formly/labelVardenhet.formly.js
+++ web/src/main/resources/META-INF/resources/webjars/common/webcert/gui/formly/labelVardenhet.formly.js
@@ -4,34 +4,9 @@ angular.module('common').run(function(formlyConfig) {
     formlyConfig.setType({
         name: 'label-vardenhet',
         templateUrl: '/web/webjars/common/webcert/gui/formly/labelVardenhet.formly.html',
-        controller: function($scope) {
-            $scope.$on('intyg.loaded', function() {
-
-                // check if all info is available from HSA. If not, display the info message that someone needs to update it
-                var vardenhetData = $scope.to.userModel.user.valdVardenhet;
-                if (vardenhetData.mottagningar !== undefined) {
-                    for (var enhetIndex = 0; enhetIndex < vardenhetData.mottagningar.length; enhetIndex++) {
-                        if (vardenhetData.mottagningar[enhetIndex].id === $scope.model.grundData.skapadAv.vardenhet.enhetsid) {
-                            vardenhetData = vardenhetData.mottagningar[enhetIndex];
-                            break;
-                        }
-                    }
-                }
-
-                // check if all info is available from HSA. If not, display the info message that someone needs to update it
-                $scope.hsaInfoMissing = false;
-                if (vardenhetData.id !== $scope.model.grundData.skapadAv.vardenhet.enhetsid) {
-                    var properties = ['postadress', 'postnummer', 'postort', 'telefonnummer'];
-                    for(var i = 0; i < properties.length; i++) {
-                        var field = vardenhetData[properties[i]];
-                        if(field === undefined || field === '') {
-                            $scope.hsaInfoMissing = true;
-                            break;
-                        }
-                    }
-                }
-            });
-        }
+        controller: ['$scope', 'common.UtkastViewStateService', function($scope, UtkastViewStateService) {
+            $scope.commonViewState = UtkastViewStateService;
+        }]
     });
 
 });
