78f77e7ca2502ddeb5ec2fae38d29c459037e65c
Per Beijer <per.beijer@usify.se>
2016-12-20 10:22:58 +0100

    INTYG-3352: Fixat missing label vid utskrift av utkast orsakad av INTYG-3200

--- web/src/main/resources/META-INF/resources/webjars/common/webcert/gui/headers/wcPrintHeader.directive.js
+++ web/src/main/resources/META-INF/resources/webjars/common/webcert/gui/headers/wcPrintHeader.directive.js
@@ -26,7 +26,7 @@ angular.module('common').directive('wcPrintHeader',
                 restrict: 'A',
                 replace: true,
                 scope: {
-                    titleId: '@',
+                    title: '@',
                     printMessageId: '@',
                     intygsId: '='
                 },
