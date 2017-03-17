9777a2ca912c45f3076ffa5e2261abee3fab2c5c
Magnus Ekstrand <magnus@ekstrand.net>
2017-03-17 09:59:35 +0100

    INTYG-3574: refactoring av direktiv så samma struktur upprätthålls

--- fk/lisjp/src/main/resources/META-INF/resources/webjars/lisjp/minaintyg/js/directives/categoryWrapper.directive.js
+++ fk/lisjp/src/main/resources/META-INF/resources/webjars/lisjp/minaintyg/js/directives/categoryWrapper.directive.js
@@ -17,7 +17,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-angular.module('common').directive('lisjpShowCategory',
+angular.module('lisjp').directive('categoryWrapper',
     function() {
         'use strict';
 
