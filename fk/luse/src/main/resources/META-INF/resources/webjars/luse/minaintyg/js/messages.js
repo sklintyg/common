c7fb7659cbe30ca61ba783e84071e6d196f894c0
Magnus Ekstrand <magnus@ekstrand.net>
2017-06-01 13:37:44 +0200

    DEVENV: Tog bort LUSE message-properties som inte längre används

--- fk/luse/src/main/resources/META-INF/resources/webjars/luse/minaintyg/js/messages.js
+++ fk/luse/src/main/resources/META-INF/resources/webjars/luse/minaintyg/js/messages.js
@@ -23,18 +23,6 @@ angular.module('luse').constant('luse.messages', {
         'luse.button.send.certificate.title': 'Skicka detta intyg till en eller flera mottagare.',
 
         // Labels
-        'luse.label.latestevent': 'Senaste händelse',
-        'luse.label.latestevent.noevents': 'Inga händelser',
-
-        'luse.label.pagetitle': 'Granska och skicka intyg',
-        'luse.label.pageingress': 'Här visas hela ditt läkarintyg. Från den här sidan kan du skicka intyget elektroniskt till Försäkringskassan, du kan även spara intyget som en PDF på din dator.<br>',
-        'luse.label.pagedescription.sendcertificate.header': '<h2>Skicka intyg</h2>',
-        'luse.label.pagedescription.sendcertificate': 'Klicka på knappen Skicka för att skicka intyget elektroniskt till Försäkringskassan eller en annan mottagare.<br>',
-        'luse.label.pagedescription.error.in.certificate.header': '<h2>Fel i intyget?</h2>',
-        'luse.label.pagedescription.error.in.certificate': 'För medicinska frågor som rör ditt intyg ska du kontakta den som utfärdade ditt intyg, eller den mottagning du besökte när du fick ditt intyg utfärdat.',
-        'luse.label.pagedescription.archive.header': '<h2>Arkivera</h2>',
-        'luse.label.pagedescription.archive': 'För att arkivera intyget klickar du på knappen Arkivera.',
-
         'luse.label.showevents.false': '<span class="glyphicon glyphicon-chevron-up"></span> Visa färre händelser',
         'luse.label.showevents.true': '<span class="glyphicon glyphicon-chevron-down"></span> Visa alla händelser',
 
@@ -62,6 +50,5 @@ angular.module('luse').constant('luse.messages', {
         'luse.error.certnotfound': 'Intygen i Inkorgen kunde inte visas. Om felet kvarstår kan du kontakta <a href="/web/start/#/hjalp">support</a>. Om du inte kan komma åt intyget på Mina intyg just nu så kan du kontakta din läkare för att få en kopia.<br><br><a href="/web/start">Gå till Inkorgen och försök igen</a>'
     },
     'en': {
-        'luse.label.pagetitle': 'Show Certificate'
     }
 });
