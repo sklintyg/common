fa8cf093648e09772a7add0f665f9ac7feb9569c
Martin Hesslund <martin.hesslund@callistaenterprise.se>
2017-06-08 10:58:01 +0200

    INTYG-3840: Lagt in enkla värden i den nya strukturen

--- fk7263/src/main/resources/META-INF/resources/webjars/fk7263/minaintyg/js/controllers/ViewCertCtrl.js
+++ fk7263/src/main/resources/META-INF/resources/webjars/fk7263/minaintyg/js/controllers/ViewCertCtrl.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 Inera AB (http://www.inera.se)
+ * Copyright (C) 2017 Inera AB (http://www.inera.se)
  *
  * This file is part of sklintyg (https://github.com/sklintyg).
  *
@@ -18,40 +18,14 @@
  */
 
 angular.module('fk7263').controller('fk7263.ViewCertCtrl',
-    [ '$location', '$log', '$rootScope', '$stateParams', '$scope', 'common.IntygListService',
-        'common.IntygService', 'common.dialogService', 'common.messageService', 'common.moduleService', 'fk7263.ViewStateService', 'fk7263.customizeViewstate',
-        function($location, $log, $rootScope, $stateParams, $scope, IntygListService,
-            IntygService, dialogService, messageService, moduleService, ViewState, customizeViewstate) {
+    [ '$location', '$log', '$stateParams', '$scope', 'common.IntygService', 'fk7263.viewConfigFactory',
+        function($location, $log, $stateParams, $scope, certificateService, viewConfigFactory) {
             'use strict';
 
-            $rootScope.cert = {};
-
-            ViewState.reset();
-            ViewState.cert = {};
-            $scope.viewState = ViewState;
-
-            $scope.messageService = messageService;
-            $scope.moduleService = moduleService;
-
-            $scope.doneLoading = false;
-            $scope.visibleStatuses = [ 'SENT' ];
-            $scope.dialog = {
-                acceptprogressdone: true,
-                focus: false
-            };
-
-            // Navigation
+            $scope.cert = undefined;
 
             $scope.send = function() {
                 $location.path('/send/fk7263/' + $stateParams.certificateId + '/FKASSA');
-            };
-
-            $scope.showStatusHistory = function() {
-                $location.path('/fk7263/statushistory');
-            };
-
-            $scope.backToViewCertificate = function() {
-                $location.path('/fk7263/view/' + $stateParams.certificateId);
             };
 
             $scope.customizeCertificate = function() {
@@ -59,84 +33,13 @@ angular.module('fk7263').controller('fk7263.ViewCertCtrl',
                 $location.path('/fk7263/customizepdf/' + $stateParams.certificateId);
             };
 
-            // expose calculated static link for pdf download
-            $scope.downloadAsPdfLink = '/moduleapi/certificate/' + 'fk7263' + '/' + $stateParams.certificateId + '/pdf';
+            $scope.doneLoading = false;
 
-
-            // Archive dialog
-            var archiveDialog = {};
-
-            $scope.archiveSelected = function() {
-                var item = $scope.cert;
-                $log.debug('archive ' + item.id);
-                $scope.dialog.acceptprogressdone = false;
-                IntygListService.archiveCertificate(item, function(fromServer, oldItem) {
-                    $log.debug('statusUpdate callback:' + fromServer);
-                    if (fromServer !== null) {
-                        // Better way to update the object?
-                        oldItem.archived = fromServer.archived;
-                        oldItem.status = fromServer.status;
-                        oldItem.selected = false;
-                        archiveDialog.close();
-                        $scope.dialog.acceptprogressdone = true;
-                        $location.path('#/start');
-                    } else {
-                        // show error view
-                        $location.path('/fk7263/fel/couldnotarchivecert');
-                    }
-                });
-            };
-
-            $scope.certToArchive = {};
-
-            $scope.openArchiveDialog = function(cert) {
-                $scope.certToArchive = cert;
-                $scope.dialog.focus = true;
-                archiveDialog = dialogService.showDialog($scope, {
-                    dialogId: 'archive-confirmation-dialog',
-                    titleId: 'inbox.archivemodal.header',
-                    bodyTextId: 'inbox.archivemodal.text',
-                    button1click: function() {
-                        $log.debug('archive');
-                        $scope.archiveSelected();
-                    },
-                    button1id: 'archive-button',
-                    button1text: 'button.archive',
-                    autoClose: false
-                });
-            };
-
-
-            // Load certificate
-
-            $scope.userVisibleStatusFilter = function(status) {
-                for (var i = 0; i < $scope.visibleStatuses.length; i++) {
-                    if (status.type === $scope.visibleStatuses[i]) {
-                        return true;
-                    }
-                }
-                return false;
-            };
-
-            $scope.filterStatuses = function(statuses) {
-                var result = [];
-                if (!angular.isObject(statuses)) {
-                    return result;
-                }
-                for (var i = 0; i < statuses.length; i++) {
-                    if ($scope.userVisibleStatusFilter(statuses[i])) {
-                        result.push(statuses[i]);
-                    }
-                }
-                return result;
-            };
-
-            IntygService.getCertificate('fk7263', $stateParams.certificateId, function(result) {
+            certificateService.getCertificate('fk7263', $stateParams.certificateId, function(result) {
                 $scope.doneLoading = true;
                 if (result !== null) {
-                    ViewState.cert = result.utlatande;
-                    ViewState.cert.filteredStatuses = $scope.filterStatuses(result.meta.statuses);
-                    $rootScope.cert = ViewState.cert;
+                    $scope.cert = result.utlatande;
+                    $scope.certMeta = result.meta;
                 } else {
                     // show error view
                     $location.path('/fk7263/visafel/certnotfound');
@@ -147,4 +50,5 @@ angular.module('fk7263').controller('fk7263.ViewCertCtrl',
             });
 
             $scope.pagefocus = true;
+            $scope.uvConfig = viewConfigFactory.getViewConfig();
         }]);
---
+++ fk7263/src/main/resources/META-INF/resources/webjars/fk7263/minaintyg/js/controllers/fk7263ViewConfig.factory.js
@@ -0,0 +1,165 @@
+/*
+ * Copyright (C) 2017 Inera AB (http://www.inera.se)
+ *
+ * This file is part of sklintyg (https://github.com/sklintyg).
+ *
+ * sklintyg is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * sklintyg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+angular.module('fk7263').factory('fk7263.viewConfigFactory', [ function() {
+    'use strict';
+
+    var viewConfig = [
+
+        {
+            type: 'uv-kategori',
+            labelKey: 'fk7263.label.smittskydd',
+            components: [ {
+                type: 'uv-fraga',
+                components: [ {
+                    type: 'uv-boolean-value',
+                    modelProp: 'avstangningSmittskydd'
+                } ]
+            } ]
+        },
+
+        {
+            type: 'uv-kategori',
+            labelKey: 'fk7263.label.diagnosis',
+            components: [ {
+                type: 'uv-fraga',
+                labelKey: 'fk7263.label.diagnosisCode',
+                components: [ {
+                    type: 'uv-simple-value',
+                    modelProp: 'diagnosKod'
+                } ]
+            } ]
+        },
+        {
+            type: 'uv-kategori',
+            labelKey: 'fk7263.label.progressofdesease',
+            components: [ {
+                type: 'uv-fraga',
+                components: [ {
+                    type: 'uv-simple-value',
+                    modelProp: 'sjukdomsforlopp'
+                } ]
+            } ]
+        },
+        {
+            type: 'uv-kategori',
+            labelKey: 'fk7263.label.disabilities',
+            components: [ {
+                type: 'uv-fraga',
+                components: [ {
+                    type: 'uv-simple-value',
+                    modelProp: 'funktionsnedsattning'
+                } ]
+            } ]
+        },
+        {
+            type: 'uv-kategori',
+            labelKey: 'fk7263.label.limitation',
+            components: [ {
+                type: 'uv-fraga',
+                components: [ {
+                    type: 'uv-simple-value',
+                    modelProp: 'aktivitetsbegransning'
+                } ]
+            } ]
+        },
+        {
+            type: 'uv-kategori',
+            labelKey: 'fk7263.label.patientworkcapacityjudgement',
+            components: [ {
+                type: 'uv-fraga',
+                components: [ {
+                    type: 'uv-simple-value',
+                    modelProp: 'arbetsformagaPrognos'
+                } ]
+            } ]
+        },
+
+        {
+            type: 'uv-kategori',
+            labelKey: 'fk7263.label.prognosis',
+            components: [ {
+                type: 'uv-fraga',
+                components: [ {
+                    type: 'uv-simple-value',
+                    modelProp: 'prognosBedomning'
+                } ]
+            } ]
+        },
+        {
+            type: 'uv-kategori',
+            labelKey: 'fk7263.label.othertransport',
+            components: [ {
+                type: 'uv-fraga',
+                components: [ {
+                    /*<span ng-show="cert.ressattTillArbeteAktuellt">
+                     <span id="resaTillArbetet-yes" message key="fk7263.label.yes"></span>
+                     </span>
+                     <span ng-show="cert.ressattTillArbeteEjAktuellt">
+                     <span id="resaTillArbetet-no" message key="fk7263.label.no"></span>
+                     </span>*/
+                } ]
+            } ]
+        },
+
+        {
+            type: 'uv-kategori',
+            labelKey: 'fk7263.label.fkcontact',
+            components: [ {
+                type: 'uv-fraga',
+                components: [ {
+                    type: 'uv-boolean-value',
+                    modelProp: 'kontaktMedFk'
+                } ]
+            } ]
+        },
+        {
+            type: 'uv-kategori',
+            labelKey: 'fk7263.label.otherinformation',
+            components: [ {
+                type: 'uv-fraga',
+                components: [ {
+                    type: 'uv-simple-value',
+                    modelProp: 'kommentar'
+                } ]
+            } ]
+        },
+        {
+            type: 'uv-kategori',
+            labelKey: 'fk7263.label.workcodes',
+            components: [ {
+                type: 'uv-fraga',
+                components: [ {
+                    type: 'uv-simple-value',
+                    modelProp: 'forskrivarkodOchArbetsplatskod'
+                } ]
+            } ]
+        },
+        {
+            type: 'uv-skapad-av',
+            modelProp: 'grundData.skapadAv'
+        }
+    ];
+
+    return {
+        getViewConfig: function() {
+            return angular.copy(viewConfig);
+        }
+    };
+} ]);
