1e8797bf5ecd94318519159b83a76d80ee7805a4
Martin Hesslund <martin.hesslund@callistaenterprise.se>
2017-06-08 11:43:12 +0200

    DEVENV: Förbättrat stabiliteten på testet

--- web/src/main/resources/META-INF/resources/webjars/common/minaintyg/gui/dynamiclink/dynamiclink.directive.spec.js
+++ web/src/main/resources/META-INF/resources/webjars/common/minaintyg/gui/dynamiclink/dynamiclink.directive.spec.js
@@ -4,6 +4,7 @@ describe('dynamiclink', function() {
     beforeEach(angular.mock.module('common'));
 
     var $scope;
+    var $compile;
     var _links = {
         'somekey': {
             'text': 'real text',
@@ -15,13 +16,14 @@ describe('dynamiclink', function() {
 
     // Create a <p> with a dynamic link to test the validation directive on.
     beforeEach(angular.mock.inject(
-        ['$compile', '$rootScope', 'common.dynamicLinkService', function($compile, $rootScope, dynamicLinkService) {
+        ['$compile', '$rootScope', 'common.dynamicLinkService', function(_$compile_, $rootScope, dynamicLinkService) {
             dynamicLinkService.addLinks(_links);
             $scope = $rootScope.$new();
+            $compile = _$compile_;
         }]));
 
 
-    it('Should print an anchor when key exists', function($compile) {
+    it('Should print an anchor when key exists', function() {
         var el = angular.element('<p>This is text with <span dynamiclink key="somekey"></span></p>');
         $compile(el)($scope);
         $scope.$digest();
@@ -32,7 +34,7 @@ describe('dynamiclink', function() {
         expect(el.html()).toContain('target="_blank"');
     });
 
-    it('Should print a warning anchor when key does not exist', function($compile) {
+    it('Should print a warning anchor when key does not exist', function() {
         var el = angular.element('<p>This is text with <span dynamiclink key="otherkey"></span></p>');
         $compile(el)($scope);
         $scope.$digest();
