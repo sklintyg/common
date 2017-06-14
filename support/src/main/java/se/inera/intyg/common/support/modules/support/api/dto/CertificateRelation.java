a2062b0e4162650b82ff814dd22c02079e8435b4
Erik Lupander <erik.lupander@callistaenterprise.se>
2017-06-14 14:44:18 +0200

    INTYG-3773: Korrigering efter code review

--- support/src/main/java/se/inera/intyg/common/support/modules/support/api/dto/CertificateRelation.java
+++ support/src/main/java/se/inera/intyg/common/support/modules/support/api/dto/CertificateRelation.java
@@ -3,6 +3,7 @@ package se.inera.intyg.common.support.modules.support.api.dto;
 import se.inera.intyg.common.support.common.enumerations.RelationKod;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 /**
  * Reusable generic representation of a Relation between two intyg. While an instance of this class usually exists
@@ -11,7 +12,7 @@ import java.time.LocalDateTime;
  *
  * CERT B <- from <- RELATION (REPLACES) -> to -> CERT A.
  *
- * An instance of this class is identified by franIntygsId, tillIntygsId and relationKod.
+ * An instance of this class is identified by fromIntygsId, toIntygsId and relationKod.
  *
  * Applications may subclass this class in order to add application-specific information about relations such
  * as UtkastState in Webcert.
@@ -20,35 +21,35 @@ import java.time.LocalDateTime;
  */
 public class CertificateRelation {
 
-    private String franIntygsId;
-    private String tillIntygsId;
+    private String fromIntygsId;
+    private String toIntygsId;
     private RelationKod relationKod;
     private LocalDateTime skapad;
 
     public CertificateRelation() {
     }
 
-    public CertificateRelation(String franIntygsId, String tillIntygsId, RelationKod relationKod, LocalDateTime skapad) {
-        this.franIntygsId = franIntygsId;
-        this.tillIntygsId = tillIntygsId;
+    public CertificateRelation(String fromIntygsId, String toIntygsId, RelationKod relationKod, LocalDateTime skapad) {
+        this.fromIntygsId = fromIntygsId;
+        this.toIntygsId = toIntygsId;
         this.relationKod = relationKod;
         this.skapad = skapad;
     }
 
-    public String getFranIntygsId() {
-        return franIntygsId;
+    public String getFromIntygsId() {
+        return fromIntygsId;
     }
 
-    public void setFranIntygsId(String franIntygsId) {
-        this.franIntygsId = franIntygsId;
+    public void setFromIntygsId(String fromIntygsId) {
+        this.fromIntygsId = fromIntygsId;
     }
 
-    public String getTillIntygsId() {
-        return tillIntygsId;
+    public String getToIntygsId() {
+        return toIntygsId;
     }
 
-    public void setTillIntygsId(String tillIntygsId) {
-        this.tillIntygsId = tillIntygsId;
+    public void setToIntygsId(String toIntygsId) {
+        this.toIntygsId = toIntygsId;
     }
 
     public RelationKod getRelationKod() {
@@ -78,22 +79,17 @@ public class CertificateRelation {
 
         CertificateRelation that = (CertificateRelation) o;
 
-        if (!franIntygsId.equals(that.franIntygsId)) {
+        if (!fromIntygsId.equals(that.fromIntygsId)) {
             return false;
         }
-        if (!tillIntygsId.equals(that.tillIntygsId)) {
+        if (!toIntygsId.equals(that.toIntygsId)) {
             return false;
         }
         return relationKod == that.relationKod;
     }
 
-    // CHECKSTYLE:OFF MagicNumber
     @Override
     public int hashCode() {
-        int result = franIntygsId.hashCode();
-        result = 31 * result + tillIntygsId.hashCode();
-        result = 31 * result + relationKod.hashCode();
-        return result;
+        return Objects.hash(fromIntygsId, toIntygsId, relationKod);
     }
-    // CHECKSTYLE:ON MagicNumber
 }
