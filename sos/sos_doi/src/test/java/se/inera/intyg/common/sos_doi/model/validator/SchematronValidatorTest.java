260b8bd8042994774627b5a261de1aa20b21108c
Karl Johan Andreasson <kalle.kaan@gmail.com>
2017-08-04 15:52:54 +0200

    DEVENV: Lägger till grund för schematron-test DB/DOI

---
+++ sos/sos_doi/src/test/java/se/inera/intyg/common/sos_doi/model/validator/SchematronValidatorTest.java
@@ -0,0 +1,23 @@
+package se.inera.intyg.common.sos_doi.model.validator;
+
+import static com.google.common.io.Resources.getResource;
+import static org.junit.Assert.assertTrue;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
+
+import se.inera.intyg.common.support.modules.support.api.dto.ValidateXmlResponse;
+import se.inera.intyg.common.support.validate.RegisterCertificateValidator;
+import se.inera.intyg.common.support.validate.XmlValidator;
+
+public class SchematronValidatorTest {
+    private static final RegisterCertificateValidator VALIDATOR = new RegisterCertificateValidator("doi.sch");
+
+    // @Test
+    public void validXmlPassesTest() throws Exception {
+        String inputXml = Resources.toString(getResource("doi.xml"), Charsets.UTF_8);
+        ValidateXmlResponse response = XmlValidator.validate(VALIDATOR, inputXml);
+        assertTrue(response.getValidationErrors().isEmpty());
+    }
+
+}
