607c12f14b64d720f0de56c6ba83eacf4c116939
Marcus Cedergren <marcus.cedergren@callistaenterprise.se>
2017-08-18 09:21:49 +0200

    INTYG-4308: Rubriker i underlagstabellen för signerat luse, luae_na och luae_fs.

--- fk/luse/src/main/resources/META-INF/resources/webjars/luse/app-shared/luseViewConfigFactory.js
+++ fk/luse/src/main/resources/META-INF/resources/webjars/luse/app-shared/luseViewConfigFactory.js
@@ -72,7 +72,7 @@ angular.module('luse').factory('luse.viewConfigFactory', [
                     components: [ {
                         type: 'uv-table',
                         contentUrl: 'utlatande',
-                        headers: ['', '', 'DFR_4.3.RBK'], // labels for th cells
+                        headers: ['FRG_4.RBK', 'Datum', 'DFR_4.3.RBK'], // labels for th cells
                         valueProps: ['KV_FKMU_0005.{typ}.RBK', 'datum', 'hamtasFran'], // {typ} refers to underlag.typ values
                         modelProp: 'underlag'
                     } ]
