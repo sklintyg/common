13026e491c0cae33050403a87fcf6afb059cfbaf
Benny Samuelsson <benny@solidcore.se>
2017-08-25 16:47:40 +0200

    INTYG-4220: utkastmodell doi

--- sos/doi/src/main/resources/META-INF/resources/webjars/doi/webcert/views/utkast/utkast.model.js
+++ sos/doi/src/main/resources/META-INF/resources/webjars/doi/webcert/views/utkast/utkast.model.js
@@ -23,62 +23,6 @@ angular.module('doi').factory('doi.Domain.IntygModel',
         function(GrundData, DraftModel, ModelAttr, BaseAtticModel, ModelTransform, ObjectHelper) {
             'use strict';
 
-            var sjukskrivningFromTransform = function(sjukskrivningArray) {
-
-                var resultObject = {
-                    HELT_NEDSATT: {
-                        period: {
-                            from: '',
-                            tom: ''
-                        }
-                    },
-                    TRE_FJARDEDEL: {
-                        period: {
-                            from: '',
-                            tom: ''
-                        }
-                    },
-                    HALFTEN: {
-                        period: {
-                            from: '',
-                            tom: ''
-                        }
-                    },
-                    EN_FJARDEDEL: {
-                        period: {
-                            from: '',
-                            tom: ''
-                        }
-                    }
-                };
-
-                for(var i = 0; i < sjukskrivningArray.length; i++){
-                    resultObject[sjukskrivningArray[i].sjukskrivningsgrad] = {
-                        period: sjukskrivningArray[i].period
-                    };
-                }
-
-                return resultObject;
-            };
-
-            var sjukskrivningToTransform = function(sjukskrivningObject) {
-
-                var resultArray = [];
-                angular.forEach(sjukskrivningObject, function(value, key) {
-                    if(!ObjectHelper.isEmpty(value.period.from) || !ObjectHelper.isEmpty(value.period.tom)) {
-                        resultArray.push({
-                            sjukskrivningsgrad: key,
-                            period: {
-                                from: value.period.from,
-                                tom: value.period.tom
-                            }
-                        });
-                    }
-                }, sjukskrivningObject);
-
-                return resultArray;
-            };
-
             var DoiModel = BaseAtticModel._extend({
                 init: function init() {
                     var grundData = GrundData.build();
@@ -88,98 +32,27 @@ angular.module('doi').factory('doi.Domain.IntygModel',
                         'textVersion': undefined,
                         'grundData': grundData,
 
-                        // Kategori 10 Smittbärarpenning
-                        'avstangningSmittskydd': new ModelAttr( 'avstangningSmittskydd', { defaultValue : false }),
-
-                        // Kategori 1 Grund för medicinskt underlag
-                        'undersokningAvPatienten': undefined,
-                        'telefonkontaktMedPatienten': undefined,
-                        'journaluppgifter': undefined,
-                        'annatGrundForMU': undefined,
-                        'annatGrundForMUBeskrivning': undefined,
-                        'motiveringTillInteBaseratPaUndersokning':undefined,
-
-                        // Kategori 2 sysselsättning
-                        'sysselsattning': new ModelAttr('sysselsattning', {
-                            toTransform: ModelTransform.enumToTransform,
-                            fromTransform: ModelTransform.enumFromTransform,
-                            defaultValue: {}
-                        }),
-                        'nuvarandeArbete' : undefined,
-
-                        // Kategori 3 diagnos
-                        'diagnoser':new ModelAttr('diagnoser', {
-                            fromTransform: ModelTransform.diagnosFromTransform,
-                            toTransform: ModelTransform.diagnosToTransform
-                        }),
-
-                        // Kategori 4 Sjukdomens konsekvenser
-                        'funktionsnedsattning': undefined,
-                        'aktivitetsbegransning': undefined,
-
-                        // Kategori 5 Medicinska behandlingar / åtgärder
-                        'pagaendeBehandling': undefined,
-                        'planeradBehandling': undefined,
-
-                        // Kategory 6 Bedömning
-                        'sjukskrivningar': new ModelAttr('sjukskrivningar', {
-                            defaultValue : {
-                                HELT_NEDSATT: {
-                                    period: {
-                                        from: '',
-                                        tom: ''
-                                    }
-                                },
-                                TRE_FJARDEDEL: {
-                                    period: {
-                                        from: '',
-                                        tom: ''
-                                    }
-                                },
-                                HALFTEN: {
-                                    period: {
-                                        from: '',
-                                        tom: ''
-                                    }
-                                },
-                                EN_FJARDEDEL: {
-                                    period: {
-                                        from: '',
-                                        tom: ''
-                                    }
-                                }
-                            },
-                            fromTransform: sjukskrivningFromTransform,
-                            toTransform: sjukskrivningToTransform
-                        }),
-                        'motiveringTillTidigtStartdatumForSjukskrivning': undefined,
-                        'motiveringTillForLangSjukskrivning': undefined,
-                        'forsakringsmedicinsktBeslutsstod': undefined,
-                        'arbetstidsforlaggning': undefined,
-                        'arbetstidsforlaggningMotivering': undefined,
-                        'arbetsresor': undefined,
-                        'prognos': {
-                            'typ': undefined,
-                            'dagarTillArbete': undefined
-                        },
-
-                        // Kategori 7 Åtgärder
-                        'arbetslivsinriktadeAtgarder': new ModelAttr('arbetslivsinriktadeAtgarder', {
-                            toTransform: ModelTransform.enumToTransform,
-                            fromTransform: ModelTransform.enumFromTransform,
-                            defaultValue: {}
-                        }),
-                        'arbetslivsinriktadeAtgarderBeskrivning': undefined,
-
-                        // Kategori 8 Övrigt
-                        'ovrigt': undefined,
-
-                        // Kategori 9 Kontakt
-                        'kontaktMedFk': new ModelAttr( 'kontaktMedFk', { defaultValue : false }),
-                        'anledningTillKontakt': undefined,
-
-                        // Kategori 9999 Tilläggsfrågor
-                        'tillaggsfragor': new ModelAttr( 'tillaggsfragor', { defaultValue : [] })
+                        'identitetStyrkt': undefined,
+                        'dodsdatumSakert': undefined,
+                        'dodsdatum': undefined,
+                        'antraffatDodDatum': undefined,
+                        'dodsplatsKommun': undefined,
+                        'dodsplatsBoende': undefined,
+                        'barn': undefined,
+                        'land': undefined,
+                        'dodsorsak': undefined,
+                        'dodsorsakDatum': undefined,
+                        'dodsorsakSpecifikation': undefined,
+                        'foljd': undefined,
+                        'bidragandeSjukdomar': undefined,
+                        'operation': undefined,
+                        'operationDatum': undefined,
+                        'operationAnledning': undefined,
+                        'forgiftning': undefined,
+                        'forgiftningOrsak': undefined,
+                        'forgiftningDatum': undefined,
+                        'forgiftningUppkommelse': undefined,
+                        'grunder': undefined
                     });
                 },
                 update: function update(content, parent) {
@@ -188,7 +61,6 @@ angular.module('doi').factory('doi.Domain.IntygModel',
                     }
                     update._super.call(this, content);
                 }
-
             }, {
                 build : function(){
                     return new DraftModel(new DoiModel());
