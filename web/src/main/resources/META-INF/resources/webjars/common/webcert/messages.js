2f839d2517bd6b4c501a3bc1ac0f17fe593c887b
Erik Lupander <erik.lupander@callistaenterprise.se>
2017-09-18 11:32:58 +0200

    INTYG-4486: Lade till common.error.pu_problem

--- web/src/main/resources/META-INF/resources/webjars/common/webcert/messages.js
+++ web/src/main/resources/META-INF/resources/webjars/common/webcert/messages.js
@@ -409,6 +409,8 @@ angular.module('common').constant('common.messages', {
 
         // PU down, cannot print draft
         'common.error_could_not_print_draft_no_pu': '<strong>Det går inte att skriva ut utkastet!</strong><br><br>På grund av tekniskt fel går det inte att skriva ut utkastet. Försök igen om en liten stund. Om problemet kvarstår, kontakta i första hand din lokala IT-avdelning och i andra hand <LINK:ineraKundserviceAnmalFel>.',
+        // Generic PU-error.
+        'common.error.pu_problem': 'Den nationella personuppgiftstjänsten svarar inte. Åtgärden kan inte genomföras eftersom den kräver att personuppgifter kan hämtas från personuppgiftsregistret.<br>Om problemet kvarstår, kontakta i första hand din lokala IT-avdelning och i andra hand <LINK:ineraKundserviceAnmalFel>.',
 
         // PU down, cannot send
         'fk7263.error_could_not_send_cert_no_pu': '<strong>Det går inte att skicka intyget!</strong><br><br>På grund av tekniskt fel kan du för tillfället inte skicka intyget, försök igen om en liten stund. Om problemet kvarstår, kontakta i första hand din lokala IT-avdelning och i andra hand <LINK:ineraKundserviceAnmalFel>.<br><br>Patienten kan själv gå in på minaintyg.se för att skriva ut och skicka intyget elektroniskt.',
