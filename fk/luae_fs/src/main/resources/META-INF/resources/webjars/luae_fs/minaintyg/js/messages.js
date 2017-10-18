b3da83588f58241ba2887ad38e5a22c5bfed1ea3
Erik Lupander <erik.lupander@callistaenterprise.se>
2017-10-18 11:50:39 +0200

    INTYG-3618: Fixade nya inledande texten per intygstyp om ersatt

--- fk/luae_fs/src/main/resources/META-INF/resources/webjars/luae_fs/minaintyg/js/messages.js
+++ fk/luae_fs/src/main/resources/META-INF/resources/webjars/luae_fs/minaintyg/js/messages.js
@@ -6,6 +6,7 @@ angular.module('luae_fs').constant('luae_fs.messages', {
 
         // Labels
         'luae_fs.label.pageingress': 'Här visas hela ditt läkarintyg. Från den här sidan kan du skicka intyget elektronisk, ladda ned intyget som en PDF och arkivera intyget. För medicinska frågor som rör ditt intyg ska du kontakta den som utfärdade ditt intyg, eller den mottagning du besökte när du fick ditt intyg utfärdat.',
+        'luae_fs.label.pageingress.ersatt': 'Här visas hela ditt ersatta läkarintyg. Klicka på länken i den gula inforutan för att komma till det nya intyget som detta blev ersatt med. Klicka på knappen Arkivera intyg för att flytta intyget till Arkiverade intyg. Du kan när som helst återställa intyget igen.',
 
         // Composite keys - used with dynamic prefix such as <intygstype> or suffixes <true/false> etc.
         // Be careful to investigate if they are used before removing them.
--- fk/luae_na/src/main/resources/META-INF/resources/webjars/luae_na/minaintyg/js/messages.js
+++ fk/luae_na/src/main/resources/META-INF/resources/webjars/luae_na/minaintyg/js/messages.js
@@ -4,6 +4,7 @@ angular.module('luae_na').constant('luae_na.messages', {
 
         // Labels
         'luae_na.label.pageingress': 'Här visas hela ditt läkarintyg. Från den här sidan kan du skicka intyget elektronisk, ladda ned intyget som en PDF och arkivera intyget. För medicinska frågor som rör ditt intyg ska du kontakta den som utfärdade ditt intyg, eller den mottagning du besökte när du fick ditt intyg utfärdat.',
+        'luae_na.label.pageingress.ersatt': 'Här visas hela ditt ersatta läkarintyg. Klicka på länken i den gula inforutan för att komma till det nya intyget som detta blev ersatt med. Klicka på knappen Arkivera intyg för att flytta intyget till Arkiverade intyg. Du kan när som helst återställa intyget igen.',
 
         // Composite keys - used with dynamic prefix such as <intygstype> or suffixes <true/false> etc.
         // Be careful to investigate if they are used before removing them.
