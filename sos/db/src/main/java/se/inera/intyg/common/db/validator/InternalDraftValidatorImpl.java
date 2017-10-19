f21f3dbf5a21011eb6ae0159068f575d163497b1
Karl Johan Andreasson <kalle.kaan@gmail.com>
2017-10-19 10:56:48 +0200

    DEVENV: Checkstyle-fix

--- sos/db/src/main/java/se/inera/intyg/common/db/validator/InternalDraftValidatorImpl.java
+++ sos/db/src/main/java/se/inera/intyg/common/db/validator/InternalDraftValidatorImpl.java
@@ -82,11 +82,11 @@ public class InternalDraftValidatorImpl implements InternalDraftValidator<DbUtla
                 ValidatorUtil
                         .addValidationError(validationMessages, "yttreUndersokning.undersokningDatum",
                                 ValidationMessageType.INVALID_FORMAT);
-            } else if (!utlatande.getUndersokningDatum().isReasonable() ||
-                    utlatande.getUndersokningDatum().asLocalDate().isAfter(LocalDate.now())) {
+            } else if (!utlatande.getUndersokningDatum().isReasonable() || utlatande.getUndersokningDatum().asLocalDate()
+                    .isAfter(LocalDate.now())) {
                 ValidatorUtil
-                        .addValidationError(validationMessages, "yttreUndersokning.undersokningDatum",
-                                ValidationMessageType.INVALID_FORMAT, "common.validation.date_out_of_range");
+                        .addValidationError(validationMessages, "yttreUndersokning.undersokningDatum", ValidationMessageType.INVALID_FORMAT,
+                                "common.validation.date_out_of_range");
             } else if (utlatande.getDodsdatum() != null && utlatande.getDodsdatum().isValidDate()
                     && utlatande.getUndersokningDatum().asLocalDate().isAfter(utlatande.getDodsdatum().asLocalDate())) {
                 ValidatorUtil
