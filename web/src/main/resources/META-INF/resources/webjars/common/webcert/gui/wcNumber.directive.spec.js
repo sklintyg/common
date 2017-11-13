dd8a7767e6c43aaf6e8fa217e783a90fafa5024c
Benny Samuelsson <benny@solidcore.se>
2017-11-13 16:17:00 +0100

    INTYG-4909: fdescribe.

--- web/src/main/resources/META-INF/resources/webjars/common/webcert/gui/wcNumber.directive.spec.js
+++ web/src/main/resources/META-INF/resources/webjars/common/webcert/gui/wcNumber.directive.spec.js
@@ -17,7 +17,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-fdescribe('wcNumber', function() {
+describe('wcNumber', function() {
     'use strict';
 
     beforeEach(angular.mock.module('common'));
