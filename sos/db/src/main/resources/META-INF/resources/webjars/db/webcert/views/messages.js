ca69f1af4cf681e7636df919f75f712c48799dff
Kebire Breitholtz <kebire.breitholtz@inera.se>
2017-11-23 09:59:08 +0100

    INTYG-5089: uppdaterat rätt text för när ett dödsbevis ska ersättas

--- sos/db/src/main/resources/META-INF/resources/webjars/db/webcert/views/messages.js
+++ sos/db/src/main/resources/META-INF/resources/webjars/db/webcert/views/messages.js
@@ -7,9 +7,9 @@ angular.module('db').constant('db.messages', {
         'db.validation.undersokningDatum.after.dodsdatum': 'Undersökningsdatum får inte infalla efter dödsdatum',
 
         'db.makulera.body.common-header': '<p>Ett dödsbevis som är inskickat på fel person kan makuleras. Genom att trycka på "Makulera" makulerar du dödsbeviset i Webcert, med detta kommer inte återkalla dödsbeviset hos Skatteverket.</p>Förutom att trycka på "Makulera" måste du omedelbart ta kontakt med Skatteverket så att felet kan rättas fort. Du tar kontakt med Skatteverket genom att ringa till Skatteupplysningen på telefon 0771-567 567 och ange "folkbokföring - dödsfall".',
-        'db.modal.ersatt.text': '<p>Senast skapade dödsorsaksintyg är det som gäller. Om du ersätter det tidigare dödsorsaksintyget och lämnar in det nya så blir det därför detta dödsorsaksintyg som gäller.</p>' +
-        '<p>Ett dödsorsaksintyg kan ersättas om det innehåller felaktiga uppgifter eller om ny information tillkommit efter att dödsorsaksintyget utfärdades. När ett dödsorsaksintyg ersätts med ett nytt, skapas ett utkast med samma information som i det ursprungliga dödsorsaksintyget. Uppgifterna i det nya utkastet går att ändra innan det signeras. Ett ersatt dödsorsaksintyg är endast tillgängligt att läsa och skriva ut. På det ersatta dödsorsaksintyget kommer en länk finnas till det nya dödsorsaksintyget.</p>' +
-        '<p><b>Notera</b>: Om intyget innehåller ett allvarligt fel, till exempel om intyget är utfärdat på fel patient ska du istället makulera intyget.</p>',
+        'db.modal.ersatt.text': '<p>Senast skapade dödsbevis är det som gäller. Om du ersätter det tidigare dödsbeviset och lämnar in det nya så blir det därför detta dödsbevis som gäller.</p>' +
+        '<p>Ett dödsbevis kan ersättas om det innehåller felaktiga uppgifter eller om ny information tillkommit efter att dödsbeviset utfärdades. När ett dödsbevis ersätts med ett nytt, skapas ett utkast med samma information som i det ursprungliga dödsbeviset. Uppgifterna i det nya utkastet går att ändra innan det signeras. Ett ersatt dödsbevis är endast tillgängligt att läsa och skriva ut. På det ersatta dödsbeviset kommer en länk finnas till det nya dödsbeviset.</p>' +
+        '<p><b>Notera</b>: Om dödsbeviset innehåller ett allvarligt fel, till exempel om dödsbeviset är utfärdat på fel patient ska du istället makulera dödsbeviset.</p>',
 
         'db.info.polisanmalan': 'Skriv även ut dödsbeviset och skicka det till polisen per post/fax.',
 
