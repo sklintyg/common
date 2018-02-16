7fe4213d51137bbeec55b5b2ad382927330b0553
Benny Samuelsson <benny@solidcore.se>
2018-02-16 10:56:23 +0100

    DEVENV: Higher timeout for lisjp.

--- fk/lisjp/karma-webcert.conf.js
+++ fk/lisjp/karma-webcert.conf.js
@@ -102,6 +102,7 @@ module.exports = function(config) {
 
         // If browser does not capture in given timeout [ms], kill it
         captureTimeout: 60000,
+        browserNoActivityTimeout: 60000,
 
         // Continuous Integration mode if true, it capture browsers, run tests and exit
         singleRun: false,
