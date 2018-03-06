e2d4c0a213898572101cf58ee2a257ee5525f247
Martin Hesslund <martin.hesslund@callistaenterprise.se>
2018-03-06 16:40:41 +0100

    INTYG-5623: Löst problem med hoppande textfält

--- web/src/main/resources/META-INF/resources/webjars/common/webcert/components/wcAutoExpand.directive.js
+++ web/src/main/resources/META-INF/resources/webjars/common/webcert/components/wcAutoExpand.directive.js
@@ -22,109 +22,71 @@
  * Used to let text areas auto size it's height.
  */
 
-angular.module('common').directive('autoExpand', ['$window', '$sniffer', function($window, $sniffer) {
+angular.module('common').directive('autoExpand', ['$window', '$interval', function($window, $interval) {
     'use strict';
 
     return  {
         restrict: 'A',
         require: '?ngModel',
         link: function (scope, element, attrs, ctrl) {
-
-            // Default settings
-            scope.attrs = {
-                rows: 2
-            };
-
-            // Merge defaults with user preferences
-            for(var i in scope.attrs){
-                if(attrs[i]){
-                    scope.attrs[i] = parseInt(attrs[i], 10);
-                }
-            }
-
             // Ensure the element is a textarea, browser is capable and autoexpand something else than false
-            if (element[0].nodeName !== 'TEXTAREA' || !$window.getComputedStyle || attrs.autoExpand === 'false') {
+            if (element[0].nodeName !== 'TEXTAREA' || attrs.autoExpand === 'false') {
                 return;
             }
 
+            var windowNode = angular.element($window);
             var node = element[0];
-            var rows = parseInt(scope.attrs.rows, 10);
-            var lineHeight = _getLineHeight(node, rows);
+            var jqueryNode = angular.element(node);
+            var offset = node.offsetHeight - node.clientHeight;
+            var defaultHeight = 0;
+            var changedHeight = false;
 
-            if (ctrl){
-                // view value changed from ngModelController - textarea content changed via javascript
-                // This is also called when viewValue changes by user interaction, but with debounce is now called much less aggressively.
-                scope.$watch(function(){
-                    return ctrl.$viewValue;
-                }, adjust);
+            function resizeTextarea() {
+                jqueryNode.css('height', 'auto');
+                var newHeight = node.scrollHeight;
+
+                if (newHeight > defaultHeight) {
+                    if (!changedHeight) {
+                        offset = node.offsetHeight - node.clientHeight;
+                        defaultHeight = node.clientHeight;
+                        changedHeight = true;
+                    }
+
+                    jqueryNode.css('height', newHeight + offset);
+                }
             }
 
-            // element became visible
-            scope.$watch(function(){
-                // element is visible if at least one of those values is not 0
-                return node.offsetHeight || node.offsetWidth;
-            }, function(newVal, oldVal){
-                if(newVal && !oldVal) {
-                    adjust();
+            function addWatches() {
+                if (ctrl) {
+                    scope.$watch(function() {
+                        return ctrl.$viewValue;
+                    }, function(newValue, oldValue) {
+                        if (newValue !== oldValue) {
+                            resizeTextarea();
+                        }
+                    });
                 }
-            });
-
-            // browser resize occurrence
-            angular.element($window).on('resize', adjust);
-
-            // initial adjust
-            adjust();
-
-            function adjust() {
-                if (isNaN(lineHeight)) {
-                    lineHeight = _getLineHeight(node);
-                }
-                if (!(node.offsetHeight || node.offsetWidth)) {
-                    return;
-                }
-                if (node.scrollHeight <= node.clientHeight) {
-                    node.style.height = '0px';
-                }
-                var h = node.scrollHeight + // actual height defined by content
-                    node.offsetHeight - // border size compensation
-                    node.clientHeight; //       -- || --
-                var isIE = $sniffer.msie || $sniffer.vendorPrefix && $sniffer.vendorPrefix.toLowerCase() === 'ms';
-                node.style.height = Math.max(h, lineHeight) +
-                    (isIE ? 1 : 0) + // ie quirk
-                    'px';
+                // browser resize occurrence
+                windowNode.on('resize', resizeTextarea);
             }
+
+            // Run until the textarea is visible
+            var stop = $interval(function() {
+                if (node.offsetHeight || node.offsetWidth) {
+                    resizeTextarea();
+
+                    $interval.cancel(stop);
+
+                    // add watches
+                    addWatches();
+                }
+            }, 500);
 
             function cleanUp() {
-                angular.element($window).off('resize', adjust);
+                windowNode.off('resize', resizeTextarea);
             }
 
             scope.$on('$destroy', cleanUp);
         }
     };
-
-    function _px2val(str) {
-        return +str.slice(0, -2);
-    }
-
-    function _getLineHeight(node, rows){
-        var lineHeightThreshold = 1.6;
-
-        var computedStyle = $window.getComputedStyle(node);
-        var lineHeightStyle = computedStyle.lineHeight;
-        var fontSizeStyle = computedStyle.fontSize;
-
-        var minHeight = _px2val(fontSizeStyle) * rows * lineHeightThreshold;
-
-        if (lineHeightStyle === 'normal') {
-            return minHeight;
-        } else {
-            var height = _px2val(lineHeightStyle);
-            return minHeight > height ? minHeight : height;
-        }
-    }
-
-    angular.element(document.head).append(
-        '<style>[autoheight]{overflow: hidden; resize: none; box-sizing: border-box;}</style>'
-    );
-
 }]);
