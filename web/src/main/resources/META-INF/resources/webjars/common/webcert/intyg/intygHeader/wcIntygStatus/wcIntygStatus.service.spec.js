78d17e915056d866970a87ba6e73d1bf055d72d7
Per Beijer <per.beijer@usify.se>
2018-03-07 15:32:15 +0100

    DEVENV: Återaktiverat alla test i common-web

--- web/src/main/resources/META-INF/resources/webjars/common/webcert/intyg/intygHeader/wcIntygStatus/wcIntygStatus.service.spec.js
+++ web/src/main/resources/META-INF/resources/webjars/common/webcert/intyg/intygHeader/wcIntygStatus/wcIntygStatus.service.spec.js
@@ -17,7 +17,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-fdescribe('wcIntygStatus', function() {
+describe('wcIntygStatus', function() {
     'use strict';
 
     var IntygStatusService;
