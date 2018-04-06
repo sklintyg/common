ede66cf6207b6a5be0bb65dffe06ee86e6cfcf5e
Benny Samuelsson <benny@solidcore.se>
2018-04-06 13:26:51 +0200

    INTYG-5514: jshint.

--- fk/lisjp/src/main/resources/META-INF/resources/webjars/lisjp/webcert/views/messages.js
+++ fk/lisjp/src/main/resources/META-INF/resources/webjars/lisjp/webcert/views/messages.js
@@ -17,6 +17,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 /* jshint maxlen: false */
+'use strict';
 
 angular.module('lisjp').constant('lisjp.messages', {
     'sv': {
