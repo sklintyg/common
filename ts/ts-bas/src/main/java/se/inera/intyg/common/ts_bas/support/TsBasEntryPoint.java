3a7fb7686d464de1ee2d453aa3819cdf035ee7b5
Erik Lupander <erik.lupander@callistaenterprise.se>
2018-11-21 17:46:50 +0100

    DEVENV: Checkstajl

--- ts/ts-bas/src/main/java/se/inera/intyg/common/ts_bas/support/TsBasEntryPoint.java
+++ ts/ts-bas/src/main/java/se/inera/intyg/common/ts_bas/support/TsBasEntryPoint.java
@@ -18,23 +18,14 @@
  */
 package se.inera.intyg.common.ts_bas.support;
 
-import static se.inera.intyg.common.support.modules.support.api.dto.PatientDetailResolveOrder.ResolveOrder.PARAMS;
-import static se.inera.intyg.common.support.modules.support.api.dto.PatientDetailResolveOrder.ResolveOrder.PARAMS_OR_PU;
-import static se.inera.intyg.common.support.modules.support.api.dto.PatientDetailResolveOrder.ResolveOrder.PU;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
 import se.inera.intyg.common.services.texts.model.IntygTexts;
 import se.inera.intyg.common.services.texts.repo.IntygTextsRepository;
 import se.inera.intyg.common.support.modules.support.ApplicationOrigin;
 import se.inera.intyg.common.support.modules.support.ModuleEntryPoint;
-import se.inera.intyg.common.support.modules.support.api.dto.PatientDetailResolveOrder;
-import se.inera.intyg.common.support.modules.support.api.dto.PatientDetailResolveOrder.ResolveOrder;
+
+import java.util.Optional;
 
 @Component("TsBasEntryPoint")
 public class TsBasEntryPoint implements ModuleEntryPoint {
