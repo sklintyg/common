9ae417048b3605366eaac97ad4429db0e544393d
Marcus Cedergren <marcus.cedergren@callistaenterprise.se>
2018-12-06 14:26:12 +0100

    INTYG-7787: Ny "Intyget är tillgängligt för patienten" dialogtext för AG1-14

--- skl/ag114/src/main/resources/META-INF/resources/webjars/ag114/webcert/views/messages.js
+++ skl/ag114/src/main/resources/META-INF/resources/webjars/ag114/webcert/views/messages.js
@@ -19,6 +19,11 @@
 /* jshint maxlen: false */
 angular.module('ag114').constant('ag114.messages', {
     'sv': {
+        //Note : these are compound message keys, overriding common messages,
+
+        /// IS-14c / Modal-02c
+        'ag114.modalbody.intygstatus.is-008': '<p>Intyget är tillgängligt för patienten i Mina intyg, som nås via <LINK:minaintyg></p>',
+
         'ag114.modal.ersatt.text':'<p>Ett intyg kan ersättas om det innehåller felaktiga uppgifter eller om ny information tillkommit efter att intyget utfärdades. När ett intyg ersätts med ett nytt, skapas ett utkast med samma information som i det ursprungliga intyget. Uppgifterna i det nya utkastet går att ändra innan det signeras. Invånaren kommer på Mina intyg, som nås via 1177.se, se att intyget är ersatt. Ett ersatt intyg är endast tillgängligt att läsa och skriva ut. På det ersatta intyget kommer en länk finnas till det nya intyget.</p>' +
             '<p><b>Notera</b>: Om intyget innehåller ett allvarligt fel, till exempel om intyget är utfärdat på fel patient bör du istället makulera intyget. Det kommer då inte vara tillgängligt för invånaren via Mina intyg.</p>',
 
