70db08db03836616807ffb243c81374945f29d23
Erik Lupander <erik.lupander@callistaenterprise.se>
2018-12-14 10:07:54 +0100

    DEVENV: Tog bort fdescribe

--- web/src/main/resources/META-INF/resources/webjars/common/webcert/components/patient/patient.service.spec.js
+++ web/src/main/resources/META-INF/resources/webjars/common/webcert/components/patient/patient.service.spec.js
@@ -17,7 +17,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-fdescribe('PatientService', function() {
+describe('PatientService', function() {
     'use strict';
 
     var PatientService;
