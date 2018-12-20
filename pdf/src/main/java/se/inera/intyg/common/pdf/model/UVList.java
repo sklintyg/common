dd06954d7171ffcd3024f437ecac480cbbbb8357
Martin Hesslund <martin.hesslund@callistaenterprise.se>
2018-12-20 14:39:45 +0100

    INTYG-7884: Rättat till index i uvList

--- pdf/src/main/java/se/inera/intyg/common/pdf/model/UVList.java
+++ pdf/src/main/java/se/inera/intyg/common/pdf/model/UVList.java
@@ -18,16 +18,16 @@
  */
 package se.inera.intyg.common.pdf.model;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import com.google.common.base.Strings;
 import com.itextpdf.layout.element.Div;
 import com.itextpdf.layout.element.ListItem;
 import com.itextpdf.layout.element.Paragraph;
 import jdk.nashorn.api.scripting.ScriptObjectMirror;
 import se.inera.intyg.common.pdf.renderer.UVRenderer;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * Renders a UVList. This is a complex component that can render values based both on pure
@@ -171,7 +171,7 @@ public class UVList extends UVComponent {
                 if (evaluatedValue instanceof Boolean) {
                     boolean evalBool = (boolean) evaluatedValue;
                     if (evalBool) {
-                        Object result = listKey.call(null, evaluatedValue, index++);
+                        Object result = listKey.call(null, evaluatedValue, index);
 
                         if (result != null) {
                             Integer i;
@@ -187,6 +187,9 @@ public class UVList extends UVComponent {
                             results.add(value);
                         }
                     }
+                    // Increment index even if the value wasn't shown
+                    index++;
+
                     // Skip null values, if no results will lead to EJ_ANGIVET
                 } else if (evaluatedValue != null) {
                     throw new IllegalStateException("UNHANDLED evaluated value type in uv-list: " + evaluatedValue.getClass().getName());
