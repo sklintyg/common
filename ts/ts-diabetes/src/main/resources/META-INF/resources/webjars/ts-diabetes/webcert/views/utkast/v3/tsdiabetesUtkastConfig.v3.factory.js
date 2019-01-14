f37294feee366988268608d4e44ade59c81aedf6
Carl Frendin <carl.frendin@inera.se>
2019-01-14 12:00:37 +0100

    BUILDENV: uppdaterade fler licensheaders

--- ts/ts-diabetes/src/main/resources/META-INF/resources/webjars/ts-diabetes/webcert/views/utkast/v3/tsdiabetesUtkastConfig.v3.factory.js
+++ ts/ts-diabetes/src/main/resources/META-INF/resources/webjars/ts-diabetes/webcert/views/utkast/v3/tsdiabetesUtkastConfig.v3.factory.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 Inera AB (http://www.inera.se)
+ * Copyright (C) 2019 Inera AB (http://www.inera.se)
  *
  * This file is part of sklintyg (https://github.com/sklintyg).
  *
@@ -16,7 +16,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 angular.module('ts-diabetes').factory('ts-diabetes.UtkastConfigFactory.v3',
     ['$log', '$timeout', 'common.ObjectHelper', 'common.DateUtilsService', 'common.ueFactoryTemplatesHelper', 'common.ueTSFactoryTemplatesHelper',
         function ($log, $timeout, ObjectHelper, DateUtils, ueFactoryTemplates, ueTSFactoryTemplates) {
