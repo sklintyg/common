283098f898cf2b04d6fc40989f33073195a5e2a5
Benny Samuelsson <benny@solidcore.se>
2019-02-25 10:48:31 +0100

    INTYG-8141: Fixade dubbel text på tsbas viewconfig

--- ts/ts-bas/src/main/resources/META-INF/resources/webjars/ts-bas/app-shared/tsbasViewConfig.v6.factory.js
+++ ts/ts-bas/src/main/resources/META-INF/resources/webjars/ts-bas/app-shared/tsbasViewConfig.v6.factory.js
@@ -31,7 +31,6 @@ angular.module('ts-bas').factory('ts-bas.viewConfigFactory.v6', [
                         labelKey: 'FRG_1.RBK',
                         components: [{
                             type: 'uv-del-fraga',
-                            labelKey: 'FRG_1.2.RBK',
                             components: [{
                                 type: 'uv-list',
                                 labelKey: 'KORKORT.{var}.RBK',
