46517217dc7e4c08e507f3e7ca75b1dbf1594638
Matias Wurschmidt-Wang <matias.wurschmidt@soprasteria.com>
2019-03-26 11:01:40 +0100

    INTYG-8329: Add red border for first row of diagnoses if no diagnoses was entered when user signs. This is fixed both for ueDiagnos(only required fix in ueDiagnosKod) and ueDiagnosAr.

--- web/src/main/resources/META-INF/resources/webjars/common/webcert/utkast/unified-edit/components/ueDiagnosAr/ueDiagnosAr.directive.js
+++ web/src/main/resources/META-INF/resources/webjars/common/webcert/utkast/unified-edit/components/ueDiagnosAr/ueDiagnosAr.directive.js
@@ -17,9 +17,11 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-angular.module('common').directive('ueDiagnosAr', [ '$log', '$timeout', 'common.DiagnosProxy', 'common.ObjectHelper',
-    'common.MonitoringLogService', 'common.UtkastValidationService', 'common.UtkastViewStateService', 'common.AtticHelper',
-    function($log, $timeout, diagnosProxy, ObjectHelper, monitoringService, UtkastValidationService, UtkastViewState, AtticHelper) {
+angular.module('common').directive('ueDiagnosAr', ['$log', '$timeout', 'common.DiagnosProxy', 'common.ObjectHelper',
+    'common.MonitoringLogService', 'common.UtkastValidationService', 'common.UtkastViewStateService',
+    'common.AtticHelper',
+    function($log, $timeout, diagnosProxy, ObjectHelper, monitoringService, UtkastValidationService, UtkastViewState,
+        AtticHelper) {
         'use strict';
 
         return {
@@ -39,19 +41,19 @@ angular.module('common').directive('ueDiagnosAr', [ '$log', '$timeout', 'common.
                 $scope.validation = UtkastViewState.validation;
 
                 // Add listeners to diagnosKod changes
-                $scope.$watch('model.'+ $scope.config.modelProp+'[0].diagnosKod', function(newVal) {
+                $scope.$watch('model.' + $scope.config.modelProp + '[0].diagnosKod', function(newVal) {
                     clearDiagnosArtal(0, newVal);
                 });
 
-                $scope.$watch('model.'+ $scope.config.modelProp+'[1].diagnosKod', function(newVal) {
+                $scope.$watch('model.' + $scope.config.modelProp + '[1].diagnosKod', function(newVal) {
                     clearDiagnosArtal(1, newVal);
                 });
 
-                $scope.$watch('model.'+ $scope.config.modelProp+'[2].diagnosKod', function(newVal) {
+                $scope.$watch('model.' + $scope.config.modelProp + '[2].diagnosKod', function(newVal) {
                     clearDiagnosArtal(2, newVal);
                 });
 
-                $scope.$watch('model.'+ $scope.config.modelProp+'[3].diagnosKod', function(newVal) {
+                $scope.$watch('model.' + $scope.config.modelProp + '[3].diagnosKod', function(newVal) {
                     clearDiagnosArtal(3, newVal);
                 });
 
@@ -64,17 +66,35 @@ angular.module('common').directive('ueDiagnosAr', [ '$log', '$timeout', 'common.
                 // Split validations on different rows
                 $scope.$watch('validation.messagesByField', function() {
                     $scope.diagnosValidations = [];
+                    var addValidationForYear = false;
                     angular.forEach($scope.validation.messagesByField,
                         function(validations, key) {
-                            if (key.substr(0, $scope.config.modelProp.length + 1) === $scope.config.modelProp.toLowerCase() + '[') {
+                            if (key.substr(0, $scope.config.modelProp.length + 1) ===
+                                $scope.config.modelProp.toLowerCase() + '[') {
                                 var index = parseInt(key.substr($scope.config.modelProp.length + 1, 1), 10);
-                                if(typeof $scope.diagnosValidations[index] === 'undefined') {
+                                if (typeof $scope.diagnosValidations[index] === 'undefined') {
                                     $scope.diagnosValidations[index] = [];
                                 }
                                 $scope.diagnosValidations[index].push(validations[0]);
+                            } else if (key === $scope.config.modelProp.toLowerCase()) {
+                                addValidationForYear = true;
                             }
                         });
+
+                    // Add validation.messageByField for diagnosArtal on first index to make the ueYearPicker present a red border.
+                    // Only do this if first row is missing a diagnose.
+                    if (addValidationForYear &&
+                        ObjectHelper.isEmpty($scope.model[$scope.config.modelProp][0].diagnosArtal)) {
+                        addYearToValidation();
+                    }
                 });
+
+                function addYearToValidation() {
+                    var validation = [{
+                        field: $scope.config.yearConfig[0].modelProp.toLowerCase()
+                    }];
+                    $scope.validation.messagesByField[$scope.config.yearConfig[0].modelProp.toLowerCase()] = validation;
+                }
 
                 // Return validation errors for the specific row (previously splitted)
                 $scope.getValidationErrors = function(index) {
