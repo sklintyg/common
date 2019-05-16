21f53c747aef58193b1e455d0eb3d357836c920d
Per Beijer <per.beijer@usify.se>
2019-05-16 11:15:45 +0200

    INTYGFV-11084: Ny teckenbegränsning för diagnosbeskrivning i FKs intyg. (#16)

--- fk/luae_na/src/main/resources/META-INF/resources/webjars/luae_na/webcert/views/utkast/luae_naUtkastConfigFactory.v1.js
+++ fk/luae_na/src/main/resources/META-INF/resources/webjars/luae_na/webcert/views/utkast/luae_naUtkastConfigFactory.v1.js
@@ -225,6 +225,7 @@ angular.module('luae_na').factory('luae_na.UtkastConfigFactory.v1',
                         fraga(6, 'FRG_6.RBK', 'FRG_6.HLP', { required: true, requiredProp: 'diagnoser[0].diagnosKod'}, [{
                             type: 'ue-diagnos',
                             modelProp: 'diagnoser',
+                            descriptionMaxLength: 81,
                             defaultKodSystem: 'ICD_10_SE',
                             diagnosBeskrivningLabel: 'DFR_6.1.RBK',
                             diagnosBeskrivningHelp: 'DFR_6.1.HLP',
