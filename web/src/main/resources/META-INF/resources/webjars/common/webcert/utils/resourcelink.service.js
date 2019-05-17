785b25623874a67110d1e7bc9feaa8aa7c3b73cb
Matias Wurschmidt-Wang <matias.wurschmidt@soprasteria.com>
2019-05-17 08:27:10 +0200

    INTYG-6719: Javadoc for ActionLinkType.

--- web/src/main/resources/META-INF/resources/webjars/common/webcert/utils/resourcelink.service.js
+++ web/src/main/resources/META-INF/resources/webjars/common/webcert/utils/resourcelink.service.js
@@ -1,6 +1,16 @@
 angular.module('common').factory('common.ResourceLinkService', function() {
     'use strict';
 
+    /**
+     * Check if a specific ActionLinkType is available among the links.
+     *
+     * @param links
+     *              links to check
+     * @param linkType
+     *              linktype to look for
+     * @returns {boolean}
+     *              true if available
+     */
     function _isLinkTypeExists(links, linkType) {
         if (links === undefined) {
             return false;
