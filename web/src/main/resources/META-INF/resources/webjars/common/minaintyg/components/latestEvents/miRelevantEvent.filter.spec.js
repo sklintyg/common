7071b73f9eacd78e31f0e7f9cae6ad233e9dd7aa
Erik Gillsbro <erik.gillsbro@cag.se>
2019-09-05 14:28:57 +0200

    INTYGFV-11446: Lagt till status Kompletterar på intyg

--- web/src/main/resources/META-INF/resources/webjars/common/minaintyg/components/latestEvents/miRelevantEvent.filter.spec.js
+++ web/src/main/resources/META-INF/resources/webjars/common/minaintyg/components/latestEvents/miRelevantEvent.filter.spec.js
@@ -20,11 +20,17 @@ describe('Filter: miRelevantEvent', function() {
             type: 'ERSATT'
         }, {
             type: 'ERSATTER'
+        }, {
+            type: 'KOMPLETTERAT'
+        }, {
+            type: 'KOMPLETTERAR'
         } ];
-        expect(_filter(events).length).toEqual(3);
+        expect(_filter(events).length).toEqual(5);
         expect(_filter(events)[0].type).toEqual('SENT');
         expect(_filter(events)[1].type).toEqual('ERSATT');
         expect(_filter(events)[2].type).toEqual('ERSATTER');
+        expect(_filter(events)[3].type).toEqual('KOMPLETTERAT');
+        expect(_filter(events)[4].type).toEqual('KOMPLETTERAR');
     });
 
     it('should handle no events', function() {
