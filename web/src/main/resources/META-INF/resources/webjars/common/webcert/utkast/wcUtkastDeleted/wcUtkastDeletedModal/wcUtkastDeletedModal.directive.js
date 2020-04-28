ed3bd20fb34a8e255d28dad73f82f45073f5314e
Michaela Hörnfeldt <54178725+mhornfeldt@users.noreply.github.com>
2020-04-28 13:39:08 +0200

    INTYGFV-11293: Removed delay and added modal for copied drafts (#302)

--- web/src/main/resources/META-INF/resources/webjars/common/webcert/utkast/wcUtkastDeleted/wcUtkastDeletedModal/wcUtkastDeletedModal.directive.js
+++ web/src/main/resources/META-INF/resources/webjars/common/webcert/utkast/wcUtkastDeleted/wcUtkastDeletedModal/wcUtkastDeletedModal.directive.js
@@ -39,7 +39,7 @@ angular.module('common').directive('wcUtkastDeletedModal',
                   backdrop: true,
                   size: 'md'
                 });
-              }, 500);
+              });
             };
 
             $scope.init = function() {
@@ -50,7 +50,7 @@ angular.module('common').directive('wcUtkastDeletedModal',
                   if (modalInstance) {
                     modalInstance.close();
                   }
-                },2000);
+                },1500);
 
                 IntygViewState.deletedDraft = false;
               }
