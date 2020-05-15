c32a35ac86008d504210dec1acab5b214174c92b
Marcus Johansson <marcus.johansson@soprasteria.com>
2020-05-15 13:39:46 +0200

    INTYGFV-12742: ModuleApi extended, Import of WC format json

--- fk/fk-parent/src/main/java/se/inera/intyg/common/fkparent/rest/FkParentModuleApi.java
+++ fk/fk-parent/src/main/java/se/inera/intyg/common/fkparent/rest/FkParentModuleApi.java
@@ -23,7 +23,9 @@ import static se.inera.intyg.common.fkparent.model.converter.RespConstants.TILLA
 import static se.inera.intyg.common.fkparent.model.converter.RespConstants.TILLAGGSFRAGOR_SVAR_JSON_ID;
 import static se.inera.intyg.common.support.Constants.KV_PART_CODE_SYSTEM;
 
-
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Strings;
+import com.google.common.primitives.Ints;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.time.LocalDateTime;
@@ -32,19 +34,12 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import javax.xml.bind.JAXBElement;
 import javax.xml.ws.soap.SOAPFaultException;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.common.base.Strings;
-import com.google.common.primitives.Ints;
-
 import se.inera.intyg.common.fkparent.model.converter.RespConstants;
 import se.inera.intyg.common.fkparent.model.converter.SvarIdHelper;
 import se.inera.intyg.common.services.texts.IntygTextsService;
@@ -59,7 +54,7 @@ import se.inera.intyg.common.support.model.converter.util.WebcertModelFactoryUti
 import se.inera.intyg.common.support.modules.converter.InternalToRevoke;
 import se.inera.intyg.common.support.modules.converter.TransportConverterUtil;
 import se.inera.intyg.common.support.modules.service.WebcertModuleService;
-import se.inera.intyg.common.support.modules.support.api.ModuleApi;
+import se.inera.intyg.common.support.modules.support.api.ModuleApi2;
 import se.inera.intyg.common.support.modules.support.api.dto.CertificateMetaData;
 import se.inera.intyg.common.support.modules.support.api.dto.CertificateResponse;
 import se.inera.intyg.common.support.modules.support.api.dto.CreateDraftCopyHolder;
@@ -90,7 +85,7 @@ import se.riv.clinicalprocess.healthcond.certificate.types.v3.Part;
 import se.riv.clinicalprocess.healthcond.certificate.v3.Intyg;
 import se.riv.clinicalprocess.healthcond.certificate.v3.ResultCodeType;
 
-public abstract class FkParentModuleApi<T extends Utlatande> implements ModuleApi {
+public abstract class FkParentModuleApi<T extends Utlatande> implements ModuleApi2 {
 
     private static final Logger LOG = LoggerFactory.getLogger(FkParentModuleApi.class);
 
@@ -350,6 +345,11 @@ public abstract class FkParentModuleApi<T extends Utlatande> implements ModuleAp
         } catch (ConverterException e) {
             throw new ModuleException(e.getMessage());
         }
+    }
+
+    @Override
+    public RegisterCertificateType getTransportFromInternal(String internalModel) throws ModuleException, ConverterException {
+        return internalToTransport(getInternal(internalModel));
     }
 
     protected abstract String getSchematronFileName();
