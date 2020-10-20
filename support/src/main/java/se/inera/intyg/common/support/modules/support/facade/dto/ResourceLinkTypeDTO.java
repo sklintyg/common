695d0c55e66d0fb76e57ad760559f2950e51b12c
Matias Wurschmidt-Wang <matias.wurschmidt@soprasteria.com>
2020-10-20 09:50:01 +0200

    INTYGFV-13295: Added ResourceLinkTypeDTO.FORWARD_CERTIFICATE.

--- support/src/main/java/se/inera/intyg/common/support/modules/support/facade/dto/ResourceLinkTypeDTO.java
+++ support/src/main/java/se/inera/intyg/common/support/modules/support/facade/dto/ResourceLinkTypeDTO.java
@@ -3,6 +3,7 @@ package se.inera.intyg.common.support.modules.support.facade.dto;
 public enum ResourceLinkTypeDTO {
     EDIT_CERTIFICATE,
     REMOVE_CERTIFICATE,
+    FORWARD_CERTIFICATE,
     SIGN_CERTIFICATE,
     SEND_CERTIFICATE,
     REVOKE_CERTIFICATE,
