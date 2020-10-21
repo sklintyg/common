10e6023fff33da1200eeb2fcdf057fa28a6c9b2f
Matias Wurschmidt-Wang <matias.wurschmidt@soprasteria.com>
2020-10-21 14:36:22 +0200

    INTYGFV-13295: Added logic to handle locked drafts.

--- support/src/main/java/se/inera/intyg/common/support/modules/support/facade/dto/CertificateRelationTypeDTO.java
+++ support/src/main/java/se/inera/intyg/common/support/modules/support/facade/dto/CertificateRelationTypeDTO.java
@@ -1,5 +1,5 @@
 package se.inera.intyg.common.support.modules.support.facade.dto;
 
 public enum CertificateRelationTypeDTO {
-    REPLACED
+    REPLACED, COPIED
 }
--- support/src/main/java/se/inera/intyg/common/support/modules/support/facade/dto/CertificateStatusDTO.java
+++ support/src/main/java/se/inera/intyg/common/support/modules/support/facade/dto/CertificateStatusDTO.java
@@ -1,5 +1,5 @@
 package se.inera.intyg.common.support.modules.support.facade.dto;
 
 public enum CertificateStatusDTO {
-    UNSIGNED, SIGNED, INVALIDATED
+    UNSIGNED, SIGNED, LOCKED, REVOKED, LOCKED_REVOKED
 }
