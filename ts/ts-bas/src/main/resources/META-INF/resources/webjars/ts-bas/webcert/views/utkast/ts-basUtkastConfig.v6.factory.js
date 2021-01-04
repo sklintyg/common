ca5d6ce37b38a3ce8d244d1da0e7172dfa3ae836
Matias Wurschmidt-Wang <46641204+mww-sopra-steria@users.noreply.github.com>
2021-01-04 17:21:03 +0100

    INTYGFV-13682: Updated license header with year 2021. And add CI-workflow to validate pull request. (#395)

--- ts/ts-bas/src/main/resources/META-INF/resources/webjars/ts-bas/webcert/views/utkast/ts-basUtkastConfig.v6.factory.js
+++ ts/ts-bas/src/main/resources/META-INF/resources/webjars/ts-bas/webcert/views/utkast/ts-basUtkastConfig.v6.factory.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020 Inera AB (http://www.inera.se)
+ * Copyright (C) 2021 Inera AB (http://www.inera.se)
  *
  * This file is part of sklintyg (https://github.com/sklintyg).
  *
@@ -16,7 +16,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 angular.module('ts-bas').factory('ts-bas.UtkastConfigFactory.v6',
     ['$log', '$timeout', 'common.ueFactoryTemplatesHelper', 'common.ueTSFactoryTemplatesHelper',
         function($log, $timeout, ueFactoryTemplates, ueTSFactoryTemplates) {
