79f76a72b1c98c345698f6fff1244226cfd495b0
Matias Wurschmidt-Wang <matias.wurschmidt@soprasteria.com>
2021-01-04 16:00:17 +0100

    INTYGFV-13682: Updated license header with year 2021.

--- web/src/main/resources/META-INF/resources/webjars/common/app-shared/unified-view/components/uvNoValue/uvNoValue.directive.js
+++ web/src/main/resources/META-INF/resources/webjars/common/app-shared/unified-view/components/uvNoValue/uvNoValue.directive.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020 Inera AB (http://www.inera.se)
+ * Copyright (C) 2021 Inera AB (http://www.inera.se)
  *
  * This file is part of sklintyg (https://github.com/sklintyg).
  *
@@ -16,7 +16,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 /**
  * This sole purpose of this simple directive is to centralize the apperance of a filed that has no value to show.
  */
