350258e7cac4eb807c6f019147e01dc8f1fb816a
Matias Wurschmidt-Wang <matias.wurschmidt@soprasteria.com>
2021-01-13 15:18:03 +0100

    INTYGFV-10646: Cache flag if warning dialog has been displayed for the care provider. Flag is cached in session storage.

--- web/src/main/resources/META-INF/resources/webjars/common/webcert/components/headers/wcAppHeader/wcOriginNormalWarning/wcOriginNormalWarning.directive.js
+++ web/src/main/resources/META-INF/resources/webjars/common/webcert/components/headers/wcAppHeader/wcOriginNormalWarning/wcOriginNormalWarning.directive.js
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2021 Inera AB (http://www.inera.se)
+ *
+ * This file is part of sklintyg (https://github.com/sklintyg).
+ *
+ * sklintyg is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * sklintyg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 angular.module('common').directive('wcOriginNormalWarning',
     ['$uibModal', '$window', 'common.UserModel', 'common.featureService',
       function($uibModal, $window, UserModel, featureService) {
@@ -9,11 +27,12 @@ angular.module('common').directive('wcOriginNormalWarning',
           template: '',
           link: function() {
             var dialogInstance;
+            var careProviderHSAWithoutDash = UserModel.user.valdVardgivare.id.replaceAll('-', '');
 
             var showOriginNormalWarningDialog = function() {
-              $window.sessionStorage.originalNormalWarningDialogShown = true;
-
               var careProviderName = UserModel.user.valdVardgivare.namn;
+
+              $window.sessionStorage[careProviderHSAWithoutDash] = true;
 
               dialogInstance = $uibModal.open({
                 templateUrl:
@@ -29,7 +48,7 @@ angular.module('common').directive('wcOriginNormalWarning',
             var shouldOriginNormalWarningDialogBeDisplayed = function() {
               return UserModel.user.origin === 'NORMAL'
                   && featureService.isFeatureActive(featureService.features.VARNING_FRISTAENDE)
-                  && !$window.sessionStorage.originalNormalWarningDialogShown;
+                  && !$window.sessionStorage[careProviderHSAWithoutDash];
             }
 
             if (shouldOriginNormalWarningDialogBeDisplayed()) {
