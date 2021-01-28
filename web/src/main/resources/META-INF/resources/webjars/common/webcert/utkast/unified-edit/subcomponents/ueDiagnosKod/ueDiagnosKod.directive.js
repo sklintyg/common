f795b6641b0ffc14efd0c2dfa3bd98d9b26346ba
Erik Karlsson <erik.karlsson@soprasteria.com>
2021-01-28 12:32:13 +0100

    INTYGFV-13753: Updated license-header.

--- web/src/main/resources/META-INF/resources/webjars/common/webcert/utkast/unified-edit/subcomponents/ueDiagnosKod/ueDiagnosKod.directive.js
+++ web/src/main/resources/META-INF/resources/webjars/common/webcert/utkast/unified-edit/subcomponents/ueDiagnosKod/ueDiagnosKod.directive.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020 Inera AB (http://www.inera.se)
+ * Copyright (C) 2021 Inera AB (http://www.inera.se)
  *
  * This file is part of sklintyg (https://github.com/sklintyg).
  *
@@ -16,7 +16,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 angular.module('common').directive('ueDiagnosKod', ['$log', '$timeout', 'common.DiagnosProxy', 'common.fmbViewState',
     'common.fmbService', 'common.srsService', 'common.ObjectHelper', 'common.MonitoringLogService',
     'common.ArendeListViewStateService', 'common.UtkastValidationService', 'common.UtkastViewStateService',
