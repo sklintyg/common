a0c6ac1bf772a76977bd569a907f4bef936399ba
Matias Wurschmidt-Wang <46641204+mww-sopra-steria@users.noreply.github.com>
2021-03-09 13:41:05 +0100

    INTYGFV-13831: Added AdditionalMetaData that can be provided by a certificate type. The additional metadata will be stored in the CertificateMetaData table in Intygstjansten. (#421)

--- fk/luae_fs/src/main/java/se/inera/intyg/common/luae_fs/v1/rest/LuaefsModuleApiV1.java
+++ fk/luae_fs/src/main/java/se/inera/intyg/common/luae_fs/v1/rest/LuaefsModuleApiV1.java
@@ -18,32 +18,31 @@
  */
 package se.inera.intyg.common.luae_fs.v1.rest;
 
+import com.google.common.collect.ImmutableList;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-
-import com.google.common.collect.ImmutableList;
-
 import se.inera.intyg.common.fkparent.model.internal.Diagnos;
 import se.inera.intyg.common.fkparent.pdf.PdfGenerator;
 import se.inera.intyg.common.fkparent.pdf.PdfGeneratorException;
 import se.inera.intyg.common.fkparent.pdf.model.FkPdfDefinition;
 import se.inera.intyg.common.fkparent.rest.FkParentModuleApi;
+import se.inera.intyg.common.luae_fs.support.LuaefsEntryPoint;
 import se.inera.intyg.common.luae_fs.v1.model.converter.InternalToTransport;
 import se.inera.intyg.common.luae_fs.v1.model.converter.TransportToInternal;
 import se.inera.intyg.common.luae_fs.v1.model.converter.UtlatandeToIntyg;
 import se.inera.intyg.common.luae_fs.v1.model.internal.LuaefsUtlatandeV1;
 import se.inera.intyg.common.luae_fs.v1.pdf.LuaefsPdfDefinitionBuilder;
-import se.inera.intyg.common.luae_fs.support.LuaefsEntryPoint;
 import se.inera.intyg.common.services.texts.model.IntygTexts;
 import se.inera.intyg.common.support.model.Status;
 import se.inera.intyg.common.support.model.UtkastStatus;
 import se.inera.intyg.common.support.model.converter.util.ConverterException;
 import se.inera.intyg.common.support.modules.support.ApplicationOrigin;
+import se.inera.intyg.common.support.modules.support.api.dto.AdditionalMetaData;
 import se.inera.intyg.common.support.modules.support.api.dto.PdfResponse;
 import se.inera.intyg.common.support.modules.support.api.exception.ModuleException;
 import se.inera.intyg.common.support.modules.support.api.exception.ModuleSystemException;
@@ -143,6 +142,26 @@ public class LuaefsModuleApiV1 extends FkParentModuleApi<LuaefsUtlatandeV1> {
             }
         } catch (ConverterException e) {
             throw new ModuleException("Could convert Intyg to Utlatande and as a result could not get additional info", e);
+        }
+    }
+
+    @Override
+    public Optional<AdditionalMetaData> getAdditionalMetaData(Intyg certificate) throws ModuleException {
+        final var additionalMetaData = new AdditionalMetaData();
+
+        final var luaefsCertificate = convertToInternal(certificate);
+        final var diagnoses = getDiagnoses(luaefsCertificate.getDiagnoser());
+
+        additionalMetaData.setDiagnoses(diagnoses);
+
+        return Optional.of(additionalMetaData);
+    }
+
+    private LuaefsUtlatandeV1 convertToInternal(Intyg certificate) throws ModuleException {
+        try {
+            return transportToInternal(certificate);
+        } catch (ConverterException e) {
+            throw new ModuleException("Could convert Intyg to Utlatande", e);
         }
     }
 }
