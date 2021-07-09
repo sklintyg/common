f01f5914d005f4c542318fd7b9f9abe321d56cd9
Matias Wurschmidt-Wang <46641204+mww-sopra-steria@users.noreply.github.com>
2021-07-09 16:26:08 +0200

    INTYGFV-13873: Created facade model for Question, Reminder and Answer (#461)
    Co-authored-by: Robert Danielsson <robert.danielsson@soprasteria.com>
    Co-authored-by: mhornfeldt <michaela.hornfeldt@soprasteria.com>
    Co-authored-by: mww-sopra-steria<matias.wurschmidt@soprasteria.com>

---
+++ support/src/main/java/se/inera/intyg/common/support/facade/model/question/Question.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2021 Inera AB (http://www.inera.se)
+ *
+ * This file is part of sklintyg (https://github.com/sklintyg).
+ *
+ * sklintyg is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * sklintyg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package se.inera.intyg.common.support.facade.model.question;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Value;
+import se.inera.intyg.common.support.facade.model.question.Question.QuestionBuilder;
+
+@JsonDeserialize(builder = QuestionBuilder.class)
+@Value
+@Builder
+public class Question {
+
+    private String id;
+    private String subject;
+    private String message;
+    private String author;
+    private LocalDateTime sent;
+    private boolean isHandled;
+    private boolean isForwarded;
+    private Answer answer;
+    private Reminder[] reminders;
+    private LocalDateTime lastUpdate;
+
+    @JsonPOJOBuilder(withPrefix = "")
+    public static class QuestionBuilder {
+
+    }
+}
