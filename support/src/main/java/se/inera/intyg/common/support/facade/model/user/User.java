2c5fc0ba095f38cb3a04c82cff722e3830c84e5b
Matias Wurschmidt-Wang <46641204+mww-sopra-steria@users.noreply.github.com>
2021-08-06 14:38:45 +0200

    INTYGFV-13882: Added SigningMethod to User. Also refactored User to use Lombok. (#464)
    Co-authored-by: Robert Danielsson <robert.danielsson@soprasteria.com>

--- support/src/main/java/se/inera/intyg/common/support/facade/model/user/User.java
+++ support/src/main/java/se/inera/intyg/common/support/facade/model/user/User.java
@@ -16,53 +16,28 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package se.inera.intyg.common.support.modules.support.facade.dto;
+package se.inera.intyg.common.support.facade.model.user;
 
-public class UserDTO {
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+import lombok.Builder;
+import lombok.Value;
+import se.inera.intyg.common.support.facade.model.user.User.UserBuilder;
+
+@JsonDeserialize(builder = UserBuilder.class)
+@Value
+@Builder
+public class User {
 
     private String hsaId;
     private String name;
     private String role;
     private String loggedInUnit;
     private String loggedInCareProvider;
+    private SigningMethod signingMethod;
 
-    public String getHsaId() {
-        return hsaId;
-    }
+    @JsonPOJOBuilder(withPrefix = "")
+    public static class UserBuilder {
 
-    public void setHsaId(String hsaId) {
-        this.hsaId = hsaId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public String getLoggedInUnit() {
-        return loggedInUnit;
-    }
-
-    public void setLoggedInUnit(String loggedInUnit) {
-        this.loggedInUnit = loggedInUnit;
-    }
-
-    public String getLoggedInCareProvider() {
-        return loggedInCareProvider;
-    }
-
-    public void setLoggedInCareProvider(String loggedInCareProvider) {
-        this.loggedInCareProvider = loggedInCareProvider;
     }
 }
