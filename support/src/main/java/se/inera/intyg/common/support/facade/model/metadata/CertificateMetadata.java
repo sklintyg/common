bb65c220f008031f696ba5624c6ebe9eba4033e2
Matias Wurschmidt-Wang <46641204+mww-sopra-steria@users.noreply.github.com>
2021-11-15 07:41:22 +0100

    INTYGFV-14231: Add readyForSign in CertificateMetadata. (#509)

--- support/src/main/java/se/inera/intyg/common/support/facade/model/metadata/CertificateMetadata.java
+++ support/src/main/java/se/inera/intyg/common/support/facade/model/metadata/CertificateMetadata.java
@@ -51,6 +51,7 @@ public class CertificateMetadata {
     private Staff issuedBy;
     private long version;
     private boolean latestMajorVersion;
+    private LocalDateTime readyForSign;
 
     @JsonPOJOBuilder(withPrefix = "")
     public static class CertificateMetadataBuilder {
