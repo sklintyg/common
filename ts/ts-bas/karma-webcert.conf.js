b59c58b04c2878aace3fd7fa71ee03fead5f4415
mikeyOne <55154460+mikeyOne@users.noreply.github.com>
2022-01-13 08:49:11 +0100

    INTYGFV-14244: Replacing PhantomJS with ChromeHeadless. (#533)

--- ts/ts-bas/karma-webcert.conf.js
+++ ts/ts-bas/karma-webcert.conf.js
@@ -82,7 +82,7 @@ module.exports = function(config) {
         // - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
         // - PhantomJS
         // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
-        browsers: [ 'PhantomJS' ],
+        browsers: [ 'ChromeHeadless' ],
 
         // If browser does not capture in given timeout [ms], kill it
         captureTimeout: 60000,
@@ -95,7 +95,7 @@ module.exports = function(config) {
 
         plugins: [
             'karma-jasmine',
-            'karma-phantomjs-launcher',
+            'karma-chrome-launcher',
             'karma-mocha-reporter'
         ],
 
