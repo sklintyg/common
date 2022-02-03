4075700510085be3b87e3c5ad061a289bad7957c
mhornfeldt <michaela.hornfeldt@soprasteria.com>
2022-02-03 10:23:58 +0100

    INTYGFV-14426: Validation messages should include question id

--- skl/ag7804/src/main/java/se/inera/intyg/common/ag7804/v1/validator/ValidatorUtil.java
+++ skl/ag7804/src/main/java/se/inera/intyg/common/ag7804/v1/validator/ValidatorUtil.java
@@ -81,9 +81,9 @@ public class ValidatorUtil {
 
         if (diagnoser.size() > 1 && !validateFirstDiagnosIsPresent(diagnoser)) {
             // Om första diagnosen saknas, så ska det visas fel för hela första raden. Då ska inga andra fel visas.
-            se.inera.intyg.common.support.validate.ValidatorUtil.addValidationError(validationMessages, CATEGORY_DIAGNOS,
+            se.inera.intyg.common.support.validate.ValidatorUtil.addValidationErrorWithQuestionId(validationMessages, CATEGORY_DIAGNOS,
                 DIAGNOS_SVAR_JSON_ID_6 + "[0].row",
-                ValidationMessageType.INCORRECT_COMBINATION, "common.validation.c-13a");
+                ValidationMessageType.INCORRECT_COMBINATION, "common.validation.c-13a", DIAGNOS_SVAR_ID_6);
             return;
         }
 
