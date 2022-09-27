503f3d391ee641a8914f89964c4adca016492335
Matias Wurschmidt-Wang <matias.wurschmidt@soprasteria.com>
2022-09-27 07:48:23 +0200

    INTYGFV-14815: Convert QuestionUndersokningYttre. Introduced a description for Undersokning.JA. Needed to avoid special solution for this particular "code".

---
+++ sos/db/src/main/java/se/inera/intyg/common/db/v1/model/converter/certificate/QuestionUndersokningYttre.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright (C) 2022 Inera AB (http://www.inera.se)
+ *
+ * This file is part of sklintyg (https://github.com/sklintyg).
+ *
+ * sklintyg is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * sklintyg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package se.inera.intyg.common.db.v1.model.converter.certificate;
+
+import static se.inera.intyg.common.sos_parent.support.RespConstants.DODSPLATS_BOENDE_QUESTION_DESCRIPTION_ID;
+import static se.inera.intyg.common.sos_parent.support.RespConstants.UNDERSOKNING_YTTRE_CATEGORY_ID;
+import static se.inera.intyg.common.sos_parent.support.RespConstants.UNDERSOKNING_YTTRE_DELSVAR_ID;
+import static se.inera.intyg.common.sos_parent.support.RespConstants.UNDERSOKNING_YTTRE_JSON_ID;
+import static se.inera.intyg.common.support.facade.util.ValidationExpressionToolkit.multipleOrExpression;
+import static se.inera.intyg.common.support.facade.util.ValidationExpressionToolkit.singleExpression;
+
+import java.util.Arrays;
+import se.inera.intyg.common.db.model.internal.Undersokning;
+import se.inera.intyg.common.services.texts.CertificateTextProvider;
+import se.inera.intyg.common.support.facade.model.CertificateDataElement;
+import se.inera.intyg.common.support.facade.model.config.CertificateDataConfigRadioMultipleCode;
+import se.inera.intyg.common.support.facade.model.config.RadioMultipleCode;
+import se.inera.intyg.common.support.facade.model.validation.CertificateDataValidation;
+import se.inera.intyg.common.support.facade.model.validation.CertificateDataValidationMandatory;
+import se.inera.intyg.common.support.facade.model.value.CertificateDataValueCode;
+
+public class QuestionUndersokningYttre {
+
+    public static CertificateDataElement toCertificate(Undersokning undersokning, int index, CertificateTextProvider texts) {
+        return CertificateDataElement.builder()
+            .id(UNDERSOKNING_YTTRE_DELSVAR_ID)
+            .parent(UNDERSOKNING_YTTRE_CATEGORY_ID)
+            .index(index)
+            .config(
+                CertificateDataConfigRadioMultipleCode.builder()
+                    .text(texts.get(UNDERSOKNING_YTTRE_JSON_ID))
+                    .description(texts.get(DODSPLATS_BOENDE_QUESTION_DESCRIPTION_ID))
+                    .list(
+                        Arrays.asList(
+                            RadioMultipleCode.builder()
+                                .id(Undersokning.JA.name())
+                                .label(Undersokning.JA.getBeskrivning())
+                                .build(),
+                            RadioMultipleCode.builder()
+                                .id(Undersokning.UNDERSOKNING_SKA_GORAS.name())
+                                .label(Undersokning.UNDERSOKNING_SKA_GORAS.getBeskrivning())
+                                .build(),
+                            RadioMultipleCode.builder()
+                                .id(Undersokning.UNDERSOKNING_GJORT_KORT_FORE_DODEN.name())
+                                .label(Undersokning.UNDERSOKNING_GJORT_KORT_FORE_DODEN.getBeskrivning())
+                                .build()
+                        )
+                    )
+                    .build()
+            )
+            .value(
+                undersokning != null ? CertificateDataValueCode.builder()
+                    .id(undersokning.name())
+                    .code(undersokning.name())
+                    .build() : CertificateDataValueCode.builder().build()
+            )
+            .validation(
+                new CertificateDataValidation[]{
+                    CertificateDataValidationMandatory.builder()
+                        .questionId(UNDERSOKNING_YTTRE_DELSVAR_ID)
+                        .expression(
+                            multipleOrExpression(
+                                singleExpression(Undersokning.JA.name()),
+                                singleExpression(Undersokning.UNDERSOKNING_SKA_GORAS.name()),
+                                singleExpression(Undersokning.UNDERSOKNING_GJORT_KORT_FORE_DODEN.name())
+                            )
+                        )
+                        .build()
+                }
+            )
+            .build();
+    }
+}
---
+++ sos/db/src/test/java/se/inera/intyg/common/db/v1/model/converter/certificate/QuestionUndersokningYttreTest.java
@@ -0,0 +1,178 @@
+/*
+ * Copyright (C) 2022 Inera AB (http://www.inera.se)
+ *
+ * This file is part of sklintyg (https://github.com/sklintyg).
+ *
+ * sklintyg is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * sklintyg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package se.inera.intyg.common.db.v1.model.converter.certificate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+import static se.inera.intyg.common.sos_parent.support.RespConstants.UNDERSOKNING_YTTRE_CATEGORY_ID;
+import static se.inera.intyg.common.sos_parent.support.RespConstants.UNDERSOKNING_YTTRE_DELSVAR_ID;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import se.inera.intyg.common.db.model.internal.Undersokning;
+import se.inera.intyg.common.services.texts.CertificateTextProvider;
+import se.inera.intyg.common.support.facade.model.config.CertificateDataConfigRadioMultipleCode;
+import se.inera.intyg.common.support.facade.model.config.CertificateDataConfigTypes;
+import se.inera.intyg.common.support.facade.model.config.RadioMultipleCode;
+import se.inera.intyg.common.support.facade.model.validation.CertificateDataValidationMandatory;
+import se.inera.intyg.common.support.facade.model.validation.CertificateDataValidationType;
+import se.inera.intyg.common.support.facade.model.value.CertificateDataValueCode;
+import se.inera.intyg.common.support.facade.model.value.CertificateDataValueType;
+
+@ExtendWith(MockitoExtension.class)
+class QuestionUndersokningYttreTest {
+
+    @Mock
+    private CertificateTextProvider texts;
+
+    @BeforeEach
+    void setup() {
+        when(texts.get(Mockito.any(String.class))).thenReturn("Test string");
+    }
+
+    @Nested
+    @TestInstance(Lifecycle.PER_CLASS)
+    class ToCertificate {
+
+        @Test
+        void shouldIncludeId() {
+            final var question = QuestionUndersokningYttre.toCertificate(null, 0, texts);
+            assertEquals(UNDERSOKNING_YTTRE_DELSVAR_ID, question.getId());
+        }
+
+        @Test
+        void shouldIncludeIndex() {
+            final var expectedIndex = 1;
+            final var question = QuestionUndersokningYttre.toCertificate(null, expectedIndex, texts);
+            assertEquals(expectedIndex, question.getIndex());
+        }
+
+        @Test
+        void shouldIncludeParentId() {
+            final var question = QuestionUndersokningYttre.toCertificate(null, 0, texts);
+            assertEquals(UNDERSOKNING_YTTRE_CATEGORY_ID, question.getParent());
+        }
+
+        @Test
+        void shouldIncludeText() {
+            final var question = QuestionUndersokningYttre.toCertificate(null, 0, texts);
+            assertTrue(question.getConfig().getText().trim().length() > 0, "Missing text");
+        }
+
+        @Test
+        void shouldIncludeRadioMultipleCodeConfigType() {
+            final var question = QuestionUndersokningYttre.toCertificate(null, 0, texts);
+            assertEquals(CertificateDataConfigTypes.UE_RADIO_MULTIPLE_CODE, question.getConfig().getType());
+        }
+
+        @Test
+        void shouldIncludeRadioMultipleCodeConfigValueJa() {
+            final var expectedCode = RadioMultipleCode.builder()
+                .id(Undersokning.JA.name())
+                .label(Undersokning.JA.getBeskrivning())
+                .build();
+            final var question = QuestionUndersokningYttre.toCertificate(null, 0, texts);
+            final var certificateDataConfigRadioMultipleCode = (CertificateDataConfigRadioMultipleCode) question.getConfig();
+            assertEquals(expectedCode, certificateDataConfigRadioMultipleCode.getList().get(0));
+        }
+
+        @Test
+        void shouldIncludeRadioMultipleCodeConfigValueSkaGoras() {
+            final var expectedCode = RadioMultipleCode.builder()
+                .id(Undersokning.UNDERSOKNING_SKA_GORAS.name())
+                .label(Undersokning.UNDERSOKNING_SKA_GORAS.getBeskrivning())
+                .build();
+            final var question = QuestionUndersokningYttre.toCertificate(null, 0, texts);
+            final var certificateDataConfigRadioMultipleCode = (CertificateDataConfigRadioMultipleCode) question.getConfig();
+            assertEquals(expectedCode, certificateDataConfigRadioMultipleCode.getList().get(1));
+        }
+
+        @Test
+        void shouldIncludeRadioMultipleCodeConfigValueKortForeDoden() {
+            final var expectedCode = RadioMultipleCode.builder()
+                .id(Undersokning.UNDERSOKNING_GJORT_KORT_FORE_DODEN.name())
+                .label(Undersokning.UNDERSOKNING_GJORT_KORT_FORE_DODEN.getBeskrivning())
+                .build();
+            final var question = QuestionUndersokningYttre.toCertificate(null, 0, texts);
+            final var certificateDataConfigRadioMultipleCode = (CertificateDataConfigRadioMultipleCode) question.getConfig();
+            assertEquals(expectedCode, certificateDataConfigRadioMultipleCode.getList().get(2));
+        }
+
+        @Test
+        void shouldIncludeCodeValueType() {
+            final var question = QuestionUndersokningYttre.toCertificate(null, 0, texts);
+            assertEquals(CertificateDataValueType.CODE, question.getValue().getType());
+        }
+
+        @Test
+        void shouldIncludeCodeValueId() {
+            final var question = QuestionUndersokningYttre.toCertificate(Undersokning.JA, 0, texts);
+            final var certificateDataValueCode = (CertificateDataValueCode) question.getValue();
+            assertEquals(Undersokning.JA.name(), certificateDataValueCode.getId());
+        }
+
+        @Test
+        void shouldIncludeCodeValue() {
+            final var question = QuestionUndersokningYttre.toCertificate(Undersokning.JA, 0, texts);
+            final var certificateDataValueCode = (CertificateDataValueCode) question.getValue();
+            assertEquals(Undersokning.JA.name(), certificateDataValueCode.getCode());
+        }
+
+        @Test
+        void shouldIncludeCodeValueEmpty() {
+            final var question = QuestionUndersokningYttre.toCertificate(null, 0, texts);
+            final var certificateDataValueCode = (CertificateDataValueCode) question.getValue();
+            assertNull(certificateDataValueCode.getCode());
+        }
+
+        @Test
+        void shouldIncludeValidationMandatoryType() {
+            final var question = QuestionUndersokningYttre.toCertificate(null, 0, texts);
+            assertEquals(CertificateDataValidationType.MANDATORY_VALIDATION, question.getValidation()[0].getType());
+        }
+
+        @Test
+        void shouldIncludeValidationMandatoryQuestionId() {
+            final var question = QuestionUndersokningYttre.toCertificate(null, 0, texts);
+            final var certificateDataValidationMandatory = (CertificateDataValidationMandatory) question.getValidation()[0];
+            assertEquals(UNDERSOKNING_YTTRE_DELSVAR_ID, certificateDataValidationMandatory.getQuestionId());
+        }
+
+        @Test
+        void shouldIncludeValidationMandatoryExpression() {
+            final var expectedExpression = "$" + Undersokning.JA.name()
+                + " || $" + Undersokning.UNDERSOKNING_SKA_GORAS.name()
+                + " || $" + Undersokning.UNDERSOKNING_GJORT_KORT_FORE_DODEN.name();
+
+            final var question = QuestionUndersokningYttre.toCertificate(null, 0, texts);
+            final var certificateDataValidationMandatory = (CertificateDataValidationMandatory) question.getValidation()[0];
+            assertEquals(expectedExpression, certificateDataValidationMandatory.getExpression());
+        }
+    }
+}
