64b53f01eda7e4626ba5ed27d88bdeb7d7bebc88
Karl Friberg <28860194+IamFriberg@users.noreply.github.com>
2022-11-21 12:03:39 +0100

    INTYGFV-14886: FIxed spotbugs.

--- sos/doi/src/main/java/se/inera/intyg/common/doi/v1/model/converter/certificate/question/QuestionBidragandeSjukdomar.java
+++ sos/doi/src/main/java/se/inera/intyg/common/doi/v1/model/converter/certificate/question/QuestionBidragandeSjukdomar.java
@@ -50,7 +50,6 @@ public class QuestionBidragandeSjukdomar {
     private static final String DEBUT_ID = "debut";
 
     public static CertificateDataElement toCertificate(List<Dodsorsak> bidragandeSjukdomar, int index, CertificateTextProvider texts) {
-        final var specifications = getSpecifications();
 
         return CertificateDataElement.builder()
             .id(BIDRAGANDE_SJUKDOM_OM_DELSVAR_ID)
