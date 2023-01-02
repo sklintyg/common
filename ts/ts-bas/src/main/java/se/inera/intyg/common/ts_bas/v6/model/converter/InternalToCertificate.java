9a871ab6e12cdf385be75a5b36d5ddf56be74c38
SIGERHED Christoffer <christoffer.sigerhed@soprasteria.com>
2023-01-02 11:12:12 +0100

    INTYGFV-14488: Updated message for korrektionsglasensstyrka.

--- ts/ts-bas/src/main/java/se/inera/intyg/common/ts_bas/v6/model/converter/InternalToCertificate.java
+++ ts/ts-bas/src/main/java/se/inera/intyg/common/ts_bas/v6/model/converter/InternalToCertificate.java
@@ -47,6 +47,7 @@ import se.inera.intyg.common.ts_bas.v6.model.converter.certificate.question.Ques
 import se.inera.intyg.common.ts_bas.v6.model.converter.certificate.question.QuestionIdentitet;
 import se.inera.intyg.common.ts_bas.v6.model.converter.certificate.question.QuestionIntygetAvser;
 import se.inera.intyg.common.ts_bas.v6.model.converter.certificate.question.QuestionKorrektionsglasensStyrka;
+import se.inera.intyg.common.ts_bas.v6.model.converter.certificate.question.QuestionKorrektionsglasensStyrkaMessage;
 import se.inera.intyg.common.ts_bas.v6.model.converter.certificate.question.QuestionLakarintygAvOgonspecialistMessage;
 import se.inera.intyg.common.ts_bas.v6.model.converter.certificate.question.QuestionNattblindhet;
 import se.inera.intyg.common.ts_bas.v6.model.converter.certificate.question.QuestionNystagmus;
@@ -102,7 +103,7 @@ public class InternalToCertificate {
                 QuestionKorrektionsglasensStyrka.toCertificate(internalCertificate.getSyn(), index++, texts)
             )
             .addElement(
-                QuestionLakarintygAvOgonspecialistMessage.toCertificate(internalCertificate.getSyn(), index++, texts)
+                QuestionKorrektionsglasensStyrkaMessage.toCertificate(internalCertificate.getSyn(), index++, texts)
             )
             .addElement(
                 CategoryHorselOchBalanssinne.toCertificate(index++, texts)
