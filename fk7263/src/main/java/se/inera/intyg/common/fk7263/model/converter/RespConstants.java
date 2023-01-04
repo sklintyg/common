a17ff43a2bb3d0109ada8e5a044c123feb157db4
SIGERHED Christoffer <christoffer.sigerhed@soprasteria.com>
2023-01-04 08:23:11 +0100

    INTYGFV-15128: Updated licenses that were found after running -PcodeQuality.

--- fk7263/src/main/java/se/inera/intyg/common/fk7263/model/converter/RespConstants.java
+++ fk7263/src/main/java/se/inera/intyg/common/fk7263/model/converter/RespConstants.java
@@ -30,28 +30,20 @@ public class RespConstants {
     public static final String HUVUDDIAGNOSKOD_SVAR_TEXT_ID = "fk7263.label.diagnosisCode";
     public static final String DIAGNOS_FORTYDLIGANDE_SVAR_ID = "3";
     public static final String DIAGNOS_FORTYDLIGANDE_SVAR_TEXT_ID = "fk7263.label.diagnosfortydligande";
-
     public static final String SJUKDOMSFORLOPP_CATEGORY_ID = "sjukdomforlopp";
     public static final String SJUKDOMSFORLOPP_CATEGORY_TEXT_ID = "fk7263.label.progressofdesease";
-
     public static final String SJUKDOMSFORLOPP_SVAR_ID = "4";
-
     public static final String FUNKTIONSNEDSATTNING_CATEGORY_ID = "funktionsnedsattning";
     public static final String FUNKTIONSNEDSATTNING_CATEGORY_TEXT_ID = "fk7263.label.disabilities";
-
     public static final String FUNKTIONSNEDSATTNING_SVAR_ID = "5";
     public static final String INTYGET_BASERAS_PA_CATEGORY_ID = "intygetBaserasPa";
     public static final String INTYGET_BASERAS_PA_CATEGORY_TEXT_ID = "fk7263.label.basedon";
-
     public static final String UNDERSOKNING_AV_PATIENTEN_DELSVAR_ID = "6.1";
     public static final String UNDERSOKNING_AV_PATIENTEN_DELSVAR_TEXT = "Min undersökning av patienten den";
-
     public static final String TELEFONKONTAKT_MED_PATIENTEN_DELSVAR_ID = "6.2";
     public static final String TELEFONKONTAKT_MED_PATIENTEN_DELSVAR_TEXT = "Min telefonkontakt med patienten den";
-
     public static final String JOURNALUPPGIFTER_DELSVAR_ID = "6.3";
     public static final String JOURNALUPPGIFTER_DELSVAR_TEXT = "Journaluppgifter, den";
-
     public static final String ANNAT_DELSVAR_ID = "6.4";
     public static final String ANNAT_DELSVAR_TEXT = "Annat, den";
     public static final String AKTIVITETSBEGRANSNINGAR_CATEGORY_ID = "aktivitetsbegransningar";
@@ -83,37 +75,30 @@ public class RespConstants {
     public static final String PATIENTENS_ARBETSFORMAGA_ARBETSLOSHET_TEXT_ID = "fk7263.label.patientworkcapacity.unemployed";
     public static final String ARBETSFORMAGA_BEDOMNING_CATEGORY_ID = "arbetsformogaBedomning";
     public static final String ARBETSFORMAGA_BEDOMNING_CATEGORY_TEXT_ID = "fk7263.label.nedsattning.falt8b";
-
     public static final String ARBETSFORMAGA_BEDOMNING_SVAR_ID = "12";
     public static final String ARBETSFORMAGA_BEDOMNING_NEDSATT_ID = "nedsatt";
     public static final String ARBETSFORMAGA_BEDOMNING_NEDSATT_TEXT = "Nedsatt med";
-
     public static final String ARBETSFORMAGA_BEDOMNING_FOM_ID = "franOchMed";
     public static final String ARBETSFORMAGA_BEDOMNING_FOM_TEXT = "Från och med";
-
     public static final String ARBETSFORMAGA_BEDOMNING_TOM_ID = "tillOchMed";
     public static final String ARBETSFORMAGA_BEDOMNING_TOM_TEXT = "Längst till och med";
     public static final String ARBETSFORMAGA_BEDOMNING_NEDSATT_25_TEXT_ID = "fk7263.nedsattningsgrad.nedsatt_med_1_4";
     public static final String ARBETSFORMAGA_BEDOMNING_NEDSATT_50_TEXT_ID = "fk7263.nedsattningsgrad.nedsatt_med_1_2";
     public static final String ARBETSFORMAGA_BEDOMNING_NEDSATT_75_TEXT_ID = "fk7263.nedsattningsgrad.nedsatt_med_3_4";
     public static final String ARBETSFORMAGA_BEDOMNING_NEDSATT_100_TEXT_ID = "fk7263.nedsattningsgrad.helt_nedsatt";
-
     public static final String ARBETSFORMAGA_FORSAKRINGSMEDICINSKA_BESLUTSTODET_CATEGORY_ID = "arbetsformogaForsakringsmedicinska";
     public static final String ARBETSFORMAGA_FORSAKRINGSMEDICINSKA_BESLUTSTODET_CATEGORY_TEXT_ID =
         "fk7263.label.patientworkcapacityjudgement";
     public static final String ARBETSFORMAGA_FORSAKRINGSMEDICINSKA_BESLUTSTODET_SVAR_ID = "13";
     public static final String ARBETSFORMAGA_PROGNOS_CATEGORY_ID = "arbetsformogaPrognos";
     public static final String ARBETSFORMAGA_PROGNOS_CATEGORY_TEXT_ID = "fk7263.label.prognosis";
-
     public static final String ARBETSFORMAGA_PROGNOS_SVAR_ID = "14";
     public static final String RESOR_TILL_FRAN_ARBETE_CATEGORY_ID = "resorTillFranArbete";
     public static final String RESOR_TILL_FRAN_ARBETE_CATEGORY_TEXT_ID = "fk7263.label.othertransport";
     public static final String RESOR_TILL_FRAN_ARBETE_SVAR_ID = "15";
-
     public static final String KONTAKT_MED_FK_CATEGORY_ID = "kontaktMedFk";
     public static final String KONTAKT_MED_FK_CATEGORY_TEXT_ID = "fk7263.label.fkcontact";
     public static final String KONTAKT_MED_FK_SVAR_ID = "16";
-
     public static final String OVRIGA_UPPLYSNINGAR_CATEGORY_ID = "ovrigaUpplysningar";
     public static final String OVRIGA_UPPLYSNINGAR_CATEGORY_TEXT_ID = "fk7263.label.otherinformation";
     public static final String OVRIGA_UPPLYSNINGAR_SVAR_ID = "17";
