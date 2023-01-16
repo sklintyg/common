942b7b278adc202b6d81d6e013c807433cdf664e
Mikael Heglind <mikael.heglind@soprasteria.com>
2023-01-16 23:05:28 +0100

    INTYGFV-15160: Implemented category Bedomning with questions.

--- ts/ts-diabetes/src/main/java/se/inera/intyg/common/ts_diabetes/v4/model/internal/BedomningKorkortstyp.java
+++ ts/ts-diabetes/src/main/java/se/inera/intyg/common/ts_diabetes/v4/model/internal/BedomningKorkortstyp.java
@@ -18,6 +18,8 @@
  */
 package se.inera.intyg.common.ts_diabetes.v4.model.internal;
 
+import java.util.stream.Stream;
+
 /**
  * This is a subset (VAR1-9, VAR11-18) of "Kv körkortsbehörighet"
  * {@link se.inera.intyg.common.ts_parent.codes.KorkortsbehorighetKod}.
@@ -25,21 +27,36 @@ package se.inera.intyg.common.ts_diabetes.v4.model.internal;
  * Note - the order is adjusted to match requirements.
  */
 public enum BedomningKorkortstyp {
-    VAR12,
-    VAR13,
-    VAR14,
-    VAR15,
-    VAR16,
-    VAR17,
-    VAR18,
-    VAR1,
-    VAR2,
-    VAR3,
-    VAR4,
-    VAR5,
-    VAR6,
-    VAR7,
-    VAR8,
-    VAR9,
-    VAR11
+    VAR12("AM"),
+    VAR13("A1"),
+    VAR14("A2"),
+    VAR15("A"),
+    VAR16("B"),
+    VAR17("BE"),
+    VAR18("TRAKTOR"),
+    VAR1("C1"),
+    VAR2("C1E"),
+    VAR3("C"),
+    VAR4("CE"),
+    VAR5("D1"),
+    VAR6("D1E"),
+    VAR7("D"),
+    VAR8("DE"),
+    VAR9("TAXI"),
+    VAR11("KANINTETASTALLNING");
+
+    final String type;
+
+    BedomningKorkortstyp(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public static BedomningKorkortstyp fromCode(String type) {
+        return Stream.of(BedomningKorkortstyp.values()).filter(s -> type.equals(s.getType())).findFirst()
+            .orElseThrow(() -> new IllegalArgumentException(type));
+    }
 }
