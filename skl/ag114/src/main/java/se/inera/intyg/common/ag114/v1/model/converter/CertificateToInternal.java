f355523de0b2bfa7f3bad8b7c312b15d317b6ffd
SIGERHED Christoffer <christoffer.sigerhed@soprasteria.com>
2023-01-24 14:26:17 +0100

    INTYGFV-15202: Removed unused import.

--- skl/ag114/src/main/java/se/inera/intyg/common/ag114/v1/model/converter/CertificateToInternal.java
+++ skl/ag114/src/main/java/se/inera/intyg/common/ag114/v1/model/converter/CertificateToInternal.java
@@ -42,10 +42,6 @@ import se.inera.intyg.common.ag114.v1.model.converter.certificate.question.Quest
 import se.inera.intyg.common.ag114.v1.model.internal.Ag114UtlatandeV1;
 import se.inera.intyg.common.support.facade.model.Certificate;
 import se.inera.intyg.common.support.modules.service.WebcertModuleService;
-import org.springframework.stereotype.Component;
-import se.inera.intyg.common.ag114.v1.model.converter.certificate.MetaDataGrundData;
-import se.inera.intyg.common.ag114.v1.model.internal.Ag114UtlatandeV1;
-import se.inera.intyg.common.support.facade.model.Certificate;
 
 
 @Component(value = "certificateToInternalAg1-14")
