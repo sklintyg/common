8f881eb2b0353aadb23ada98df93e96f68ef4de9
SIGERHED Christoffer <christoffer.sigerhed@soprasteria.com>
2023-02-16 16:13:59 +0100

    INTYGFV-15291: Implemented AbstractQuestionFunktionsnedsattningLisjp and extended it in QuestionFunktionsnedsattning.

--- fk/lisjp/src/main/java/se/inera/intyg/common/lisjp/v1/model/converter/certificate/question/QuestionFunktionsnedsattning.java
+++ fk/lisjp/src/main/java/se/inera/intyg/common/lisjp/v1/model/converter/certificate/question/QuestionFunktionsnedsattning.java
@@ -19,80 +19,30 @@
 
 package se.inera.intyg.common.lisjp.v1.model.converter.certificate.question;
 
-import static se.inera.intyg.common.lisjp.v1.model.converter.RespConstants.AVSTANGNING_SMITTSKYDD_SVAR_ID_27;
-import static se.inera.intyg.common.lisjp.v1.model.converter.RespConstants.AVSTANGNING_SMITTSKYDD_SVAR_JSON_ID_27;
 import static se.inera.intyg.common.lisjp.v1.model.converter.RespConstants.FUNKTIONSNEDSATTNING_CATEGORY_ID;
-import static se.inera.intyg.common.lisjp.v1.model.converter.RespConstants.FUNKTIONSNEDSATTNING_DELSVAR_BESKRIVNING;
-import static se.inera.intyg.common.lisjp.v1.model.converter.RespConstants.FUNKTIONSNEDSATTNING_DELSVAR_TEXT;
-import static se.inera.intyg.common.lisjp.v1.model.converter.RespConstants.FUNKTIONSNEDSATTNING_ICF_COLLECTION;
-import static se.inera.intyg.common.lisjp.v1.model.converter.RespConstants.FUNKTIONSNEDSATTNING_ICF_INFO;
-import static se.inera.intyg.common.lisjp.v1.model.converter.RespConstants.FUNKTIONSNEDSATTNING_ICF_PLACEHOLDER;
 import static se.inera.intyg.common.lisjp.v1.model.converter.RespConstants.FUNKTIONSNEDSATTNING_SVAR_ID_35;
 import static se.inera.intyg.common.lisjp.v1.model.converter.RespConstants.FUNKTIONSNEDSATTNING_SVAR_JSON_ID_35;
-import static se.inera.intyg.common.lisjp.v1.model.converter.RespConstants.FUNKTIONSNEDSATTNING_SVAR_TEXT;
-import static se.inera.intyg.common.support.facade.util.ValidationExpressionToolkit.exists;
-import static se.inera.intyg.common.support.facade.util.ValueToolkit.icfCodeValue;
-import static se.inera.intyg.common.support.facade.util.ValueToolkit.icfTextValue;
 
 import java.util.List;
+import se.inera.intyg.common.fkparent.model.converter.certificate.AbstractQuestionFunktionsnedsattningLisjp;
 import se.inera.intyg.common.services.texts.CertificateTextProvider;
 import se.inera.intyg.common.support.facade.model.Certificate;
 import se.inera.intyg.common.support.facade.model.CertificateDataElement;
-import se.inera.intyg.common.support.facade.model.config.CertificateDataConfigIcf;
-import se.inera.intyg.common.support.facade.model.validation.CertificateDataValidation;
-import se.inera.intyg.common.support.facade.model.validation.CertificateDataValidationHide;
-import se.inera.intyg.common.support.facade.model.validation.CertificateDataValidationMandatory;
-import se.inera.intyg.common.support.facade.model.value.CertificateDataIcfValue;
 
-public class QuestionFunktionsnedsattning {
+public class QuestionFunktionsnedsattning extends AbstractQuestionFunktionsnedsattningLisjp {
 
     public static CertificateDataElement toCertificate(String value,
         List<String> disabilityCategories, int index,
         CertificateTextProvider texts) {
-        return CertificateDataElement.builder()
-            .id(FUNKTIONSNEDSATTNING_SVAR_ID_35)
-            .index(index)
-            .parent(FUNKTIONSNEDSATTNING_CATEGORY_ID)
-            .config(
-                CertificateDataConfigIcf.builder()
-                    .header(texts.get(FUNKTIONSNEDSATTNING_SVAR_TEXT))
-                    .text(texts.get(FUNKTIONSNEDSATTNING_DELSVAR_TEXT))
-                    .description(texts.get(FUNKTIONSNEDSATTNING_DELSVAR_BESKRIVNING))
-                    .modalLabel(FUNKTIONSNEDSATTNING_ICF_INFO)
-                    .collectionsLabel(FUNKTIONSNEDSATTNING_ICF_COLLECTION)
-                    .id(FUNKTIONSNEDSATTNING_SVAR_JSON_ID_35)
-                    .placeholder(FUNKTIONSNEDSATTNING_ICF_PLACEHOLDER)
-                    .build()
-            )
-            .value(
-                CertificateDataIcfValue.builder()
-                    .id(FUNKTIONSNEDSATTNING_SVAR_JSON_ID_35)
-                    .text(value)
-                    .icfCodes(disabilityCategories)
-                    .build()
-            )
-            .validation(
-                new CertificateDataValidation[]{
-                    CertificateDataValidationMandatory.builder()
-                        .questionId(FUNKTIONSNEDSATTNING_SVAR_ID_35)
-                        .expression(
-                            exists(FUNKTIONSNEDSATTNING_SVAR_JSON_ID_35)
-                        )
-                        .build(),
-                    CertificateDataValidationHide.builder()
-                        .questionId(AVSTANGNING_SMITTSKYDD_SVAR_ID_27)
-                        .expression(exists(AVSTANGNING_SMITTSKYDD_SVAR_JSON_ID_27))
-                        .build()
-                }
-            )
-            .build();
+        return toCertificate(value, FUNKTIONSNEDSATTNING_SVAR_ID_35, FUNKTIONSNEDSATTNING_CATEGORY_ID, FUNKTIONSNEDSATTNING_SVAR_JSON_ID_35,
+            disabilityCategories, index, texts);
     }
 
     public static String toInternalTextValue(Certificate certificate) {
-        return icfTextValue(certificate.getData(), FUNKTIONSNEDSATTNING_SVAR_ID_35, FUNKTIONSNEDSATTNING_SVAR_JSON_ID_35);
+        return toInternalTextValue(certificate, FUNKTIONSNEDSATTNING_SVAR_ID_35, FUNKTIONSNEDSATTNING_SVAR_JSON_ID_35);
     }
 
     public static List<String> toInternalCodeValue(Certificate certificate) {
-        return icfCodeValue(certificate.getData(), FUNKTIONSNEDSATTNING_SVAR_ID_35, FUNKTIONSNEDSATTNING_SVAR_JSON_ID_35);
+        return toInternalCodeValue(certificate, FUNKTIONSNEDSATTNING_SVAR_ID_35, FUNKTIONSNEDSATTNING_SVAR_JSON_ID_35);
     }
 }
