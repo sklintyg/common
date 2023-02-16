df5e3f6191bde235f96b47ea5f5823fc9abc2bd4
SIGERHED Christoffer <christoffer.sigerhed@soprasteria.com>
2023-02-16 16:02:03 +0100

    INTYGFV-15291: Implemented AbstractQuestionAvstangningSmittskydd and extended it in QuestionAvstangningSmittskydd.

--- fk/lisjp/src/main/java/se/inera/intyg/common/lisjp/v1/model/converter/certificate/question/QuestionAvstangningSmittskydd.java
+++ fk/lisjp/src/main/java/se/inera/intyg/common/lisjp/v1/model/converter/certificate/question/QuestionAvstangningSmittskydd.java
@@ -19,46 +19,24 @@
 
 package se.inera.intyg.common.lisjp.v1.model.converter.certificate.question;
 
-import static se.inera.intyg.common.lisjp.v1.model.converter.RespConstants.ANSWER_NOT_SELECTED;
-import static se.inera.intyg.common.lisjp.v1.model.converter.RespConstants.ANSWER_YES;
 import static se.inera.intyg.common.lisjp.v1.model.converter.RespConstants.AVSTANGNING_SMITTSKYDD_CATEGORY_ID;
-import static se.inera.intyg.common.lisjp.v1.model.converter.RespConstants.AVSTANGNING_SMITTSKYDD_QUESTION_LABEL;
 import static se.inera.intyg.common.lisjp.v1.model.converter.RespConstants.AVSTANGNING_SMITTSKYDD_SVAR_ID_27;
 import static se.inera.intyg.common.lisjp.v1.model.converter.RespConstants.AVSTANGNING_SMITTSKYDD_SVAR_JSON_ID_27;
-import static se.inera.intyg.common.support.facade.util.ValueToolkit.booleanValue;
 
+import se.inera.intyg.common.fkparent.model.converter.certificate.AbstractQuestionAvstangningSmittskydd;
 import se.inera.intyg.common.services.texts.CertificateTextProvider;
 import se.inera.intyg.common.support.facade.model.Certificate;
 import se.inera.intyg.common.support.facade.model.CertificateDataElement;
-import se.inera.intyg.common.support.facade.model.config.CertificateDataConfigCheckboxBoolean;
-import se.inera.intyg.common.support.facade.model.value.CertificateDataValueBoolean;
 
-public class QuestionAvstangningSmittskydd {
+public class QuestionAvstangningSmittskydd extends AbstractQuestionAvstangningSmittskydd {
 
     public static CertificateDataElement toCertificate(Boolean value, int index,
         CertificateTextProvider texts) {
-        return CertificateDataElement.builder()
-            .id(AVSTANGNING_SMITTSKYDD_SVAR_ID_27)
-            .index(index)
-            .parent(AVSTANGNING_SMITTSKYDD_CATEGORY_ID)
-            .config(
-                CertificateDataConfigCheckboxBoolean.builder()
-                    .id(AVSTANGNING_SMITTSKYDD_SVAR_JSON_ID_27)
-                    .label(texts.get(AVSTANGNING_SMITTSKYDD_QUESTION_LABEL))
-                    .selectedText(texts.get(ANSWER_YES))
-                    .unselectedText(texts.get(ANSWER_NOT_SELECTED))
-                    .build()
-            )
-            .value(
-                CertificateDataValueBoolean.builder()
-                    .id(AVSTANGNING_SMITTSKYDD_SVAR_JSON_ID_27)
-                    .selected(value)
-                    .build()
-            )
-            .build();
+        return toCertificate(value, AVSTANGNING_SMITTSKYDD_SVAR_ID_27, AVSTANGNING_SMITTSKYDD_CATEGORY_ID,
+            AVSTANGNING_SMITTSKYDD_SVAR_JSON_ID_27, index, texts);
     }
 
     public static Boolean toInternal(Certificate certificate) {
-        return booleanValue(certificate.getData(), AVSTANGNING_SMITTSKYDD_SVAR_ID_27, AVSTANGNING_SMITTSKYDD_SVAR_JSON_ID_27);
+        return toInternal(certificate, AVSTANGNING_SMITTSKYDD_SVAR_ID_27, AVSTANGNING_SMITTSKYDD_SVAR_JSON_ID_27);
     }
 }
