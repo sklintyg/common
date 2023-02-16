5b3494ae3af3d5e1cd26656f019255401ba5f178
mhornfeldt <michaela.hoernfeldt@soprasteria.com>
2023-02-16 16:32:09 +0100

    INTYGFV-15052: Checkstyle updates

--- skl/ag7804/src/test/java/se/inera/intyg/common/ag7804/v1/model/converter/InternalToCertificateTest.java
+++ skl/ag7804/src/test/java/se/inera/intyg/common/ag7804/v1/model/converter/InternalToCertificateTest.java
@@ -3949,7 +3949,8 @@ class InternalToCertificateTest {
                 assertAll("Validation question validation",
                     () -> assertEquals(PROGNOS_BESKRIVNING_DELSVAR_ID_39, certificateDataValidationMandatory.getQuestionId()),
                     () -> assertEquals(
-                        "exists(TRETTIO_DGR) || exists(SEXTIO_DGR) || exists(NITTIO_DGR) || exists(HUNDRAATTIO_DAGAR) || exists(TREHUNDRASEXTIOFEM_DAGAR)",
+                        "exists(TRETTIO_DGR) || exists(SEXTIO_DGR) || exists(NITTIO_DGR) "
+                            + "|| exists(HUNDRAATTIO_DAGAR) || exists(TREHUNDRASEXTIOFEM_DAGAR)",
                         certificateDataValidationMandatory.getExpression()
                     )
                 );
@@ -4513,9 +4514,18 @@ class InternalToCertificateTest {
                 final var certificateDataValidationMandatory = (CertificateDataValidationMandatory) question.getValidation()[0];
                 assertAll("Validation question validation",
                     () -> assertEquals(ARBETSLIVSINRIKTADE_ATGARDER_SVAR_ID_40, certificateDataValidationMandatory.getQuestionId()),
-                    () -> assertEquals("exists(EJ_AKTUELLT) || exists(ARBETSTRANING) || exists(ARBETSANPASSNING) || exists(BESOK_ARBETSPLATS)"
-                            + " || exists(ERGONOMISK) || exists(HJALPMEDEL) || exists(KONTAKT_FHV) || exists(OMFORDELNING) || exists(OVRIGA_ATGARDER)",
-                        certificateDataValidationMandatory.getExpression())
+                    () -> assertEquals(
+                        "exists(EJ_AKTUELLT) " +
+                            "|| exists(ARBETSTRANING) " +
+                            "|| exists(ARBETSANPASSNING) " +
+                            "|| exists(BESOK_ARBETSPLATS) " +
+                            "|| exists(ERGONOMISK) " +
+                            "|| exists(HJALPMEDEL) " +
+                            "|| exists(KONTAKT_FHV)" +
+                            "|| exists(OMFORDELNING)" +
+                            "|| exists(OVRIGA_ATGARDER)",
+                        certificateDataValidationMandatory.getExpression()
+                    )
                 );
             }
 
