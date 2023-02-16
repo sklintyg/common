27bb1fffbc615c51040920255e33142e75c23341
SIGERHED Christoffer <christoffer.sigerhed@soprasteria.com>
2023-02-16 15:09:09 +0100

    INTYGFV-15291: Implemented AbstractQuestionArbetsresor and extended it in QuestionArbetsresor.

---
+++ fk/fk-parent/src/main/java/se/inera/intyg/common/fkparent/model/converter/certificate/AbstractQuestionArbetsresor.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2023 Inera AB (http://www.inera.se)
+ *
+ * This file is part of sklintyg (https://github.com/sklintyg).
+ *
+ * sklintyg is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * sklintyg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package se.inera.intyg.common.fkparent.model.converter.certificate;
+
+import static se.inera.intyg.common.fkparent.model.converter.RespConstants.ANSWER_NO;
+import static se.inera.intyg.common.fkparent.model.converter.RespConstants.ANSWER_YES;
+import static se.inera.intyg.common.fkparent.model.converter.RespConstants.ARBETSRESOR_SVAR_TEXT;
+import static se.inera.intyg.common.fkparent.model.converter.RespConstants.AVSTANGNING_SMITTSKYDD_SVAR_ID_27;
+import static se.inera.intyg.common.fkparent.model.converter.RespConstants.AVSTANGNING_SMITTSKYDD_SVAR_JSON_ID_27;
+import static se.inera.intyg.common.support.facade.util.ValidationExpressionToolkit.exists;
+import static se.inera.intyg.common.support.facade.util.ValueToolkit.booleanValue;
+
+import se.inera.intyg.common.services.texts.CertificateTextProvider;
+import se.inera.intyg.common.support.facade.model.Certificate;
+import se.inera.intyg.common.support.facade.model.CertificateDataElement;
+import se.inera.intyg.common.support.facade.model.config.CertificateDataConfigCheckboxBoolean;
+import se.inera.intyg.common.support.facade.model.validation.CertificateDataValidation;
+import se.inera.intyg.common.support.facade.model.validation.CertificateDataValidationHide;
+import se.inera.intyg.common.support.facade.model.value.CertificateDataValueBoolean;
+
+public abstract class AbstractQuestionArbetsresor {
+
+    public static CertificateDataElement toCertificate(Boolean value, String id, String parent, String jsonId, int index,
+        CertificateTextProvider texts) {
+        return CertificateDataElement.builder()
+            .id(id)
+            .index(index)
+            .parent(parent)
+            .config(
+                CertificateDataConfigCheckboxBoolean.builder()
+                    .id(jsonId)
+                    .label(texts.get(ARBETSRESOR_SVAR_TEXT))
+                    .selectedText(texts.get(ANSWER_YES))
+                    .unselectedText(texts.get(ANSWER_NO))
+                    .build()
+            )
+            .value(
+                CertificateDataValueBoolean.builder()
+                    .id(jsonId)
+                    .selected(value)
+                    .build()
+            )
+            .validation(
+                new CertificateDataValidation[]{
+                    CertificateDataValidationHide.builder()
+                        .questionId(AVSTANGNING_SMITTSKYDD_SVAR_ID_27)
+                        .expression(exists(AVSTANGNING_SMITTSKYDD_SVAR_JSON_ID_27))
+                        .build()
+                }
+            )
+            .build();
+    }
+
+    public static Boolean toInternal(Certificate certificate, String questionId, String jsonId) {
+        return booleanValue(certificate.getData(), questionId, jsonId);
+    }
+}
--- fk/lisjp/src/main/java/se/inera/intyg/common/lisjp/v1/model/converter/certificate/question/QuestionArbetsresor.java
+++ fk/lisjp/src/main/java/se/inera/intyg/common/lisjp/v1/model/converter/certificate/question/QuestionArbetsresor.java
@@ -19,59 +19,23 @@
 
 package se.inera.intyg.common.lisjp.v1.model.converter.certificate.question;
 
-import static se.inera.intyg.common.lisjp.v1.model.converter.RespConstants.ANSWER_NO;
-import static se.inera.intyg.common.lisjp.v1.model.converter.RespConstants.ANSWER_YES;
 import static se.inera.intyg.common.lisjp.v1.model.converter.RespConstants.ARBETSRESOR_SVAR_ID_34;
 import static se.inera.intyg.common.lisjp.v1.model.converter.RespConstants.ARBETSRESOR_SVAR_JSON_ID_34;
-import static se.inera.intyg.common.lisjp.v1.model.converter.RespConstants.ARBETSRESOR_SVAR_TEXT;
-import static se.inera.intyg.common.lisjp.v1.model.converter.RespConstants.AVSTANGNING_SMITTSKYDD_SVAR_ID_27;
-import static se.inera.intyg.common.lisjp.v1.model.converter.RespConstants.AVSTANGNING_SMITTSKYDD_SVAR_JSON_ID_27;
 import static se.inera.intyg.common.lisjp.v1.model.converter.RespConstants.BEDOMNING_CATEGORY_ID;
-import static se.inera.intyg.common.support.facade.util.ValidationExpressionToolkit.exists;
-import static se.inera.intyg.common.support.facade.util.ValueToolkit.booleanValue;
 
+import se.inera.intyg.common.fkparent.model.converter.certificate.AbstractQuestionArbetsresor;
 import se.inera.intyg.common.services.texts.CertificateTextProvider;
 import se.inera.intyg.common.support.facade.model.Certificate;
 import se.inera.intyg.common.support.facade.model.CertificateDataElement;
-import se.inera.intyg.common.support.facade.model.config.CertificateDataConfigCheckboxBoolean;
-import se.inera.intyg.common.support.facade.model.validation.CertificateDataValidation;
-import se.inera.intyg.common.support.facade.model.validation.CertificateDataValidationHide;
-import se.inera.intyg.common.support.facade.model.value.CertificateDataValueBoolean;
 
-public class QuestionArbetsresor {
+public class QuestionArbetsresor extends AbstractQuestionArbetsresor {
 
     public static CertificateDataElement toCertificate(Boolean value, int index,
         CertificateTextProvider texts) {
-        return CertificateDataElement.builder()
-            .id(ARBETSRESOR_SVAR_ID_34)
-            .index(index)
-            .parent(BEDOMNING_CATEGORY_ID)
-            .config(
-                CertificateDataConfigCheckboxBoolean.builder()
-                    .id(ARBETSRESOR_SVAR_JSON_ID_34)
-                    .label(texts.get(ARBETSRESOR_SVAR_TEXT))
-                    .selectedText(texts.get(ANSWER_YES))
-                    .unselectedText(texts.get(ANSWER_NO))
-                    .build()
-            )
-            .value(
-                CertificateDataValueBoolean.builder()
-                    .id(ARBETSRESOR_SVAR_JSON_ID_34)
-                    .selected(value)
-                    .build()
-            )
-            .validation(
-                new CertificateDataValidation[]{
-                    CertificateDataValidationHide.builder()
-                        .questionId(AVSTANGNING_SMITTSKYDD_SVAR_ID_27)
-                        .expression(exists(AVSTANGNING_SMITTSKYDD_SVAR_JSON_ID_27))
-                        .build()
-                }
-            )
-            .build();
+        return toCertificate(value, ARBETSRESOR_SVAR_ID_34, BEDOMNING_CATEGORY_ID, ARBETSRESOR_SVAR_JSON_ID_34, index, texts);
     }
 
     public static Boolean toInternal(Certificate certificate) {
-        return booleanValue(certificate.getData(), ARBETSRESOR_SVAR_ID_34, ARBETSRESOR_SVAR_JSON_ID_34);
+        return toInternal(certificate, ARBETSRESOR_SVAR_ID_34, ARBETSRESOR_SVAR_JSON_ID_34);
     }
 }
