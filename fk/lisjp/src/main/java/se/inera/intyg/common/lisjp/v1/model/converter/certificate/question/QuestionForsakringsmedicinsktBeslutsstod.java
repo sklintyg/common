64f62bf1d23089dcbaad7eb4191622114cd45d35
SIGERHED Christoffer <christoffer.sigerhed@soprasteria.com>
2023-02-16 16:07:23 +0100

    INTYGFV-15291: Implemented AbstractQuestionForsakringsmedicinsktBeslutsstod and extended it in QuestionForsakringsmedicinsktBeslutsstod.

--- fk/lisjp/src/main/java/se/inera/intyg/common/lisjp/v1/model/converter/certificate/question/QuestionForsakringsmedicinsktBeslutsstod.java
+++ fk/lisjp/src/main/java/se/inera/intyg/common/lisjp/v1/model/converter/certificate/question/QuestionForsakringsmedicinsktBeslutsstod.java
@@ -19,58 +19,25 @@
 
 package se.inera.intyg.common.lisjp.v1.model.converter.certificate.question;
 
-import static se.inera.intyg.common.lisjp.v1.model.converter.RespConstants.AVSTANGNING_SMITTSKYDD_SVAR_ID_27;
-import static se.inera.intyg.common.lisjp.v1.model.converter.RespConstants.AVSTANGNING_SMITTSKYDD_SVAR_JSON_ID_27;
 import static se.inera.intyg.common.lisjp.v1.model.converter.RespConstants.BEDOMNING_CATEGORY_ID;
-import static se.inera.intyg.common.lisjp.v1.model.converter.RespConstants.FORSAKRINGSMEDICINSKT_BESLUTSSTOD_SVAR_BESKRIVNING;
 import static se.inera.intyg.common.lisjp.v1.model.converter.RespConstants.FORSAKRINGSMEDICINSKT_BESLUTSSTOD_SVAR_ID_37;
 import static se.inera.intyg.common.lisjp.v1.model.converter.RespConstants.FORSAKRINGSMEDICINSKT_BESLUTSSTOD_SVAR_JSON_ID_37;
-import static se.inera.intyg.common.lisjp.v1.model.converter.RespConstants.FORSAKRINGSMEDICINSKT_BESLUTSSTOD_SVAR_TEXT;
-import static se.inera.intyg.common.support.facade.util.ValidationExpressionToolkit.exists;
-import static se.inera.intyg.common.support.facade.util.ValueToolkit.textValue;
 
+import se.inera.intyg.common.fkparent.model.converter.certificate.AbstractQuestionForsakringsmedicinsktBeslutsstod;
 import se.inera.intyg.common.services.texts.CertificateTextProvider;
 import se.inera.intyg.common.support.facade.model.Certificate;
 import se.inera.intyg.common.support.facade.model.CertificateDataElement;
-import se.inera.intyg.common.support.facade.model.config.CertificateDataConfigTextArea;
-import se.inera.intyg.common.support.facade.model.validation.CertificateDataValidation;
-import se.inera.intyg.common.support.facade.model.validation.CertificateDataValidationHide;
-import se.inera.intyg.common.support.facade.model.value.CertificateDataTextValue;
 
-public class QuestionForsakringsmedicinsktBeslutsstod {
+public class QuestionForsakringsmedicinsktBeslutsstod extends AbstractQuestionForsakringsmedicinsktBeslutsstod {
 
     public static CertificateDataElement toCertificate(String value, int index,
         CertificateTextProvider texts) {
-        return CertificateDataElement.builder()
-            .id(FORSAKRINGSMEDICINSKT_BESLUTSSTOD_SVAR_ID_37)
-            .index(index)
-            .parent(BEDOMNING_CATEGORY_ID)
-            .config(
-                CertificateDataConfigTextArea.builder()
-                    .text(texts.get(FORSAKRINGSMEDICINSKT_BESLUTSSTOD_SVAR_TEXT))
-                    .description(texts.get(FORSAKRINGSMEDICINSKT_BESLUTSSTOD_SVAR_BESKRIVNING))
-                    .id(FORSAKRINGSMEDICINSKT_BESLUTSSTOD_SVAR_JSON_ID_37)
-                    .build()
-            )
-            .value(
-                CertificateDataTextValue.builder()
-                    .id(FORSAKRINGSMEDICINSKT_BESLUTSSTOD_SVAR_JSON_ID_37)
-                    .text(value)
-                    .build()
-            )
-            .validation(
-                new CertificateDataValidation[]{
-                    CertificateDataValidationHide.builder()
-                        .questionId(AVSTANGNING_SMITTSKYDD_SVAR_ID_27)
-                        .expression(exists(AVSTANGNING_SMITTSKYDD_SVAR_JSON_ID_27))
-                        .build()
-                }
-            )
-            .build();
+        return toCertificate(value, FORSAKRINGSMEDICINSKT_BESLUTSSTOD_SVAR_ID_37, BEDOMNING_CATEGORY_ID,
+            FORSAKRINGSMEDICINSKT_BESLUTSSTOD_SVAR_JSON_ID_37, index, texts);
     }
 
     public static String toInternal(Certificate certificate) {
-        return textValue(certificate.getData(), FORSAKRINGSMEDICINSKT_BESLUTSSTOD_SVAR_ID_37,
+        return toInternal(certificate, FORSAKRINGSMEDICINSKT_BESLUTSSTOD_SVAR_ID_37,
             FORSAKRINGSMEDICINSKT_BESLUTSSTOD_SVAR_JSON_ID_37);
     }
 }
