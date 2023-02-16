e13c613281eda211a8097f79962b6081dc06b85c
SIGERHED Christoffer <christoffer.sigerhed@soprasteria.com>
2023-02-16 15:01:59 +0100

    INTYGFV-15291: Implemented AbstractQuestionAktivitetsbegransningar and extended it in QuestionAktivitetsbegransningar.

--- fk/lisjp/src/main/java/se/inera/intyg/common/lisjp/v1/model/converter/certificate/question/QuestionAktivitetsbegransningar.java
+++ fk/lisjp/src/main/java/se/inera/intyg/common/lisjp/v1/model/converter/certificate/question/QuestionAktivitetsbegransningar.java
@@ -19,81 +19,30 @@
 
 package se.inera.intyg.common.lisjp.v1.model.converter.certificate.question;
 
-import static se.inera.intyg.common.lisjp.v1.model.converter.RespConstants.AKTIVITETSBEGRANSNING_DELSVAR_BESKRIVNING;
-import static se.inera.intyg.common.lisjp.v1.model.converter.RespConstants.AKTIVITETSBEGRANSNING_DELSVAR_TEXT;
-import static se.inera.intyg.common.lisjp.v1.model.converter.RespConstants.AKTIVITETSBEGRANSNING_ICF_COLLECTION;
-import static se.inera.intyg.common.lisjp.v1.model.converter.RespConstants.AKTIVITETSBEGRANSNING_ICF_INFO;
-import static se.inera.intyg.common.lisjp.v1.model.converter.RespConstants.AKTIVITETSBEGRANSNING_ICF_PLACEHOLDER;
 import static se.inera.intyg.common.lisjp.v1.model.converter.RespConstants.AKTIVITETSBEGRANSNING_SVAR_ID_17;
 import static se.inera.intyg.common.lisjp.v1.model.converter.RespConstants.AKTIVITETSBEGRANSNING_SVAR_JSON_ID_17;
-import static se.inera.intyg.common.lisjp.v1.model.converter.RespConstants.AKTIVITETSBEGRANSNING_SVAR_TEXT;
-import static se.inera.intyg.common.lisjp.v1.model.converter.RespConstants.AVSTANGNING_SMITTSKYDD_SVAR_ID_27;
-import static se.inera.intyg.common.lisjp.v1.model.converter.RespConstants.AVSTANGNING_SMITTSKYDD_SVAR_JSON_ID_27;
 import static se.inera.intyg.common.lisjp.v1.model.converter.RespConstants.FUNKTIONSNEDSATTNING_CATEGORY_ID;
-import static se.inera.intyg.common.support.facade.util.ValidationExpressionToolkit.exists;
-import static se.inera.intyg.common.support.facade.util.ValidationExpressionToolkit.singleExpression;
-import static se.inera.intyg.common.support.facade.util.ValueToolkit.icfCodeValue;
-import static se.inera.intyg.common.support.facade.util.ValueToolkit.icfTextValue;
 
 import java.util.List;
+import se.inera.intyg.common.fkparent.model.converter.certificate.AbstractQuestionAktivitetsbegransningar;
 import se.inera.intyg.common.services.texts.CertificateTextProvider;
 import se.inera.intyg.common.support.facade.model.Certificate;
 import se.inera.intyg.common.support.facade.model.CertificateDataElement;
-import se.inera.intyg.common.support.facade.model.config.CertificateDataConfigIcf;
-import se.inera.intyg.common.support.facade.model.validation.CertificateDataValidation;
-import se.inera.intyg.common.support.facade.model.validation.CertificateDataValidationHide;
-import se.inera.intyg.common.support.facade.model.validation.CertificateDataValidationMandatory;
-import se.inera.intyg.common.support.facade.model.value.CertificateDataIcfValue;
 
-public class QuestionAktivitetsbegransningar {
+public class QuestionAktivitetsbegransningar extends AbstractQuestionAktivitetsbegransningar {
 
     public static CertificateDataElement toCertificate(String value, List<String> activityLimitationCategories,
         int index,
         CertificateTextProvider texts) {
-        return CertificateDataElement.builder()
-            .id(AKTIVITETSBEGRANSNING_SVAR_ID_17)
-            .index(index)
-            .parent(FUNKTIONSNEDSATTNING_CATEGORY_ID)
-            .config(
-                CertificateDataConfigIcf.builder()
-                    .header(texts.get(AKTIVITETSBEGRANSNING_SVAR_TEXT))
-                    .text(texts.get(AKTIVITETSBEGRANSNING_DELSVAR_TEXT))
-                    .description(texts.get(AKTIVITETSBEGRANSNING_DELSVAR_BESKRIVNING))
-                    .modalLabel(AKTIVITETSBEGRANSNING_ICF_INFO)
-                    .collectionsLabel(AKTIVITETSBEGRANSNING_ICF_COLLECTION)
-                    .id(AKTIVITETSBEGRANSNING_SVAR_JSON_ID_17)
-                    .placeholder(AKTIVITETSBEGRANSNING_ICF_PLACEHOLDER)
-                    .build()
-            )
-            .value(
-                CertificateDataIcfValue.builder()
-                    .id(AKTIVITETSBEGRANSNING_SVAR_JSON_ID_17)
-                    .text(value)
-                    .icfCodes(activityLimitationCategories)
-                    .build()
-            )
-            .validation(
-                new CertificateDataValidation[]{
-                    CertificateDataValidationMandatory.builder()
-                        .questionId(AKTIVITETSBEGRANSNING_SVAR_ID_17)
-                        .expression(
-                            singleExpression(AKTIVITETSBEGRANSNING_SVAR_JSON_ID_17)
-                        )
-                        .build(),
-                    CertificateDataValidationHide.builder()
-                        .questionId(AVSTANGNING_SMITTSKYDD_SVAR_ID_27)
-                        .expression(exists(AVSTANGNING_SMITTSKYDD_SVAR_JSON_ID_27))
-                        .build()
-                }
-            )
-            .build();
+        return toCertificate(value, AKTIVITETSBEGRANSNING_SVAR_ID_17, FUNKTIONSNEDSATTNING_CATEGORY_ID,
+            AKTIVITETSBEGRANSNING_SVAR_JSON_ID_17, activityLimitationCategories, index, texts);
     }
 
     public static String toInternalTextValue(Certificate certificate) {
-        return icfTextValue(certificate.getData(), AKTIVITETSBEGRANSNING_SVAR_ID_17, AKTIVITETSBEGRANSNING_SVAR_JSON_ID_17);
+        return toInternalTextValue(certificate, AKTIVITETSBEGRANSNING_SVAR_ID_17, AKTIVITETSBEGRANSNING_SVAR_JSON_ID_17);
     }
 
     public static List<String> toInternalCodeValue(Certificate certificate) {
-        return icfCodeValue(certificate.getData(), AKTIVITETSBEGRANSNING_SVAR_ID_17, AKTIVITETSBEGRANSNING_SVAR_JSON_ID_17);
+        return toInternalCodeValue(certificate, AKTIVITETSBEGRANSNING_SVAR_ID_17, AKTIVITETSBEGRANSNING_SVAR_JSON_ID_17);
     }
 }
