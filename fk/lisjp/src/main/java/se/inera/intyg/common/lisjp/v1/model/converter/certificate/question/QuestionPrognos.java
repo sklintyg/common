4229742229d0a80cb30fb545824709cd58185e29
SIGERHED Christoffer <christoffer.sigerhed@soprasteria.com>
2023-02-17 11:36:03 +0100

    INTYGFV-15291: Implemented AbstractQuestionPrognos and extended it in QuestionPrognos.

--- fk/lisjp/src/main/java/se/inera/intyg/common/lisjp/v1/model/converter/certificate/question/QuestionPrognos.java
+++ fk/lisjp/src/main/java/se/inera/intyg/common/lisjp/v1/model/converter/certificate/question/QuestionPrognos.java
@@ -19,118 +19,23 @@
 
 package se.inera.intyg.common.lisjp.v1.model.converter.certificate.question;
 
-import static se.inera.intyg.common.lisjp.v1.model.converter.RespConstants.AVSTANGNING_SMITTSKYDD_SVAR_ID_27;
-import static se.inera.intyg.common.lisjp.v1.model.converter.RespConstants.AVSTANGNING_SMITTSKYDD_SVAR_JSON_ID_27;
 import static se.inera.intyg.common.lisjp.v1.model.converter.RespConstants.BEDOMNING_CATEGORY_ID;
 import static se.inera.intyg.common.lisjp.v1.model.converter.RespConstants.PROGNOS_BESKRIVNING_DELSVAR_ID_39;
-import static se.inera.intyg.common.lisjp.v1.model.converter.RespConstants.PROGNOS_SVAR_ATER_X_ANTAL_DAGAR;
-import static se.inera.intyg.common.lisjp.v1.model.converter.RespConstants.PROGNOS_SVAR_BESKRIVNING;
 import static se.inera.intyg.common.lisjp.v1.model.converter.RespConstants.PROGNOS_SVAR_ID_39;
-import static se.inera.intyg.common.lisjp.v1.model.converter.RespConstants.PROGNOS_SVAR_PROGNOS_OKLAR;
-import static se.inera.intyg.common.lisjp.v1.model.converter.RespConstants.PROGNOS_SVAR_SANNOLIKT_INTE;
-import static se.inera.intyg.common.lisjp.v1.model.converter.RespConstants.PROGNOS_SVAR_STOR_SANNOLIKHET;
-import static se.inera.intyg.common.lisjp.v1.model.converter.RespConstants.PROGNOS_SVAR_TEXT;
-import static se.inera.intyg.common.support.facade.util.ValidationExpressionToolkit.exists;
-import static se.inera.intyg.common.support.facade.util.ValidationExpressionToolkit.multipleOrExpression;
-import static se.inera.intyg.common.support.facade.util.ValueToolkit.codeValue;
 
-import java.util.Arrays;
 import se.inera.intyg.common.lisjp.model.internal.Prognos;
-import se.inera.intyg.common.lisjp.model.internal.PrognosDagarTillArbeteTyp;
-import se.inera.intyg.common.lisjp.model.internal.PrognosTyp;
 import se.inera.intyg.common.services.texts.CertificateTextProvider;
 import se.inera.intyg.common.support.facade.model.Certificate;
 import se.inera.intyg.common.support.facade.model.CertificateDataElement;
-import se.inera.intyg.common.support.facade.model.config.CertificateDataConfigRadioMultipleCodeOptionalDropdown;
-import se.inera.intyg.common.support.facade.model.config.RadioMultipleCodeOptionalDropdown;
-import se.inera.intyg.common.support.facade.model.validation.CertificateDataValidation;
-import se.inera.intyg.common.support.facade.model.validation.CertificateDataValidationHide;
-import se.inera.intyg.common.support.facade.model.validation.CertificateDataValidationMandatory;
-import se.inera.intyg.common.support.facade.model.value.CertificateDataValueCode;
 
-public class QuestionPrognos {
+public class QuestionPrognos extends AbstractQuestionPrognos {
 
     public static CertificateDataElement toCertificate(Prognos prognos, int index,
         CertificateTextProvider texts) {
-        return CertificateDataElement.builder()
-            .id(PROGNOS_SVAR_ID_39)
-            .index(index)
-            .parent(BEDOMNING_CATEGORY_ID)
-            .config(
-                CertificateDataConfigRadioMultipleCodeOptionalDropdown.builder()
-                    .text(texts.get(PROGNOS_SVAR_TEXT))
-                    .description(texts.get(PROGNOS_SVAR_BESKRIVNING))
-                    .list(
-                        Arrays.asList(
-                            RadioMultipleCodeOptionalDropdown.builder()
-                                .id(PrognosTyp.MED_STOR_SANNOLIKHET.getId())
-                                .label(texts.get(PROGNOS_SVAR_STOR_SANNOLIKHET))
-                                .build(),
-                            RadioMultipleCodeOptionalDropdown.builder()
-                                .id(PrognosTyp.ATER_X_ANTAL_DGR.getId())
-                                .label(texts.get(PROGNOS_SVAR_ATER_X_ANTAL_DAGAR))
-                                .dropdownQuestionId(PROGNOS_BESKRIVNING_DELSVAR_ID_39)
-                                .build(),
-                            RadioMultipleCodeOptionalDropdown.builder()
-                                .id(PrognosTyp.SANNOLIKT_EJ_ATERGA_TILL_SYSSELSATTNING.getId())
-                                .label(texts.get(PROGNOS_SVAR_SANNOLIKT_INTE))
-                                .build(),
-                            RadioMultipleCodeOptionalDropdown.builder()
-                                .id(PrognosTyp.PROGNOS_OKLAR.getId())
-                                .label(texts.get(PROGNOS_SVAR_PROGNOS_OKLAR))
-                                .build()
-                        )
-                    )
-                    .build()
-            )
-            .value(
-                CertificateDataValueCode.builder()
-                    .id(getPrognosValue(prognos))
-                    .code(getPrognosValue(prognos))
-                    .build()
-            )
-            .validation(
-                new CertificateDataValidation[]{
-                    CertificateDataValidationMandatory.builder()
-                        .questionId(PROGNOS_SVAR_ID_39)
-                        .expression(
-                            multipleOrExpression(
-                                exists(PrognosTyp.MED_STOR_SANNOLIKHET.getId()),
-                                exists(PrognosTyp.ATER_X_ANTAL_DGR.getId()),
-                                exists(PrognosTyp.SANNOLIKT_EJ_ATERGA_TILL_SYSSELSATTNING.getId()),
-                                exists(PrognosTyp.PROGNOS_OKLAR.getId())
-                            )
-                        )
-                        .build(),
-                    CertificateDataValidationHide.builder()
-                        .questionId(AVSTANGNING_SMITTSKYDD_SVAR_ID_27)
-                        .expression(exists(AVSTANGNING_SMITTSKYDD_SVAR_JSON_ID_27))
-                        .build()
-                }
-            )
-            .build();
-    }
-
-    private static String getPrognosValue(Prognos prognos) {
-        return (prognos != null && prognos.getTyp() != null) ? prognos.getTyp().getId() : null;
-    }
-
-    private static PrognosTyp getPrognosType(String type) {
-        return type != null ? PrognosTyp.fromId(type) : null;
-    }
-
-    private static PrognosDagarTillArbeteTyp getPrognosDays(String days) {
-        return days != null && !days.isEmpty() ? PrognosDagarTillArbeteTyp.fromId(days) : null;
+        return toCertificate(prognos, PROGNOS_SVAR_ID_39, BEDOMNING_CATEGORY_ID, index, texts);
     }
 
     public static Prognos toInternal(Certificate certificate) {
-        var codeType = codeValue(certificate.getData(), PROGNOS_SVAR_ID_39);
-        var codeDays = codeValue(certificate.getData(), PROGNOS_BESKRIVNING_DELSVAR_ID_39);
-
-        if (codeType == null && codeDays == null) {
-            return null;
-        }
-
-        return Prognos.create(getPrognosType(codeType), getPrognosDays(codeDays));
+        return toInternal(certificate, PROGNOS_SVAR_ID_39, PROGNOS_BESKRIVNING_DELSVAR_ID_39);
     }
 }
