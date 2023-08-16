1989b4a06e30542803011cab89655edba35905a3
mhornfeldt <michaela.hoernfeldt@soprasteria.com>
2023-08-16 18:57:47 +0200

    INTYGFV-15719: Fixed variables for logging

--- web/src/main/resources/META-INF/resources/webjars/common/webcert/components/wcSupportPanelManager/wcSrsPanelTab/wcSrsPanelTab.directive.js
+++ web/src/main/resources/META-INF/resources/webjars/common/webcert/components/wcSupportPanelManager/wcSrsPanelTab/wcSrsPanelTab.directive.js
@@ -171,8 +171,8 @@ angular.module('common').directive('wcSrsPanelTab',
                     $scope.srs.isReadOnly = true;
                     $scope.srs.intygsTyp = content.typ;
                     $scope.srs.personId = content.grundData.patient.personId;
-                    $scope.srs.vardgivareHsaId = content.grundData.skapadAv.vardenhet.vardgivarid;
-                    $scope.srs.HsaId = content.grundData.skapadAv.vardenhet.enhetsid
+                    $scope.srs.vardgivareHsaId = content.grundData.skapadAv.vardenhet.vardgivare.vardgivarid;
+                    $scope.srs.hsaId = content.grundData.skapadAv.vardenhet.enhetsid
                     if(content.diagnoser.length > 0) {
                         $scope.srs.originalDiagnosKod = content.diagnoser[0].diagnosKod
                     }
