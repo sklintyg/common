ce6c20bfef0b30736d4971d43ef8ee09f118ea7e
Daniel Jönsson <daniel.joensson@soprasteria.com>
2025-10-14 13:40:53 +0200

    K1J-1410: Implement Serializable interface in PaTitle class

--- support/src/main/java/se/inera/intyg/common/support/model/common/internal/PaTitle.java
+++ support/src/main/java/se/inera/intyg/common/support/model/common/internal/PaTitle.java
@@ -1,9 +1,10 @@
 package se.inera.intyg.common.support.model.common.internal;
 
+import java.io.Serializable;
 import lombok.Data;
 
 @Data
-public class PaTitle {
+public class PaTitle implements Serializable {
 
     String paTitleCode;
     String paTitleName;
