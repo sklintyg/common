bdc5e255462e90e6f8ba7768e3c8b3eb48b09535
Daniel Jönsson <daniel.joensson@soprasteria.com>
2025-10-21 15:50:05 +0200

    K1J-1410: Refactor RoundTripTest to use AF00251UtlatandeV1 for JSON comparison

--- af/af00251/src/test/java/se/inera/intyg/common/af00251/v1/model/converter/RoundTripTest.java
+++ af/af00251/src/test/java/se/inera/intyg/common/af00251/v1/model/converter/RoundTripTest.java
@@ -113,9 +113,11 @@ class RoundTripTest {
         assertFalse(diff.hasDifferences(), name + " " + diff.toString());
 
         JsonNode tree = objectMapper.valueToTree(TransportToInternal.convert(transport.getIntyg()));
-        System.out.println(scenario.asInternalModel());
-        JsonNode expectedTree = objectMapper.readTree(
-            getClass().getResourceAsStream("/internal/scenarios/roundtripjson/" + name + ".json"));
+
+        AF00251UtlatandeV1 expectedInternal = objectMapper.readValue(
+            getClass().getResourceAsStream("/internal/scenarios/roundtripjson/" + name + ".json"),
+            AF00251UtlatandeV1.class);
+        JsonNode expectedTree = objectMapper.valueToTree(expectedInternal);
         JSONAssert.assertEquals(expectedTree.toString(), tree.toString(), false);
     }
 
@@ -131,9 +133,10 @@ class RoundTripTest {
         AF00251UtlatandeV1 internal = TransportToInternal.convert(scenario.asTransportModel().getIntyg());
 
         JsonNode tree = objectMapper.valueToTree(internal);
-        JsonNode expectedTree = objectMapper.readTree(
-            getClass().getResourceAsStream("/internal/scenarios/roundtripjson/" + name + ".json"));
-
+        AF00251UtlatandeV1 expectedInternal = objectMapper.readValue(
+            getClass().getResourceAsStream("/internal/scenarios/roundtripjson/" + name + ".json"),
+            AF00251UtlatandeV1.class);
+        JsonNode expectedTree = objectMapper.valueToTree(expectedInternal);
         JSONAssert.assertEquals(expectedTree.toString(), tree.toString(), false);
 
         JAXBContext jaxbContext = JAXBContext.newInstance(RegisterCertificateType.class, DatePeriodType.class, PQType.class);
